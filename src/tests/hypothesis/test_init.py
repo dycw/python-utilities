e2d156169f31f1f58a4b1bdd4d49e56ef2aad067
Derek Wan <d.wan@icloud.com>
2022-05-17 22:43:33 +0800

    Add draw_and_map/flatmap

--- src/tests/hypothesis/test_init.py
+++ src/tests/hypothesis/test_init.py
@@ -1,9 +1,16 @@
+from typing import Union
+
 from hypothesis import given
+from hypothesis.strategies import DataObject
+from hypothesis.strategies import SearchStrategy
 from hypothesis.strategies import booleans
+from hypothesis.strategies import data
 from hypothesis.strategies import just
 from pytest import raises
 
 from dycw_utilities.hypothesis import assume_does_not_raise
+from dycw_utilities.hypothesis import draw_and_flatmap
+from dycw_utilities.hypothesis import draw_and_map
 from dycw_utilities.hypothesis import setup_hypothesis_profiles
 
 
@@ -37,6 +44,46 @@ class TestAssumeDoesNotRaise:
         ):
             if x is True:
                 raise ValueError("x is True")
+
+
+def uses_draw_and_map(
+    x: Union[bool, SearchStrategy[bool]], /
+) -> SearchStrategy[bool]:
+    def inner(x: bool, /) -> bool:
+        return x
+
+    return draw_and_map(inner, x)
+
+
+class TestDrawAndMap:
+    @given(data=data(), x=booleans())
+    def test_fixed(self, data: DataObject, x: bool) -> None:
+        result = data.draw(uses_draw_and_map(x))
+        assert result is x
+
+    @given(x=uses_draw_and_map(booleans()))
+    def test_strategy(self, x: bool) -> None:
+        assert isinstance(x, bool)
+
+
+def uses_draw_and_flatmap(
+    x: Union[bool, SearchStrategy[bool]], /
+) -> SearchStrategy[bool]:
+    def inner(x: bool, /) -> SearchStrategy[bool]:
+        return just(x)
+
+    return draw_and_flatmap(inner, x)
+
+
+class TestDrawAndFlatMap:
+    @given(data=data(), x=booleans())
+    def test_fixed(self, data: DataObject, x: bool) -> None:
+        result = data.draw(uses_draw_and_flatmap(x))
+        assert result is x
+
+    @given(x=uses_draw_and_flatmap(booleans()))
+    def test_strategy(self, x: bool) -> None:
+        assert isinstance(x, bool)
 
 
 class TestSetupHypothesisProfiles:
