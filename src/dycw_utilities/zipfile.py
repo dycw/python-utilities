ff3d376e6d64b7c97fddc4a1ef3437da19a8edbc
Derek Wan <d.wan@icloud.com>
2022-05-22 13:23:34 +0800

    Fudge the coverage

--- src/dycw_utilities/zipfile.py
+++ src/dycw_utilities/zipfile.py
@@ -14,3 +14,4 @@ def yield_zip_file_contents(path: PathLike, /) -> Iterator[list[Path]]:
     with ZipFile(path) as zf, TemporaryDirectory() as temp:
         zf.extractall(path=temp)
         yield list(temp.iterdir())
+    _ = zf  # this ensures that `zf` is considered returned; for coverage
