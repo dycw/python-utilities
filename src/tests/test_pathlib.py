b7ce29bb8f1eab5034b02f98f19c2f325c40614f
Derek Wan <d.wan@icloud.com>
2022-05-22 16:46:25 +0800

    Add `profile` (#36)

--- src/tests/test_pathlib.py
+++ src/tests/test_pathlib.py
@@ -1,21 +1,26 @@
 from pathlib import Path
 
+from pytest import mark
+from pytest import param
 from pytest import raises
 from typeguard import typechecked
 
 from dycw_utilities.pathlib import PathLike
 
 
-class TestPathLike:
-    def test_main(self) -> None:
-        @typechecked
-        def func(path: PathLike, /) -> PathLike:
-            return path
+@typechecked
+def _uses_pathlike(path: PathLike, /) -> PathLike:
+    return path
 
-        _ = func(Path.home())
-        _ = func("~")
+
+class TestPathLike:
+    @mark.parametrize("path", [param(Path.home()), param("~")])
+    def test_main(self, path: PathLike) -> None:
+        _ = _uses_pathlike(path)
+
+    def test_error(self) -> None:
         with raises(
             TypeError,
             match='type of argument "path" must be one .*; got NoneType instead',
         ):
-            _ = func(None)  # type: ignore
+            _ = _uses_pathlike(None)  # type: ignore
