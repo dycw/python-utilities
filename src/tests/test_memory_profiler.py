2cb608abfa6837cc5b03135cb72442dd912429e6
Derek Wan <d.wan@icloud.com>
2022-05-22 15:01:03 +0800

    No need isinstance

--- src/tests/test_memory_profiler.py
+++ src/tests/test_memory_profiler.py
@@ -2,7 +2,6 @@ from hypothesis import given
 from hypothesis.strategies import integers
 from typeguard import typeguard_ignore
 
-from dycw_utilities.memory_profiler import Output
 from dycw_utilities.memory_profiler import memory_profiled
 
 
@@ -12,10 +11,9 @@ def func(n: int, /) -> list[int]:
 
 
 class TestMemoryProfiled:
-    @given(n=integers(1, int(1e6)))
+    @given(n=integers(1, int(1e3)))
     def test_main(self, n: int) -> None:
         profiled = memory_profiled(func)
         result = profiled(n)
-        assert isinstance(result, Output)
         assert result.value == list(range(n))
         assert result.memory <= (80.0 * n)
