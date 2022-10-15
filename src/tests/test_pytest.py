01181bd208e94317e958c063526876f416019783
Derek Wan <d.wan@icloud.com>
2022-10-15 21:05:34 +0800

    Add `hatch`, `os` and `subprocess`

--- src/tests/test_pytest.py
+++ src/tests/test_pytest.py
@@ -1,4 +1,4 @@
-from tempenv import TemporaryEnvironment
+from os import environ
 
 from utilities.pytest import is_pytest
 
@@ -8,5 +8,5 @@ class TestIsPytest:
         assert is_pytest()
 
     def test_disable(self) -> None:
-        with TemporaryEnvironment({"PYTEST_CURRENT_TEST": None}):
-            assert not is_pytest()
+        del environ["PYTEST_CURRENT_TEST"]
+        assert not is_pytest()
