b89628e9107e570cbe9bb9a204d9be2b5ebdf12f
Derek Wan <d.wan@icloud.com>
2022-11-10 08:17:54 +0800

    Add `get_dialect`, `yield_connection` and `yield_in_clause_rows` (#67)

--- src/tests/test_sqlalchemy.py
+++ src/tests/test_sqlalchemy.py
@@ -3,10 +3,15 @@ from typing import Any
 from typing import cast
 
 from hypothesis import given
+from hypothesis.strategies import DataObject
+from hypothesis.strategies import data
 from hypothesis.strategies import fixed_dictionaries
 from hypothesis.strategies import integers
 from hypothesis.strategies import lists
 from hypothesis.strategies import none
+from hypothesis.strategies import sampled_from
+from hypothesis.strategies import sets
+from hypothesis_sqlalchemy.sample import table_records_lists
 from pytest import mark
 from pytest import param
 from pytest import raises
@@ -14,7 +19,11 @@ from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import MetaData
 from sqlalchemy import Table
+from sqlalchemy import create_engine as _create_engine
+from sqlalchemy import func
+from sqlalchemy import insert
 from sqlalchemy import select
+from sqlalchemy.engine import Connection
 from sqlalchemy.engine import Engine
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import declarative_base
@@ -28,8 +37,11 @@ from utilities.sqlalchemy import ensure_table_created
 from utilities.sqlalchemy import ensure_table_dropped
 from utilities.sqlalchemy import get_column_names
 from utilities.sqlalchemy import get_columns
+from utilities.sqlalchemy import get_dialect
 from utilities.sqlalchemy import get_table
 from utilities.sqlalchemy import get_table_name
+from utilities.sqlalchemy import yield_connection
+from utilities.sqlalchemy import yield_in_clause_rows
 
 
 class TestColumnwiseMinMax:
@@ -102,6 +114,26 @@ class TestCreateEngine:
     def test_main(self, temp_path: Path) -> None:
         engine = create_engine("sqlite", database=temp_path.name)
         assert isinstance(engine, Engine)
+
+
+class TestGetDialect:
+    @given(engine=sqlite_engines())
+    def test_sqlite(self, engine: Engine) -> None:
+        assert get_dialect(engine) == "sqlite"
+
+    @mark.parametrize(
+        ["url", "expected"],
+        [
+            param("mssql+pyodbc://scott:tiger@mydsn", "mssql"),
+            param("mysql://scott:tiger@localhost/foo", "mysql"),
+            param("oracle://scott:tiger@127.0.0.1:1521/sidname", "oracle"),
+            param(
+                "postgresql://scott:tiger@localhost/mydatabase", "postgresql"
+            ),
+        ],
+    )
+    def test_non_sqlite(self, url: str, expected: str) -> None:
+        assert get_dialect(_create_engine(url)) == expected
 
 
 class TestEnsureTableCreated:
@@ -219,7 +251,9 @@ class TestGetColumns:
 
 class TestGetTable:
     def test_core(self) -> None:
-        table = Table("example", MetaData(), Column("id", primary_key=True))
+        table = Table(
+            "example", MetaData(), Column("id", Integer, primary_key=True)
+        )
         result = get_table(table)
         assert result is table
 
@@ -236,7 +270,9 @@ class TestGetTable:
 
 class TestGetTableName:
     def test_core(self) -> None:
-        table = Table("example", MetaData(), Column("id", primary_key=True))
+        table = Table(
+            "example", MetaData(), Column("id", Integer, primary_key=True)
+        )
         result = get_table_name(table)
         expected = "example"
         assert result == expected
@@ -250,3 +286,50 @@ class TestGetTableName:
         result = get_table_name(Example)
         expected = "example"
         assert result == expected
+
+
+class TestYieldConnection:
+    @given(engine=sqlite_engines())
+    def test_engine(self, engine: Engine) -> None:
+        with yield_connection(engine) as conn:
+            assert isinstance(conn, Connection)
+
+    @given(engine=sqlite_engines())
+    def test_connection(self, engine: Engine) -> None:
+        with engine.begin() as conn1, yield_connection(conn1) as conn2:
+            assert isinstance(conn2, Connection)
+
+
+class TestYieldInClauseRows:
+    @given(
+        data=data(),
+        engine=sqlite_engines(),
+        chunk_size=integers(1, 10) | none(),
+    )
+    def test_main(
+        self, data: DataObject, engine: Engine, chunk_size: int | None
+    ) -> None:
+        table = Table(
+            "example", MetaData(), Column("id", Integer, primary_key=True)
+        )
+        rows = data.draw(table_records_lists(table, min_size=1))
+        num_rows = len(rows)
+        with engine.begin() as conn:
+            table.create(conn)
+            _ = conn.execute(insert(table).values(rows))
+            assert (
+                conn.execute(select([func.count()]).select_from(table)).scalar()
+                == num_rows
+            )
+        row_vals = [row[0] for row in rows]
+        values = data.draw(sets(sampled_from(row_vals)))
+        result = list(
+            yield_in_clause_rows(
+                select(table.c.id),
+                table.c.id,
+                values,
+                engine,
+                chunk_size=chunk_size,
+            )
+        )
+        assert len(result) == len(values)
--- src/utilities/sqlalchemy/__init__.py
+++ src/utilities/sqlalchemy/__init__.py
@@ -1,21 +1,36 @@
 from collections.abc import Callable
+from collections.abc import Iterable
+from collections.abc import Iterator
+from contextlib import contextmanager
 from functools import reduce
 from operator import ge
 from operator import le
 from re import search
 from typing import Any
+from typing import Literal
+from typing import cast
 
 from beartype import beartype
 from sqlalchemy import Table
 from sqlalchemy import and_
 from sqlalchemy import case
 from sqlalchemy import create_engine as _create_engine
+from sqlalchemy.dialects.mssql import dialect as mssql_dialect
+from sqlalchemy.dialects.mysql import dialect as mysql_dialect
+from sqlalchemy.dialects.oracle import dialect as oracle_dialect
+from sqlalchemy.dialects.postgresql import dialect as postgresql_dialect
+from sqlalchemy.dialects.sqlite import dialect as sqlite_dialect
 from sqlalchemy.engine import URL
+from sqlalchemy.engine import Connection
 from sqlalchemy.engine import Engine
 from sqlalchemy.exc import DatabaseError
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.pool import NullPool
 from sqlalchemy.pool import Pool
+from sqlalchemy.sql import Selectable
+
+from utilities.itertools import chunked
+from utilities.typing import never
 
 
 @beartype
@@ -86,6 +101,31 @@ def create_engine(
         database=database,
     )
     return _create_engine(url, future=True, poolclass=poolclass)
+
+
+Dialect = Literal["mssql", "mysql", "oracle", "postgresql", "sqlite"]
+
+
+@beartype
+def get_dialect(engine_or_conn: Engine | Connection, /) -> Dialect:
+    """Get the dialect of a database."""
+
+    if isinstance(dialect := engine_or_conn.dialect, mssql_dialect):
+        return "mssql"
+    elif isinstance(dialect, mysql_dialect):
+        return "mysql"
+    elif isinstance(dialect, oracle_dialect):
+        return "oracle"
+    elif isinstance(dialect, postgresql_dialect):
+        return "postgresql"
+    elif isinstance(dialect, sqlite_dialect):
+        return "sqlite"
+    else:  # pragma: no cover
+        raise UnsupportedDialect(f"{dialect=}")
+
+
+class UnsupportedDialect(TypeError):
+    """Raised when an unsupported dialect is encountered."""
 
 
 @beartype
@@ -161,3 +201,54 @@ def get_table_name(table_or_model: Any, /) -> str:
     """Get the table name from a ORM model."""
 
     return get_table(table_or_model).name
+
+
+@contextmanager
+@beartype
+def yield_connection(
+    engine_or_conn: Engine | Connection, /
+) -> Iterator[Connection]:
+    """Yield a connection."""
+
+    if isinstance(engine_or_conn, Engine):
+        with engine_or_conn.begin() as conn:
+            yield conn
+    else:
+        yield engine_or_conn
+
+
+@beartype
+def yield_in_clause_rows(
+    sel: Selectable,
+    column: Any,
+    values: Iterable[Any],
+    engine_or_conn: Engine | Connection,
+    /,
+    *,
+    chunk_size: int | None = None,
+    frac: float = 0.95,
+) -> Iterator[Any]:
+    """Yield the rows from an `in` clause."""
+
+    if chunk_size is None:
+        dialect = get_dialect(engine_or_conn)
+        if (dialect) == "mssql":  # pragma: no cover
+            max_params = 2100
+        elif dialect == "mysql":  # pragma: no cover
+            max_params = 65535
+        elif dialect == "oracle":  # pragma: no cover
+            max_params = 1000
+        elif dialect == "postgresql":  # pragma: no cover
+            max_params = 32767
+        elif dialect == "sqlite":
+            max_params = 100
+        else:
+            return never(dialect)  # pragma: no cover
+        chunk_size_use = round(frac * max_params)
+    else:
+        chunk_size_use = chunk_size
+    value_chunks = chunked(values, chunk_size_use)
+    with yield_connection(engine_or_conn) as conn:
+        for values_i in value_chunks:
+            sel_i: Selectable = cast(Any, sel).where(column.in_(values_i))
+            yield from conn.execute(sel_i).all()
