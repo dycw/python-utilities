32d02d9269c0b90f127486436123912f908a7399
Derek Wan <d.wan@icloud.com>
2023-02-04 19:35:50 +0800

    Add `check_duplicates` (#82)

--- src/tests/test_iterables.py
+++ src/tests/test_iterables.py
@@ -1,8 +1,38 @@
+from itertools import chain
 from typing import Any
 
-from pytest import mark, param
+from hypothesis import given
+from hypothesis.strategies import (
+    DataObject,
+    data,
+    integers,
+    lists,
+    sampled_from,
+    sets,
+)
+from pytest import mark, param, raises
 
-from utilities.iterables import is_iterable_not_str
+from utilities.iterables import (
+    DuplicatedError,
+    check_duplicates,
+    is_iterable_not_str,
+)
+
+
+class TestCheckDuplicates:
+    @given(x=sets(integers()))
+    def test_main(self, x: set[int]) -> None:
+        check_duplicates(x)
+
+    @given(data=data(), x=lists(integers(), min_size=1))
+    def test_error(self, data: DataObject, x: list[int]) -> None:
+        x_i = data.draw(sampled_from(x))
+        y = chain(x, [x_i])
+        with raises(
+            DuplicatedError,
+            match=r"Iterable contains duplicates: {.+}",
+        ):
+            check_duplicates(y)
 
 
 class TestIsIterableNotStr:
