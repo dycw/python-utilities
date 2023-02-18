c91e23751a401cf7a44f34b31054386700378c87
Derek Wan <d.wan@icloud.com>
2023-02-18 15:38:17 +0800

    Add `sqlalchemy` table comparisons (#101)

--- src/tests/test_types.py
+++ src/tests/test_types.py
@@ -1,8 +1,43 @@
+from typing import Any
+
 from beartype.door import die_if_unbearable
 from beartype.roar import BeartypeAbbyHintViolation
 from pytest import mark, param, raises
 
-from utilities.types import NoneType, Number
+from utilities.types import (
+    NoneType,
+    Number,
+    ensure_class,
+    issubclass_except_bool_int,
+)
+
+
+class TestEnsureClass:
+    @mark.parametrize(
+        ("x", "expected"),
+        [param(None, NoneType), param(NoneType, NoneType)],
+    )
+    def test_main(self, x: Any, expected: type[Any]) -> None:
+        assert ensure_class(x) is expected
+
+
+class TestIsSubclassExceptBoolInt:
+    @mark.parametrize(
+        ("x", "y", "expected"),
+        [
+            param(bool, bool, True),
+            param(bool, int, False),
+            param(int, int, True),
+        ],
+    )
+    def test_main(self, x: type[Any], y: type[Any], expected: bool) -> None:
+        assert issubclass_except_bool_int(x, y) is expected
+
+    def test_subclass_of_int(self) -> None:
+        class MyInt(int):
+            ...
+
+        assert not issubclass_except_bool_int(bool, MyInt)
 
 
 class TestNoneType:
