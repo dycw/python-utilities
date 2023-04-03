ff3c38c38f323f2556a30076c2e535f0db754688
Derek Wan <d.wan@icloud.com>
2023-04-03 14:15:09 +0900

    Upgrade to `semver-3` (#193)

--- src/tests/luigi/test_semver.py
+++ src/tests/luigi/test_semver.py
@@ -1,12 +1,12 @@
 from hypothesis import given
 from semver import VersionInfo
 
-from utilities.hypothesis.semver import version_infos
+from utilities.hypothesis.semver import versions
 from utilities.luigi.semver import VersionParameter
 
 
 class TestVersionParameter:
-    @given(version=version_infos())
+    @given(version=versions())
     def test_main(self, version: VersionInfo) -> None:
         param = VersionParameter()
         norm = param.normalize(version)
--- src/utilities/semver.py
+++ src/utilities/semver.py
@@ -1,12 +1,10 @@
 from typing import Union
 
 from beartype import beartype
-from semver import VersionInfo
+from semver import Version
 
 
 @beartype
-def ensure_version_info(version: Union[VersionInfo, str], /) -> VersionInfo:
-    """Ensure the object is a `VersionInfo`."""
-    if isinstance(version, VersionInfo):
-        return version
-    return VersionInfo.parse(version)
+def ensure_version(version: Union[Version, str], /) -> Version:
+    """Ensure the object is a `Version`."""
+    return version if isinstance(version, Version) else Version.parse(version)
