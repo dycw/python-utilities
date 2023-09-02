015ebacdf52db5fabc2a4652295adc4aee206f7a
Derek Wan <d.wan@icloud.com>
2023-09-02 18:37:35 +0800

    Try 3.10

--- src/utilities/enum.py
+++ src/utilities/enum.py
@@ -9,7 +9,9 @@ from typing_extensions import override
 from utilities.sys import PYTHON_AT_LEAST_3_11
 
 if PYTHON_AT_LEAST_3_11:  # pragma: version-le-310
-    from enum import StrEnum as _StrEnum  # type: ignore
+    from enum import (  # pragma: version-le-310
+        StrEnum as _StrEnum,  # type: ignore
+    )
 else:  # pragma: version-ge-311
 
     class _StrEnum(str, Enum):
