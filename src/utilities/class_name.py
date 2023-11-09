f55dfa57d61b654ec3dfafc79e3d367e04443101
Derek Wan <d.wan@icloud.com>
2023-11-09 22:53:45 +0800

    Add __all__ (#291)

--- src/utilities/class_name.py
+++ src/utilities/class_name.py
@@ -9,3 +9,8 @@ def get_class_name(x: Any, /, *, snake: bool = False) -> str:
     """Get the name of a class."""
     cls_name = (x if isinstance(x, type) else type(x)).__name__
     return snake_case(cls_name) if snake else cls_name
+
+
+__all__ = [
+    "get_class_name",
+]
