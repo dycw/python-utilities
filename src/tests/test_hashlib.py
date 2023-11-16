f9e202ffbefc9bf774c1264a589ec5fb80da94e8
Derek Wan <d.wan@icloud.com>
2023-11-17 06:58:42 +0800

    Have `md5_hash` sort the elements for increased determinism (#312)

--- src/tests/test_hashlib.py
+++ src/tests/test_hashlib.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import Any
 
+from hypothesis import given
+from hypothesis.strategies import dictionaries, frozensets, integers, sets
 from pytest import mark, param
 
 from utilities.hashlib import md5_hash
@@ -18,5 +20,32 @@ class TestMD5Hash:
             param(None, "7d1e55650014e21b4568ed3e3d1fc531"),
         ],
     )
-    def test_main(self, obj: Any, expected: str) -> None:
+    def test_main(self, *, obj: Any, expected: str) -> None:
         assert md5_hash(obj) == expected
+
+    @given(
+        x=dictionaries(integers(0, 10), integers(0, 10), max_size=2),
+        y=dictionaries(integers(0, 10), integers(0, 10), max_size=2),
+    )
+    def test_dicts(self, *, x: dict[int, int], y: dict[int, int]) -> None:
+        res = md5_hash(x) == md5_hash(y)
+        expected = x == y
+        assert res is expected
+
+    @given(
+        x=sets(integers(0, 10), max_size=2),
+        y=sets(integers(0, 10), max_size=2),
+    )
+    def test_sets(self, *, x: set[int], y: set[int]) -> None:
+        res = md5_hash(x) == md5_hash(y)
+        expected = x == y
+        assert res is expected
+
+    @given(
+        x=frozensets(integers(0, 10), max_size=2),
+        y=frozensets(integers(0, 10), max_size=2),
+    )
+    def test_frozensets(self, *, x: frozenset[int], y: frozenset[int]) -> None:
+        res = md5_hash(x) == md5_hash(y)
+        expected = x == y
+        assert res is expected
--- src/utilities/hashlib.py
+++ src/utilities/hashlib.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from hashlib import md5
+from operator import itemgetter
 from pickle import dumps
 from typing import Any
 
@@ -11,6 +12,10 @@ def md5_hash(obj: Any, /) -> str:
         return md5(obj, usedforsecurity=False).hexdigest()
     if isinstance(obj, str):
         return md5_hash(obj.encode())
+    if isinstance(obj, dict):
+        return md5_hash(sorted(obj.items(), key=itemgetter(0)))
+    if isinstance(obj, set | frozenset):
+        return md5_hash(sorted(obj))
     return md5_hash(dumps(obj))
 
 
