29484c25b57af75da857baf8d0dedf09978dfa24
Derek Wan <d.wan@icloud.com>
2023-11-18 00:43:29 +0800

    Use `skipif_not_*` marks for brevity (#314)

--- src/tests/test_xlrd.py
+++ src/tests/test_xlrd.py
@@ -5,13 +5,8 @@ import datetime as dt
 from pytest import mark, param, raises
 
 from utilities.datetime import UTC
-from utilities.platform import (
-    IS_NOT_MAC,
-    IS_NOT_WINDOWS,
-    SYSTEM,
-    System,
-    UnsupportedSystemError,
-)
+from utilities.platform import SYSTEM, System, UnsupportedSystemError
+from utilities.pytest import skipif_not_mac, skipif_not_windows
 from utilities.xlrd import _get_date_mode, to_date, to_datetime
 
 
@@ -28,36 +23,12 @@ class TestToDate:
     @mark.parametrize(
         ("date", "expected"),
         [
-            param(
-                0.0,
-                dt.date(1899, 12, 31),
-                marks=mark.skipif(IS_NOT_WINDOWS, reason="Windows only"),
-            ),
-            param(
-                0.5,
-                dt.date(1899, 12, 31),
-                marks=mark.skipif(IS_NOT_WINDOWS, reason="Windows only"),
-            ),
-            param(
-                1.0,
-                dt.date(1900, 1, 1),
-                marks=mark.skipif(IS_NOT_WINDOWS, reason="Windows only"),
-            ),
-            param(
-                0.0,
-                dt.date(1904, 1, 1),
-                marks=mark.skipif(IS_NOT_MAC, reason="Mac only"),
-            ),
-            param(
-                0.5,
-                dt.date(1904, 1, 1),
-                marks=mark.skipif(IS_NOT_MAC, reason="Mac only"),
-            ),
-            param(
-                1.0,
-                dt.date(1904, 1, 2),
-                marks=mark.skipif(IS_NOT_MAC, reason="Mac only"),
-            ),
+            param(0.0, dt.date(1899, 12, 31), marks=skipif_not_windows),
+            param(0.5, dt.date(1899, 12, 31), marks=skipif_not_windows),
+            param(1.0, dt.date(1900, 1, 1), marks=skipif_not_windows),
+            param(0.0, dt.date(1904, 1, 1), marks=skipif_not_mac),
+            param(0.5, dt.date(1904, 1, 1), marks=skipif_not_mac),
+            param(1.0, dt.date(1904, 1, 2), marks=skipif_not_mac),
         ],
     )
     def test_main(self, *, date: float, expected: dt.date) -> None:
@@ -68,36 +39,14 @@ class TestToDatetime:
     @mark.parametrize(
         ("date", "expected"),
         [
+            param(0.0, dt.datetime(1899, 12, 31, tzinfo=UTC), marks=skipif_not_windows),
             param(
-                0.0,
-                dt.datetime(1899, 12, 31, tzinfo=UTC),
-                marks=mark.skipif(IS_NOT_WINDOWS, reason="Windows only"),
+                0.5, dt.datetime(1899, 12, 31, 12, tzinfo=UTC), marks=skipif_not_windows
             ),
-            param(
-                0.5,
-                dt.datetime(1899, 12, 31, 12, tzinfo=UTC),
-                marks=mark.skipif(IS_NOT_WINDOWS, reason="Windows only"),
-            ),
-            param(
-                1.0,
-                dt.datetime(1900, 1, 1, tzinfo=UTC),
-                marks=mark.skipif(IS_NOT_WINDOWS, reason="Windows only"),
-            ),
-            param(
-                0.0,
-                dt.datetime(1904, 1, 1, tzinfo=UTC),
-                marks=mark.skipif(IS_NOT_MAC, reason="Mac only"),
-            ),
-            param(
-                0.5,
-                dt.datetime(1904, 1, 1, 12, tzinfo=UTC),
-                marks=mark.skipif(IS_NOT_MAC, reason="Mac only"),
-            ),
-            param(
-                1.0,
-                dt.datetime(1904, 1, 2, tzinfo=UTC),
-                marks=mark.skipif(IS_NOT_MAC, reason="Mac only"),
-            ),
+            param(1.0, dt.datetime(1900, 1, 1, tzinfo=UTC), marks=skipif_not_windows),
+            param(0.0, dt.datetime(1904, 1, 1, tzinfo=UTC), marks=skipif_not_mac),
+            param(0.5, dt.datetime(1904, 1, 1, 12, tzinfo=UTC), marks=skipif_not_mac),
+            param(1.0, dt.datetime(1904, 1, 2, tzinfo=UTC), marks=skipif_not_mac),
         ],
     )
     def test_main(self, *, date: float, expected: dt.datetime) -> None:
