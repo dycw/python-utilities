2c96c90335bbc16087208d55026c3c0a13a82470
Derek Wan <d.wan@icloud.com>
2023-11-22 07:07:00 +0800

    Add `save/load_model`, for `pydantic` (#326)

--- src/tests/test_pydantic.py
+++ src/tests/test_pydantic.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 from hypothesis import given
 from hypothesis.strategies import integers
+from pydantic import BaseModel
 
-from utilities.pydantic import HashableBaseModel
+from utilities.hypothesis import temp_paths
+from utilities.pydantic import HashableBaseModel, load_model, save_model
 
 
 class TestHashableBaseModel:
@@ -14,3 +18,17 @@ class TestHashableBaseModel:
 
         example = Example(x=x)
         assert isinstance(hash(example), int)
+
+
+class TestSaveAndLoadModel:
+    @given(x=integers(), root=temp_paths())
+    def test_main(self, *, x: int, root: Path) -> None:
+        path = root.joinpath("model.gz")
+
+        class Example(BaseModel):
+            x: int
+
+        example = Example(x=x)
+        save_model(example, path)
+        loaded = load_model(Example, path)
+        assert loaded == example
