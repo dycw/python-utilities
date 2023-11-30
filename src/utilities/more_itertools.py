fdc3d7683d19b60fc0a1d421b89cceb6554a6081
Derek Wan <d.wan@icloud.com>
2023-11-30 23:20:50 +0800

    Re-add `always_iterable` (#347)

--- src/utilities/more_itertools.py
+++ src/utilities/more_itertools.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from collections.abc import Iterable
+from collections.abc import Iterable, Iterator
 from re import escape
-from typing import TypeVar
+from typing import Any, TypeVar
 
+from more_itertools import always_iterable as _always_iterable
 from more_itertools import one as _one
 
 from utilities.errors import redirect_error
@@ -11,8 +12,18 @@ from utilities.errors import redirect_error
 _T = TypeVar("_T")
 
 
+def always_iterable(
+    obj: _T | Iterable[_T],
+    /,
+    *,
+    base_type: type[Any] | tuple[type[Any], ...] | None = (str, bytes),
+) -> Iterator[_T]:
+    """Typed version of `always_iterable`."""
+    return _always_iterable(obj, base_type=base_type)
+
+
 def one(iterable: Iterable[_T], /) -> _T:
-    """Return the only item from iterable."""
+    """Custom version of `one` with separate exceptions."""
     try:
         return _one(iterable)
     except ValueError as error:
@@ -40,4 +51,4 @@ class OneNonUniqueError(OneError):
     ...
 
 
-__all__ = ["OneEmptyError", "OneError", "OneNonUniqueError", "one"]
+__all__ = ["OneEmptyError", "OneError", "OneNonUniqueError", "always_iterable", "one"]
