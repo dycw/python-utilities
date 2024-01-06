e0330bc7c2cbf7e29284dceaf9011382f4d5588c
Derek Wan <d.wan@icloud.com>
2024-01-06 10:43:40 +0800

    Rename to `beartype_if_dev`, support `ipython` and `jupyter` (#392)

--- src/utilities/sys.py
+++ src/utilities/sys.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
-from sys import version_info
+from sys import modules, version_info
 
 VERSION_MAJOR_MINOR = (version_info.major, version_info.minor)
 
 
-__all__ = ["VERSION_MAJOR_MINOR"]
+def is_pytest() -> bool:
+    """Check if `pytest` is running."""
+
+    return "pytest" in modules
+
+
+__all__ = ["is_pytest", "VERSION_MAJOR_MINOR"]
