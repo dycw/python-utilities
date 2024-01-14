1af99c74a98b58622dcc5fe30057fbb353158b91
Derek Wan <d.wan@icloud.com>
2024-01-14 10:55:35 +0800

    Improve error messages (`humps`) (#406)

--- src/utilities/humps.py
+++ src/utilities/humps.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+from collections.abc import Hashable
+from dataclasses import dataclass
 from re import search
 
-from bidict import ValueDuplicationError, bidict
+from bidict import bidict
 from humps import decamelize
+from typing_extensions import override
 
-from utilities.errors import redirect_error
 from utilities.iterables import CheckDuplicatesError, check_duplicates
 from utilities.types import IterableStrs
 
@@ -22,20 +24,45 @@ def snake_case(text: str, /) -> str:
 def snake_case_mappings(text: IterableStrs, /) -> bidict[str, str]:
     """Map a set of text into their snake cases."""
 
-    text_as_list = list(text)
-    with redirect_error(
-        CheckDuplicatesError, SnakeCaseMappingsError(f"{text_as_list=}")
-    ):
-        check_duplicates(text_as_list)
+    keys = list(text)
+    try:
+        check_duplicates(keys)
+    except CheckDuplicatesError as error:
+        raise _SnakeCaseMappingsDuplicateKeysError(
+            text=keys, counts=error.counts
+        ) from None
+    values = list(map(snake_case, keys))
+    try:
+        check_duplicates(values)
+    except CheckDuplicatesError as error:
+        raise _SnakeCaseMappingsDuplicateValuesError(
+            text=values, counts=error.counts
+        ) from None
+    return bidict(zip(keys, values, strict=True))
 
-    with redirect_error(
-        ValueDuplicationError, SnakeCaseMappingsError(f"{text_as_list=}")
-    ):
-        return bidict({t: snake_case(t) for t in text_as_list})
 
-
+@dataclass(frozen=True, kw_only=True, slots=True)
 class SnakeCaseMappingsError(Exception):
-    ...
+    text: list[str]
+    counts: dict[Hashable, int]
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class _SnakeCaseMappingsDuplicateKeysError(SnakeCaseMappingsError):
+    @override
+    def __str__(self) -> str:
+        return "Strings {} must not contain duplicates; got {}".format(
+            self.text, self.counts
+        )
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class _SnakeCaseMappingsDuplicateValuesError(SnakeCaseMappingsError):
+    @override
+    def __str__(self) -> str:
+        return "Snake-cased strings {} must not contain duplicates; got {}".format(
+            self.text, self.counts
+        )
 
 
 __all__ = ["SnakeCaseMappingsError", "snake_case", "snake_case_mappings"]
