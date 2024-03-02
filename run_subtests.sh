b2095877ad132440dd65a593f86e6005fa1c658f
Derek Wan <d.wan@icloud.com>
2024-03-02 18:55:23 +0800

    more

--- run_subtests.sh
+++ run_subtests.sh
@@ -2,7 +2,7 @@
 
 # for package in ast atomicwrites beartype fastapi more-itertools scipy xlrd; do
 packages=(
-    pandas
+    pathvalidate
 )
 for package in "${packages[@]}"; do
     uv pip sync "requirements/${package}.txt"
