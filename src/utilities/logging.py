b0f52742d929086e09011fd6a16b5207264b763c
Derek Wan <d.wan@icloud.com>
2024-05-10 10:24:29 +0900

    save

--- src/utilities/logging.py
+++ src/utilities/logging.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from enum import Enum, unique
 from logging import basicConfig
+
+from typing_extensions import override
 
 from utilities.datetime import maybe_sub_pct_y
 
@@ -17,6 +20,43 @@ def basic_config(
         style="{",
         level=LogLevel.DEBUG.name,
     )
+
+
+def get_logging_level(level: str, /) -> int:
+    """Get the logging level.
+
+    Hard-coded mapping only needed for Python 3.10.
+    """
+    try:
+        from logging import getLevelNamesMapping  # type: ignore[]
+    except ImportError:
+        mapping = {
+            "CRITICAL": 50,
+            "FATAL": 50,
+            "ERROR": 40,
+            "WARN": 30,
+            "WARNING": 30,
+            "INFO": 20,
+            "DEBUG": 10,
+            "NOTSET": 0,
+            "VERBOSE": 19,
+            "TRACE": 9,
+        }
+    else:
+        mapping = getLevelNamesMapping()
+    try:
+        return mapping[level]
+    except KeyError:
+        raise GetLoggingLevelError(level=level) from None
+
+
+@dataclass(kw_only=True)
+class GetLoggingLevelError(Exception):
+    level: str
+
+    @override
+    def __str__(self) -> str:
+        return f"Logging level {self.level!r} must be valid"
 
 
 @unique
