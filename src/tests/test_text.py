f17a73cc84c4ee18793c7a5bf7dad52fc083904a
Derek Wan <d.wan@icloud.com>
2024-05-11 22:26:22 +0900

    Extend `ensure_str` with nullability

--- src/tests/test_text.py
+++ src/tests/test_text.py
@@ -1,17 +1,28 @@
 from __future__ import annotations
 
-from pytest import raises
+from pytest import mark, param, raises
 
+from utilities.sentinel import sentinel
 from utilities.text import EnsureStrError, ensure_str, strip_and_dedent
 
 
 class TestEnsureStr:
-    def test_main(self) -> None:
-        assert isinstance(ensure_str(""), str)
+    @mark.parametrize(
+        ("obj", "nullable"), [param("", False), param("", True), param(None, True)]
+    )
+    def test_main(self, *, obj: bool | None, nullable: bool) -> None:
+        _ = ensure_str(obj, nullable=nullable)
 
-    def test_error(self) -> None:
-        with raises(EnsureStrError, match="Object .* must be a string; got .* instead"):
-            _ = ensure_str(None)
+    @mark.parametrize(
+        ("nullable", "match"),
+        [
+            param(False, "Object .* must be a string"),
+            param(True, "Object .* must be a string or None"),
+        ],
+    )
+    def test_error(self, *, nullable: bool, match: str) -> None:
+        with raises(EnsureStrError, match=f"{match}; got .* instead"):
+            _ = ensure_str(sentinel, nullable=nullable)
 
 
 class TestStripAndDedent:
--- src/utilities/text.py
+++ src/utilities/text.py
@@ -2,28 +2,34 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from textwrap import dedent
-from typing import Any
+from typing import Any, Literal, overload
 
 from typing_extensions import override
 
 from utilities.types import EnsureClassError, ensure_class, get_class_name
 
 
-def ensure_str(obj: Any, /) -> str:
+@overload
+def ensure_str(obj: Any, /, *, nullable: bool) -> str | None: ...
+@overload
+def ensure_str(obj: Any, /, *, nullable: Literal[False] = False) -> str: ...
+def ensure_str(obj: Any, /, *, nullable: bool = False) -> str | None:
     """Ensure an object is a string."""
     try:
-        return ensure_class(obj, str)
+        return ensure_class(obj, str, nullable=nullable)
     except EnsureClassError as error:
-        raise EnsureStrError(obj=error.obj) from None
+        raise EnsureStrError(obj=error.obj, nullable=nullable) from None
 
 
 @dataclass(kw_only=True)
 class EnsureStrError(Exception):
     obj: Any
+    nullable: bool
 
     @override
     def __str__(self) -> str:
-        return f"Object {self.obj} must be a string; got {get_class_name(self.obj)} instead"
+        desc = " or None" if self.nullable else ""
+        return f"Object {self.obj} must be a string{desc}; got {get_class_name(self.obj)} instead"
 
 
 def strip_and_dedent(text: str, /) -> str:
