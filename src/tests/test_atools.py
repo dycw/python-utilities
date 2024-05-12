df23afa2701b261075b806ae6e6bcb832559f7db
Derek Wan <d.wan@icloud.com>
2024-05-12 19:35:00 +0900

    Add `refresh_memoized` (#504)

--- src/tests/test_atools.py
+++ src/tests/test_atools.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 from asyncio import sleep
 
-from pytest import mark
+from pytest import mark, raises
 
-from utilities.atools import memoize
+from utilities.atools import RefreshMemoizedError, memoize, refresh_memoized
 
 
 class TestMemoize:
@@ -18,10 +18,8 @@ class TestMemoize:
             i += 1
             return i
 
-        assert i == 0
         for _ in range(2):
             assert (await increment()) == 1
-            assert i == 1
 
     @mark.asyncio
     async def test_with_duration(self) -> None:
@@ -33,11 +31,37 @@ class TestMemoize:
             i += 1
             return i
 
-        assert i == 0
         for _ in range(2):
             assert (await increment()) == 1
-            assert i == 1
         await sleep(1)
         for _ in range(2):
             assert (await increment()) == 2
-            assert i == 2
+
+
+class TestRefreshMemoized:
+    @mark.asyncio
+    async def test_main(self) -> None:
+        i = 0
+
+        @memoize(duration=1)
+        async def increment() -> int:
+            nonlocal i
+            i += 1
+            return i
+
+        for _ in range(2):
+            assert (await increment()) == 1
+        await sleep(1)
+        for _ in range(2):
+            assert (await increment()) == 2
+        assert await refresh_memoized(increment) == 3
+
+    @mark.asyncio
+    async def test_error(self) -> None:
+        async def none() -> None:
+            return None
+
+        with raises(
+            RefreshMemoizedError, match="Asynchronous function .* must be memoized"
+        ):
+            await refresh_memoized(none)
