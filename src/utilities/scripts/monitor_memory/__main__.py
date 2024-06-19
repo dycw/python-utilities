47262a3b29b02974a16bdbedacac4bdc63fa92d9
Derek Wan <d.wan@icloud.com>
2024-06-19 23:03:29 +0900

    another no cover

--- src/utilities/scripts/monitor_memory/__main__.py
+++ src/utilities/scripts/monitor_memory/__main__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations  # pragma: no cover
 
-from utilities.scripts.monitor_memory import main
+from utilities.scripts.monitor_memory import main  # pragma: no cover
 
 if __name__ == "__main__":  # pragma: no cover
     main()
