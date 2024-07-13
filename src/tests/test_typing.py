b55ea59eda2f2c05a01e57e36c535baf06f12ca1
Derek Wan <d.wan@icloud.com>
2024-07-13 19:34:57 +0900

    more

--- src/tests/test_typing.py
+++ src/tests/test_typing.py
@@ -1,10 +1,25 @@
 from __future__ import annotations
 
-from typing import Literal
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, Literal
 
-from pytest import raises
+from pytest import mark, param, raises
 
-from utilities.typing import GetLiteralArgsError, get_literal_args
+from utilities.typing import (
+    GetLiteralArgsError,
+    get_literal_args,
+    is_dict_type,
+    is_frozenset_type,
+    is_list_type,
+    is_literal_type,
+    is_mapping_type,
+    is_optional_type,
+    is_set_type,
+    is_union_type,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 
 class TestGetLiteralArgs:
@@ -18,6 +33,35 @@ class TestGetLiteralArgs:
         union = int | str
         with raises(
             GetLiteralArgsError,
-            match="Object .* must be a Literal; got origin .* instead",
+            match="Object .* must be a Literal annotation; got int | str instead",
         ):
             _ = get_literal_args(union)
+
+
+class TestIsAnnotationOfType:
+    @mark.parametrize(
+        ("func", "obj", "expected"),
+        [
+            param(is_dict_type, dict[int, int], True),
+            param(is_dict_type, list[int], False),
+            param(is_frozenset_type, frozenset[int], True),
+            param(is_frozenset_type, list[int], False),
+            param(is_list_type, list[int], True),
+            param(is_list_type, set[int], False),
+            param(is_mapping_type, Mapping[int, int], True),
+            param(is_mapping_type, list[int], False),
+            param(is_literal_type, Literal["a", "b", "c"], True),
+            param(is_literal_type, list[int], False),
+            param(is_optional_type, int | None, True),
+            param(is_optional_type, int | str, False),
+            param(is_optional_type, list[int], False),
+            param(is_set_type, set[int], True),
+            param(is_set_type, list[int], False),
+            param(is_union_type, int | str, True),
+            param(is_union_type, list[int], False),
+        ],
+    )
+    def test_main(
+        self, *, func: Callable[[Any], bool], obj: Any, expected: bool
+    ) -> None:
+        assert func(obj) is expected
--- src/utilities/typing.py
+++ src/utilities/typing.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from collections.abc import Mapping
 from dataclasses import dataclass
-from types import UnionType
-from typing import Any, Literal, get_args, get_origin
+from types import NoneType, UnionType
+from typing import Any, Literal, get_origin
+from typing import get_args as _get_args
 
 from typing_extensions import override
 
@@ -12,12 +14,17 @@ except ImportError:  # pragma: no cover
     TypeAliasType = None
 
 
+def get_args(obj: Any, /) -> tuple[Any, ...]:
+    """Get the arguments of an annotation."""
+    if (TypeAliasType is not None) and isinstance(obj, TypeAliasType):
+        return get_args(obj.__value__)
+    return _get_args(obj)
+
+
 def get_literal_args(obj: Any, /) -> tuple[Any, ...]:
     """Get the arguments of a Literal."""
     if not is_literal_type(obj):
         raise GetLiteralArgsError(obj=obj)
-    if (TypeAliasType is not None) and isinstance(obj, TypeAliasType):
-        return get_literal_args(obj.__value__)
     return get_args(obj)
 
 
@@ -27,46 +34,67 @@ class GetLiteralArgsError(Exception):
 
     @override
     def __str__(self) -> str:
-        return f"Object must be a Literal; got {self.obj} instead"
+        return f"Object must be a Literal annotation; got {self.obj} instead"
+
+
+def is_dict_type(obj: Any, /) -> bool:
+    """Check if an object is a dict type annotation."""
+    return _is_annotation_of_type(obj, dict)
 
 
 def is_frozenset_type(obj: Any, /) -> bool:
-    """Check if an object is a frozenset annotation."""
-    return _is_annotation_with_origin(obj, frozenset)
+    """Check if an object is a frozenset type annotation."""
+    return _is_annotation_of_type(obj, frozenset)
 
 
 def is_list_type(obj: Any, /) -> bool:
-    """Check if an object is a list annotation."""
-    return _is_annotation_with_origin(obj, list)
+    """Check if an object is a list type annotation."""
+    return _is_annotation_of_type(obj, list)
 
 
 def is_literal_type(obj: Any, /) -> bool:
-    """Check if an object is a Literal annotation."""
-    return _is_annotation_with_origin(obj, Literal)
+    """Check if an object is a literal type annotation."""
+    return _is_annotation_of_type(obj, Literal)
+
+
+def is_mapping_type(obj: Any, /) -> bool:
+    """Check if an object is a mapping type annotation."""
+    return _is_annotation_of_type(obj, Mapping)
+
+
+def is_optional_type(obj: Any, /) -> bool:
+    """Check if an object is an optional type annotation."""
+    return is_union_type(obj) and any(a is NoneType for a in get_args(obj))
 
 
 def is_set_type(obj: Any, /) -> bool:
-    """Check if an object is a set annotation."""
-    return _is_annotation_with_origin(obj, set)
+    """Check if an object is a set type annotation."""
+    return _is_annotation_of_type(obj, set)
 
 
 def is_union_type(obj: Any, /) -> bool:
-    """Check if an object is a union type."""
-    return _is_annotation_with_origin(obj, UnionType)
+    """Check if an object is a union type annotation."""
+    return _is_annotation_of_type(obj, UnionType)
 
 
-def _is_annotation_with_origin(obj: Any, origin: Any, /) -> bool:
+def _is_annotation_of_type(obj: Any, origin: Any, /) -> bool:
     """Check if an object is an annotation with a given origin."""
     return (get_origin(obj) is origin) or (
         (TypeAliasType is not None)
         and isinstance(obj, TypeAliasType)
-        and _is_annotation_with_origin(obj.__value__, origin)
+        and _is_annotation_of_type(obj.__value__, origin)
     )
 
 
 __all__ = [
     "GetLiteralArgsError",
     "get_literal_args",
+    "is_dict_type",
     "is_frozenset_type",
+    "is_list_type",
     "is_literal_type",
+    "is_mapping_type",
+    "is_optional_type",
+    "is_set_type",
+    "is_union_type",
 ]
