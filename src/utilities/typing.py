0177630f8ecd2434972fd7034e7e9ddd92352c36
Derek Wan <d.wan@icloud.com>
2024-07-13 14:02:23 +0900

    Extend `get_literal_args` to support 3.12 type aliases (#574)

--- src/utilities/typing.py
+++ src/utilities/typing.py
@@ -5,9 +5,18 @@ from typing import Any, Literal, get_args, get_origin
 
 from typing_extensions import override
 
+try:  # pragma: version-ge-312
+    from typing import TypeAliasType  # type: ignore[reportAttributeAccessIssue]
+except ImportError:  # pragma: no cover
+    TypeAliasType = None
+
 
 def get_literal_args(obj: Any, /) -> tuple[Any, ...]:
     """Get the arguments of a Literal."""
+    if (TypeAliasType is not None) and isinstance(  # pragma: version-ge-312
+        obj, TypeAliasType
+    ):
+        return get_literal_args(obj.__value__)  # pragma: no cover
     if (origin := get_origin(obj)) is not Literal:
         raise GetLiteralArgsError(obj=obj, origin=origin)
     return get_args(obj)
