cd0c412409e9cb2dec5db55a36872e7d456768b7
Derek Wan <d.wan@icloud.com>
2024-07-29 17:21:19 +0800

    more

--- src/tests/test_atools.py
+++ src/tests/test_atools.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 from asyncio import sleep
-from typing import TYPE_CHECKING
+from inspect import signature
+from typing import Annotated, Any
 
 from pytest import mark, raises
 
-from utilities.atools import RefreshMemoizedError, memoize, refresh_memoized
-
-if TYPE_CHECKING:
-    from collections.abc import Hashable
+from utilities.atools import (
+    RefreshMemoizedError,
+    _memoize_auto_keygen_is_param,
+    memoize,
+    no_memoize,
+    refresh_memoized,
+)
 
 
 class TestMemoize:
@@ -45,20 +49,43 @@ class TestMemoize:
     async def test_with_keygen(self) -> None:
         i = 0
 
-        def _keygen(*, j: int, _ignore: bool) -> tuple[Hashable, ...]:
-            return (j,)
-
-        @memoize(keygen=_keygen)
-        async def increment(j: int, /, *, _ignore: bool) -> int:
+        @memoize
+        async def increment(j: int, /, *, ignore: Annotated[bool, no_memoize]) -> int:
             nonlocal i
             i += j
-            _ = _ignore
+            _ = ignore
             return i
 
         for j in [True, False]:
-            assert (await increment(1, _ignore=j)) == 1
+            assert (await increment(1, ignore=j)) == 1
         for j in [True, False]:
-            assert (await increment(2, _ignore=j)) == 3
+            assert (await increment(2, ignore=j)) == 3
+
+
+class TestMemoizeAutoKeygenIsParam:
+    def test_no_annotation(self) -> None:
+        def func(a, /) -> Any:  # noqa: ANN001 # pyright: ignore[reportMissingParameterType]
+            return a
+
+        ann = signature(func).parameters["a"].annotation
+        result = _memoize_auto_keygen_is_param(ann)
+        assert result is True
+
+    def test_basic_annotation(self) -> None:
+        def func(a: int, /) -> int:
+            return a
+
+        ann = signature(func).parameters["a"].annotation
+        result = _memoize_auto_keygen_is_param(ann)
+        assert result is True
+
+    def test_no_memoize(self) -> None:
+        def func(a: Annotated[int, no_memoize], /) -> int:
+            return a
+
+        ann = signature(func).parameters["a"].annotation
+        result = _memoize_auto_keygen_is_param(ann)
+        assert result is False
 
 
 class TestRefreshMemoized:
