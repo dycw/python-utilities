2e62cdfffe220b8c226e9f7a55d12800a79c5293
Derek Wan <d.wan@icloud.com>
2024-08-20 02:11:41 +0900

    more

--- src/utilities/functions.py
+++ src/utilities/functions.py
@@ -1,32 +1,8 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import Any, Protocol, TypeVar
-
-from typing_extensions import override
+from typing import TypeVar
 
 _T = TypeVar("_T")
-
-
-class _HasName(Protocol):
-    @property
-    def name(self) -> Any: ...  # pragma: no cover
-
-
-def check_name(obj: _HasName, name: Any, /) -> None:
-    """Check if an object has the required name."""
-    if obj.name != name:
-        raise CheckNameError(obj=obj, name=name)
-
-
-@dataclass(kw_only=True)
-class CheckNameError(Exception):
-    obj: _HasName
-    name: Any
-
-    @override
-    def __str__(self) -> str:
-        return f"Object must have name {self.name}; got {self.obj.name}."
 
 
 def identity(obj: _T, /) -> _T:
@@ -34,4 +10,4 @@ def identity(obj: _T, /) -> _T:
     return obj
 
 
-__all__ = ["CheckNameError", "check_name", "identity"]
+__all__ = ["identity"]
