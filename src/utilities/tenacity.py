54938397e13c9fb356f17e47746d6a56b543cb3e
Derek Wan <d.wan@icloud.com>
2024-08-19 09:09:18 +0900

    more

---
+++ src/utilities/tenacity.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import logging
+from typing import Any, Literal
+
+from typing_extensions import override
+
+
+class LoguruAdapter(logging.Logger):
+    """Proxy for `loguru`, for use in `tenacity`."""
+
+    @override
+    def __init__(self) -> None: ...  # pyright: ignore[reportCallIssue]
+
+    @override
+    def log(  # pyright: ignore[reportIncompatibleMethodOverride]
+        self,
+        msg: Any,
+        level: int,
+        /,
+        *,
+        exc_info: BaseException | Literal[False] | None,
+    ) -> None:
+        from loguru import logger
+
+        logger.opt(exception=exc_info).log(msg, level)
+
+
+__all__ = ["LoguruAdapter"]
