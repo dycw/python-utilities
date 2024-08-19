68f2d7336982eb97e8a87905296eb17f422a2383
Derek Wan <d.wan@icloud.com>
2024-08-20 01:12:55 +0900

    more

--- src/tests/test_pathlib.py
+++ src/tests/test_pathlib.py
@@ -1,23 +1,15 @@
 from __future__ import annotations
 
-import datetime as dt
 from pathlib import Path
 
 from hypothesis import given
-from hypothesis.strategies import booleans, integers, sets
+from hypothesis.strategies import integers, sets
 from pathvalidate import ValidationError
-from pytest import mark, param, raises
+from pytest import raises
 
 from utilities.hypothesis import temp_paths
-from utilities.pathlib import (
-    ensure_path,
-    ensure_suffix,
-    get_modified_time,
-    list_dir,
-    temp_cwd,
-    walk,
-)
-from utilities.pathvalidate import valid_path, valid_path_cwd
+from utilities.pathlib import ensure_path, list_dir, temp_cwd
+from utilities.pathvalidate import valid_path_cwd
 
 
 class TestEnsurePath:
@@ -32,34 +24,6 @@ class TestEnsurePath:
         assert ensure_path("a\0b", sanitize=True) == Path("ab")
 
 
-class TestEnsureSuffix:
-    @mark.parametrize(
-        ("path", "expected"),
-        [
-            param("hello.txt", "hello.txt"),
-            param("hello.1.txt", "hello.1.txt"),
-            param("hello.1.2.txt", "hello.1.2.txt"),
-            param("hello.jpg", "hello.jpg.txt"),
-            param("hello.1.jpg", "hello.1.jpg.txt"),
-            param("hello.1.2.jpg", "hello.1.2.jpg.txt"),
-            param("hello.txt.jpg", "hello.txt.jpg.txt"),
-            param("hello.txt.1.jpg", "hello.txt.1.jpg.txt"),
-            param("hello.txt.1.2.jpg", "hello.txt.1.2.jpg.txt"),
-        ],
-    )
-    def test_main(self, *, path: Path, expected: Path) -> None:
-        result = ensure_suffix(path, ".txt")
-        assert result == valid_path(expected)
-
-
-class TestGetModifiedTime:
-    @given(path=temp_paths())
-    def test_main(self, *, path: Path) -> None:
-        path.touch()
-        mod = get_modified_time(path)
-        assert isinstance(mod, dt.datetime)
-
-
 class TestListDir:
     @given(root=temp_paths(), nums=sets(integers(0, 100), max_size=10))
     def test_main(self, *, root: Path, nums: set[str]) -> None:
@@ -69,35 +33,6 @@ class TestListDir:
         result = list_dir(root)
         expected = sorted(Path(root, f"{n}.txt") for n in nums)
         assert result == expected
-
-
-class TestWalk:
-    @given(
-        root=temp_paths(),
-        topdown=booleans(),
-        onerror=booleans(),
-        followlinks=booleans(),
-    )
-    def test_main(
-        self, *, root: Path, topdown: bool, onerror: bool, followlinks: bool
-    ) -> None:
-        def on_error(error: OSError, /) -> None:
-            assert error.args != ()
-
-        for dirpath, dirnames, filenames in walk(
-            root,
-            topdown=topdown,
-            onerror=on_error if onerror else None,
-            followlinks=followlinks,
-        ):
-            assert isinstance(dirpath, Path)
-            assert isinstance(dirnames, list)
-            for dirname in dirnames:
-                assert isinstance(dirname, Path)
-                assert dirname.is_dir()
-            for filename in filenames:
-                assert isinstance(filename, Path)
-                assert filename.is_file()
 
 
 class TestTempCWD:
