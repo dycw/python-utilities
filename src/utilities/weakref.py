af6bde4bc4e938787b5e698ebc3ec713a6c43b72
Derek Wan <d.wan@icloud.com>
2024-08-20 12:59:05 +0900

    more

--- src/utilities/weakref.py
+++ src/utilities/weakref.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, TypeVar
 from weakref import ReferenceType, ref
 
 if TYPE_CHECKING:
     from collections.abc import Callable
 
+_T = TypeVar("_T")
 
-def add_finalizer(obj: Any, callback: Callable[[], None], /) -> ReferenceType:
+
+def add_finalizer(obj: _T, callback: Callable[[], None], /) -> ReferenceType[_T]:
     """Add a finalizer for an object."""
     return ref(obj, _add_finalizer_modify(callback))
 
