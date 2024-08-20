4537331162a075625165384ba0dd94d76314c33f
Derek Wan <d.wan@icloud.com>
2024-08-20 09:45:36 +0900

    more

--- src/utilities/slack_sdk.py
+++ src/utilities/slack_sdk.py
@@ -13,9 +13,9 @@ _TIMEOUT = 30
 
 def send_slack_sync(text: str, /, *, url: str, timeout: int = _TIMEOUT) -> None:
     """Send a message to Slack, synchronously."""
-    client = _get_client_sync(url, timeout=timeout)
-    response = client.send(text=text)
-    _check_status_code(response)
+    client = _get_client_sync(url, timeout=timeout)  # pragma: no cover
+    response = client.send(text=text)  # pragma: no cover
+    _check_status_code(response)  # pragma: no cover
 
 
 async def send_slack_async(
@@ -26,14 +26,14 @@ async def send_slack_async(
     timeout: int = _TIMEOUT,  # noqa: ASYNC109
 ) -> None:
     """Send a message via Slack."""
-    client = _get_client_async(url, timeout=timeout)
-    response = await client.send(text=text)
-    _check_status_code(response)
+    client = _get_client_async(url, timeout=timeout)  # pragma: no cover
+    response = await client.send(text=text)  # pragma: no cover
+    _check_status_code(response)  # pragma: no cover
 
 
 def _check_status_code(response: WebhookResponse, /) -> None:
     """Check that a chunk was successfully sent."""
-    if response.status_code != HTTPStatus.OK:
+    if response.status_code != HTTPStatus.OK:  # pragma: no cover
         raise SendSlackError(response=response)
 
 
@@ -43,7 +43,7 @@ class SendSlackError(Exception):
 
     @override
     def __str__(self) -> str:
-        return f"Webhook response was not OK; got {self.response.status_code}"
+        return f"Webhook response was not OK; got {self.response.status_code}"  # pragma: no cover
 
 
 @ttl_cache(maxsize=1)
