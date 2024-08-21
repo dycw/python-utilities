e6c7141f5b9a662de7922d63f55376b8a9029989
Derek Wan <d.wan@icloud.com>
2024-08-22 01:27:55 +0900

    more

--- src/utilities/enum.py
+++ src/utilities/enum.py
@@ -1,15 +1,18 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from enum import Enum
-from typing import TYPE_CHECKING, Generic, TypeVar, overload
+from typing import TYPE_CHECKING, Generic, TypeVar, cast, overload
 
 from typing_extensions import override
 
 from utilities.iterables import (
+    MaybeIterable,
     _OneStrCaseInsensitiveBijectionError,
     _OneStrCaseInsensitiveEmptyError,
     _OneStrCaseSensitiveEmptyError,
+    is_iterable_not_str,
     one_str,
 )
 
@@ -29,12 +32,26 @@ def ensure_enum(
 def ensure_enum(
     enum: type[_E], /, *, value: MaybeStr[_E], case_sensitive: bool = ...
 ) -> _E: ...
+@overload
 def ensure_enum(
-    enum: type[_E], /, *, value: MaybeStr[_E] | None, case_sensitive: bool = False
-) -> _E | None:
+    enum: type[_E], /, *, value: Iterable[MaybeStr[_E]], case_sensitive: bool = ...
+) -> Iterable[_E]: ...
+def ensure_enum(
+    enum: type[_E],
+    /,
+    *,
+    value: MaybeIterable[MaybeStr[_E]] | None,
+    case_sensitive: bool = False,
+) -> _E | Iterable[_E] | None:
     """Ensure the object is a member of the enum."""
     if value is None:
         return None
+    if is_iterable_not_str(value):
+        return (
+            ensure_enum(enum, value=v, case_sensitive=case_sensitive)
+            for v in cast(Iterable[MaybeStr[_E]], value)
+        )
+    value = cast(MaybeStr[_E], value)
     if isinstance(value, Enum):
         return value
     return parse_enum(enum, value, case_sensitive=case_sensitive)
