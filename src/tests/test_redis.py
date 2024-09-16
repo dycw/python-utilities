29c76a5720c48f656504b82d7c19af548ea89472
Derek Wan <d.wan@icloud.com>
2024-09-16 21:55:30 +0900

    hi

--- src/tests/test_redis.py
+++ src/tests/test_redis.py
@@ -2,23 +2,8 @@ from __future__ import annotations
 
 import redis
 import redis.asyncio
-from hypothesis.strategies import sampled_from
 
-from utilities.datetime import EPOCH_UTC, drop_microseconds
-from utilities.hypothesis import zoned_datetimes
 from utilities.redis import yield_client, yield_client_async
-from utilities.zoneinfo import UTC, HongKong
-
-valid_zoned_datetimes = zoned_datetimes(
-    min_value=EPOCH_UTC, time_zone=sampled_from([HongKong, UTC]), valid=True
-).map(drop_microseconds)
-invalid_zoned_datetimes = (
-    zoned_datetimes(
-        max_value=EPOCH_UTC, time_zone=sampled_from([HongKong, UTC]), valid=True
-    )
-    .map(drop_microseconds)
-    .filter(lambda t: t < EPOCH_UTC)
-)
 
 
 class TestYieldClient:
--- src/utilities/__init__.py
+++ src/utilities/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-__version__ = "0.55.5"
+__version__ = "0.56.0"
