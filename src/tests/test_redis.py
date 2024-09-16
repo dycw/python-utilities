830fbdb4c4ff666975488c341d18047477c0bdb9
Derek Wan <d.wan@icloud.com>
2024-09-17 01:18:45 +0900

    Add `RedisHashMapKey` (#740)

--- src/tests/test_redis.py
+++ src/tests/test_redis.py
@@ -4,10 +4,11 @@ import redis
 import redis.asyncio
 from hypothesis import given
 from hypothesis.strategies import DataObject, booleans, data
+from pytest import mark
 
 from tests.conftest import SKIPIF_CI_AND_NOT_LINUX
-from utilities.hypothesis import redis_cms
-from utilities.redis import RedisKey, yield_client, yield_client_async
+from utilities.hypothesis import redis_cms, text_ascii
+from utilities.redis import RedisHashMapKey, RedisKey, yield_client, yield_client_async
 
 
 class TestRedisKey:
@@ -18,13 +19,31 @@ class TestRedisKey:
             key = RedisKey(name=container.key, type=bool)
             match container.client:
                 case redis.Redis():
-                    assert key.get() is None
-                    _ = key.set(value)
-                    assert key.get() is value
+                    assert key.get(db=15) is None
+                    _ = key.set(value, db=15)
+                    assert key.get(db=15) is value
                 case redis.asyncio.Redis():
-                    assert await key.get_async() is None
-                    _ = await key.set_async(value)
-                    assert await key.get_async() is value
+                    assert await key.get_async(db=15) is None
+                    _ = await key.set_async(value, db=15)
+                    assert await key.get_async(db=15) is value
+
+
+class TestRedisHashMapKey:
+    @mark.skip
+    @given(data=data(), key=text_ascii(), value=booleans())
+    @SKIPIF_CI_AND_NOT_LINUX
+    async def test_main(self, *, data: DataObject, key: str, value: bool) -> None:
+        async with redis_cms(data) as container:
+            hash_map_key = RedisHashMapKey(name=container.key, type=bool)
+            match container.client:
+                case redis.Redis():
+                    assert hash_map_key.hget(key, db=15) is None
+                    _ = hash_map_key.hset(key, value, db=15)
+                    assert hash_map_key.hget(key, db=15) is value
+                case redis.asyncio.Redis():
+                    assert await hash_map_key.hget_async(key, db=15) is None
+                    _ = await hash_map_key.hset_async(key, value, db=15)
+                    assert await hash_map_key.hget_async(key, db=15) is value
 
 
 class TestYieldClient:
--- src/utilities/redis.py
+++ src/utilities/redis.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+from collections.abc import Awaitable
 from contextlib import asynccontextmanager, contextmanager
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Generic, TypeVar, assert_never
+from typing import TYPE_CHECKING, Any, Generic, TypeVar, assert_never, cast
 
 import redis
 import redis.asyncio
@@ -33,6 +34,140 @@ class RedisContainer(Generic[_TRedis]):
     timestamp: dt.datetime
     uuid: UUID
     key: str
+
+
+@dataclass(frozen=True, kw_only=True)
+class RedisHashMapKey(Generic[_T]):
+    """A hashmap key in a redis store."""
+
+    name: str
+    type: type[_T]
+
+    def hget(
+        self,
+        key: str,
+        /,
+        *,
+        client: redis.Redis | None = None,
+        host: str = _HOST,
+        port: int = _PORT,
+        db: int = 0,
+        password: str | None = None,
+        connection_pool: redis.ConnectionPool | None = None,
+        decode_responses: bool = False,
+        **kwargs: Any,
+    ) -> _T | None:
+        """Get a value from a hashmap in `redis`."""
+        from utilities.orjson import deserialize  # skipif-ci-and-not-linux
+
+        with yield_client(  # skipif-ci-and-not-linux
+            client=client,
+            host=host,
+            port=port,
+            db=db,
+            password=password,
+            connection_pool=connection_pool,
+            decode_responses=decode_responses,
+            **kwargs,
+        ) as client_use:
+            maybe_ser = client_use.hget(self.name, key)
+        if maybe_ser is None:  # skipif-ci-and-not-linux
+            return None
+        return deserialize(ensure_bytes(maybe_ser))  # skipif-ci-and-not-linux
+
+    def hset(
+        self,
+        key: str,
+        value: _T,
+        /,
+        *,
+        client: redis.Redis | None = None,
+        host: str = _HOST,
+        port: int = _PORT,
+        db: int = 0,
+        password: str | None = None,
+        connection_pool: redis.ConnectionPool | None = None,
+        decode_responses: bool = False,
+        **kwargs: Any,
+    ) -> ResponseT:
+        """Set a value in a hashmap in `redis`."""
+        from utilities.orjson import serialize  # skipif-ci-and-not-linux
+
+        ser = serialize(value)  # skipif-ci-and-not-linux
+        with yield_client(  # skipif-ci-and-not-linux
+            client=client,
+            host=host,
+            port=port,
+            db=db,
+            password=password,
+            connection_pool=connection_pool,
+            decode_responses=decode_responses,
+            **kwargs,
+        ) as client_use:
+            return client_use.hset(self.name, key=key, value=cast(Any, ser))
+
+    async def hget_async(
+        self,
+        key: str,
+        /,
+        *,
+        client: redis.asyncio.Redis | None = None,
+        host: str = _HOST,
+        port: int = _PORT,
+        db: str | int = 0,
+        password: str | None = None,
+        connection_pool: redis.asyncio.ConnectionPool | None = None,
+        decode_responses: bool = False,
+        **kwargs: Any,
+    ) -> _T | None:
+        """Get a value from a hashmap in `redis` asynchronously."""
+        from utilities.orjson import deserialize  # skipif-ci-and-not-linux
+
+        async with yield_client_async(  # skipif-ci-and-not-linux
+            client=client,
+            host=host,
+            port=port,
+            db=db,
+            password=password,
+            connection_pool=connection_pool,
+            decode_responses=decode_responses,
+            **kwargs,
+        ) as client_use:
+            maybe_ser = await cast(Awaitable[Any], client_use.hget(self.name, key))
+        if maybe_ser is None:  # skipif-ci-and-not-linux
+            return None
+        return deserialize(ensure_bytes(maybe_ser))  # skipif-ci-and-not-linux
+
+    async def hset_async(
+        self,
+        key: str,
+        value: _T,
+        /,
+        *,
+        client: redis.asyncio.Redis | None = None,
+        host: str = _HOST,
+        port: int = _PORT,
+        db: str | int = 0,
+        password: str | None = None,
+        connection_pool: redis.asyncio.ConnectionPool | None = None,
+        decode_responses: bool = False,
+        **kwargs: Any,
+    ) -> ResponseT:
+        """Set a value in a hashmap in `redis` asynchronously."""
+        from utilities.orjson import serialize  # skipif-ci-and-not-linux
+
+        ser = serialize(value)  # skipif-ci-and-not-linux
+        async with yield_client_async(  # skipif-ci-and-not-linux
+            client=client,
+            host=host,
+            port=port,
+            db=db,
+            password=password,
+            connection_pool=connection_pool,
+            decode_responses=decode_responses,
+            **kwargs,
+        ) as client_use:
+            return await client_use.hset(self.name, key=key, value=cast(Any, ser))
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -67,10 +202,10 @@ class RedisKey(Generic[_T]):
             decode_responses=decode_responses,
             **kwargs,
         ) as client_use:
-            maybe_ser = ensure_bytes(client_use.get(self.name), nullable=True)
-        return (  # skipif-ci-and-not-linux
-            None if maybe_ser is None else deserialize(maybe_ser)
-        )
+            maybe_ser = client_use.get(self.name)
+        if maybe_ser is None:  # skipif-ci-and-not-linux
+            return None
+        return deserialize(ensure_bytes(maybe_ser))  # skipif-ci-and-not-linux
 
     def set(
         self,
@@ -127,10 +262,10 @@ class RedisKey(Generic[_T]):
             decode_responses=decode_responses,
             **kwargs,
         ) as client_use:
-            maybe_ser = ensure_bytes(await client_use.get(self.name), nullable=True)
-        return (  # skipif-ci-and-not-linux
-            None if maybe_ser is None else deserialize(maybe_ser)
-        )
+            maybe_ser = await client_use.get(self.name)
+        if maybe_ser is None:  # skipif-ci-and-not-linux
+            return None
+        return deserialize(ensure_bytes(maybe_ser))  # skipif-ci-and-not-linux
 
     async def set_async(
         self,
@@ -231,4 +366,10 @@ async def yield_client_async(
                 assert_never(never)
 
 
-__all__ = ["RedisContainer", "RedisKey", "yield_client", "yield_client_async"]
+__all__ = [
+    "RedisContainer",
+    "RedisHashMapKey",
+    "RedisKey",
+    "yield_client",
+    "yield_client_async",
+]
