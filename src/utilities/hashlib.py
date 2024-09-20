5c59afd021752ede5378c35b5dcd2a0095c17ff8
Derek Wan <d.wan@icloud.com>
2024-09-21 00:44:17 +0900

    save

--- src/utilities/hashlib.py
+++ src/utilities/hashlib.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 from hashlib import md5
 from typing import Any
 
-from utilities.orjson import serialize
-
 
 def md5_hash(obj: Any, /) -> str:
     """Compute the MD5 hash of an arbitrary object."""
+    from utilities.orjson import serialize
+
     return md5(serialize(obj), usedforsecurity=False).hexdigest()
 
 
