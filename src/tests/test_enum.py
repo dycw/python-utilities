0a0bdb0446f6aba4e6d997cfc001119e4f878501
Derek Wan <d.wan@icloud.com>
2024-10-16 22:04:47 +0900

    Simplify `ensure_enum` (#796)

--- src/tests/test_enum.py
+++ src/tests/test_enum.py
@@ -3,112 +3,17 @@ from __future__ import annotations
 from enum import Enum, StrEnum, auto
 
 from hypothesis import given
-from hypothesis.strategies import DataObject, data, lists, sampled_from
+from hypothesis.strategies import DataObject, data, sampled_from
 from pytest import mark, param, raises
 
 from utilities.enum import (
-    EnsureEnumError,
     MaybeStr,
     ParseEnumError,
+    _EnsureEnumParseError,
+    _EnsureEnumTypeEnumError,
     ensure_enum,
     parse_enum,
 )
-
-
-class TestEnsureEnum:
-    @given(data=data())
-    def test_single_value_single_enum(self, *, data: DataObject) -> None:
-        class Truth(Enum):
-            true = auto()
-            false = auto()
-
-        truth: Truth = data.draw(sampled_from(Truth))
-        input_: MaybeStr[Truth] = data.draw(sampled_from([truth, truth.name]))
-        result = ensure_enum(input_, Truth)
-        assert result is truth
-
-    @given(data=data())
-    def test_iterable_value_single_enum(self, *, data: DataObject) -> None:
-        class Truth(Enum):
-            true = auto()
-            false = auto()
-
-        truth: Truth = data.draw(sampled_from(Truth))
-        input_: list[MaybeStr[Truth]] = data.draw(
-            lists(sampled_from([truth, truth.name]))
-        )
-        result = list(ensure_enum(input_, Truth))
-        for r in result:
-            assert r is truth
-
-    @given(data=data())
-    def test_single_value_multiple_enums(self, *, data: DataObject) -> None:
-        class Truth1(Enum):
-            true1 = auto()
-            false1 = auto()
-
-        class Truth2(Enum):
-            true2 = auto()
-            false2 = auto()
-
-        truth: Truth1 | Truth2 = data.draw(sampled_from(Truth1) | sampled_from(Truth2))
-        input_: MaybeStr[Truth1 | Truth2] = data.draw(sampled_from([truth, truth.name]))
-        result = ensure_enum(input_, (Truth1, Truth2))
-        assert result is truth
-
-    @given(data=data())
-    def test_multiple_values_multiple_enums(self, *, data: DataObject) -> None:
-        class Truth1(Enum):
-            true1 = auto()
-            false1 = auto()
-
-        class Truth2(Enum):
-            true2 = auto()
-            false2 = auto()
-
-        truth: Truth1 | Truth2 = data.draw(sampled_from(Truth1) | sampled_from(Truth2))
-        input_: list[MaybeStr[Truth1 | Truth2]] = data.draw(
-            lists(sampled_from([truth, truth.name]))
-        )
-        result = list(ensure_enum(input_, (Truth1, Truth2)))
-        for r in result:
-            assert r is truth
-
-    def test_none(self) -> None:
-        class Truth(Enum):
-            true = auto()
-            false = auto()
-
-        result = ensure_enum(None, Truth)
-        assert result is None
-
-    @given(data=data())
-    def test_error_single_value_single_enum(self, *, data: DataObject) -> None:
-        class Truth1(Enum):
-            true1 = auto()
-            false1 = auto()
-
-        class Truth2(Enum):
-            true2 = auto()
-            false2 = auto()
-
-        truth: Truth1 = data.draw(sampled_from(Truth1))
-        with raises(EnsureEnumError, match=".* is not an instance of .*"):
-            _ = ensure_enum(truth, Truth2)
-
-    @given(data=data())
-    def test_error_single_value_multiple_enums(self, *, data: DataObject) -> None:
-        class Truth1(Enum):
-            true1 = auto()
-            false1 = auto()
-
-        class Truth2(Enum):
-            true2 = auto()
-            false2 = auto()
-
-        truth: Truth1 = data.draw(sampled_from(Truth1))
-        with raises(EnsureEnumError, match=".* is not an instance of .*"):
-            _ = ensure_enum(truth, (Truth2,))
 
 
 class TestParseEnum:
@@ -133,14 +38,6 @@ class TestParseEnum:
         truth: Truth = data.draw(sampled_from(Truth))
         result = parse_enum(truth.name, Truth, case_sensitive=True)
         assert result is truth
-
-    def test_generic_enum_none(self) -> None:
-        class Truth(Enum):
-            true = auto()
-            false = auto()
-
-        result = parse_enum(None, Truth)
-        assert result is None
 
     @given(data=data())
     def test_str_enum(self, *, data: DataObject) -> None:
@@ -223,3 +120,46 @@ class TestParseEnum:
             match=r"StrEnum .* contains 'true_or_false' in both its keys and values \(case sensitive\)",
         ):
             _ = parse_enum("true_or_false", Truth, case_sensitive=True)
+
+    @given(data=data())
+    def test_ensure(self, *, data: DataObject) -> None:
+        class Truth(Enum):
+            true = auto()
+            false = auto()
+
+        truth: Truth = data.draw(sampled_from(Truth))
+        input_: MaybeStr[Truth] = data.draw(sampled_from([truth, truth.name]))
+        result = ensure_enum(input_, Truth)
+        assert result is truth
+
+    def test_ensure_none(self) -> None:
+        class Truth(Enum):
+            true = auto()
+            false = auto()
+
+        result = ensure_enum(None, Truth)
+        assert result is None
+
+    @given(data=data())
+    def test_error_ensure_type(self, *, data: DataObject) -> None:
+        class Truth1(Enum):
+            true1 = auto()
+            false1 = auto()
+
+        class Truth2(Enum):
+            true2 = auto()
+            false2 = auto()
+
+        truth: Truth1 = data.draw(sampled_from(Truth1))
+        with raises(_EnsureEnumTypeEnumError, match=".* is not an instance of .*"):
+            _ = ensure_enum(truth, Truth2)
+
+    def test_error_ensure_parse(self) -> None:
+        class Truth(Enum):
+            true = auto()
+            false = auto()
+
+        with raises(
+            _EnsureEnumParseError, match="Unable to ensure enum; got 'invalid'"
+        ):
+            _ = ensure_enum("invalid", Truth)
