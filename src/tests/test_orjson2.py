4bd5819e5a871af64cadf3d37fd6eff6ec61ca40
Derek Wan <d.wan@icloud.com>
2024-10-25 14:01:20 +0900

    save

--- src/tests/test_orjson2.py
+++ src/tests/test_orjson2.py
@@ -1,43 +1,57 @@
 from __future__ import annotations
 
+import datetime as dt
 from typing import TYPE_CHECKING
 
 from hypothesis import given
-from hypothesis.strategies import dictionaries, lists
+from hypothesis.strategies import booleans, dates, dictionaries, floats, lists
+from pytest import raises
 
-from utilities.hypothesis import text_ascii, zoned_datetimes
+from utilities.hypothesis import int64s, text_ascii, zoned_datetimes
 from utilities.orjson2 import deserialize2, serialize2
+from utilities.sentinel import sentinel
 
-if TYPE_CHECKING:
-    import datetime as dt
+_Object = bool | float | str | dt.date | dt.datetime
+objects = (
+    booleans()
+    | floats(allow_nan=False, allow_infinity=False)
+    | int64s()
+    | text_ascii()
+    | dates()
+    | zoned_datetimes()
+)
 
 
-class TestSerialize2:
-    @given(datetime=zoned_datetimes())
-    def test_main(self, *, datetime: dt.datetime) -> None:
-        result = deserialize2(serialize2(datetime))
-        assert result == datetime
+class TestSerializeAndDeserialize2:
+    @given(obj=objects)
+    def test_main(self, *, obj: _Object) -> None:
+        result = deserialize2(serialize2(obj))
+        assert result == obj
 
-    @given(datetimes=lists(zoned_datetimes()))
-    def test_list_of_dates(self, *, datetimes: list[dt.datetime]) -> None:
-        result = deserialize2(serialize2(datetimes))
-        assert result == datetimes
-
-    @given(datetimes=dictionaries(text_ascii(), zoned_datetimes()))
-    def test_dict_of_dates(self, *, datetimes: dict[str, dt.datetime]) -> None:
-        result = deserialize2(serialize2(datetimes))
-        assert result == datetimes
-
-    @given(datetimes=lists(dictionaries(text_ascii(), zoned_datetimes())))
-    def test_list_of_dict_of_dates(
-        self, *, datetimes: list[dict[str, dt.datetime]]
+    @given(
+        objects=lists(objects)
+        | dictionaries(text_ascii(), objects)
+        | lists(dictionaries(text_ascii(), objects))
+        | dictionaries(text_ascii(), lists(objects))
+    )
+    def test_nested(
+        self,
+        *,
+        objects: list[_Object]
+        | dict[str, _Object]
+        | list[dict[str, _Object]]
+        | dict[str, list[_Object]],
     ) -> None:
-        result = deserialize2(serialize2(datetimes))
-        assert result == datetimes
+        result = deserialize2(serialize2(objects))
+        assert result == objects
 
-    @given(datetimes=dictionaries(text_ascii(), lists(zoned_datetimes())))
-    def test_dict_of_list_of_dates(
-        self, *, datetimes: dict[str, list[dt.datetime]]
-    ) -> None:
-        result = deserialize2(serialize2(datetimes))
-        assert result == datetimes
+    def test_arbitrary_objects(self) -> None:
+        with raises(TypeError, match="Type is not JSON serializable: Sentinel"):
+            _ = serialize2(sentinel)
+        result = serialize2(sentinel, fallback=True)
+        expected = b'"<sentinel>"'
+        assert result == expected
+
+    def test_error_serialize(self) -> None:
+        with raises(TypeError, match="Type is not JSON serializable: Sentinel"):
+            _ = serialize2(sentinel)
--- src/utilities/orjson2.py
+++ src/utilities/orjson2.py
@@ -2,43 +2,42 @@ from __future__ import annotations
 
 import datetime as dt
 import re
-from dataclasses import dataclass
 from functools import partial
+from re import escape
 from typing import Any
 
 from orjson import OPT_PASSTHROUGH_DATETIME, OPT_SORT_KEYS, dumps, loads
-from typing_extensions import override
 
-from utilities.functions import get_class_name
-from utilities.whenever import parse_zoned_datetime, serialize_zoned_datetime
+from utilities.whenever import (
+    parse_date,
+    parse_zoned_datetime,
+    serialize_date,
+    serialize_zoned_datetime,
+)
+
+_DATE_PREFIX = "[d]"
+_DATETIME_PREFIX = "[t]"
 
 
 def serialize2(obj: Any, /, *, fallback: bool = False) -> bytes:
     """Serialize an object."""
     return dumps(
         obj,
-        default=partial(_serialize_default, fallback=fallback),
+        default=partial(_serialize2_default, fallback=fallback),
         option=OPT_PASSTHROUGH_DATETIME | OPT_SORT_KEYS,
     )
 
 
-@dataclass(kw_only=True, slots=True)
-class Serialize2Error(Exception):
-    obj: Any
-
-    @override
-    def __str__(self) -> str:
-        cls = get_class_name(self.obj)
-        return f"Unable to serialize object {self.obj} of type {cls!r}"
-
-
-def _serialize_default(obj: Any, /, *, fallback: bool = False) -> str:
+def _serialize2_default(obj: Any, /, *, fallback: bool = False) -> str:
     if isinstance(obj, dt.datetime):
         ser = serialize_zoned_datetime(obj)
-        return f"[dt]{ser}"
+        return f"{_DATETIME_PREFIX}{ser}"
+    if isinstance(obj, dt.date):
+        ser = serialize_date(obj)
+        return f"{_DATE_PREFIX}{ser}"
     if fallback:
         return str(obj)
-    raise Serialize2Error(obj=obj) from None
+    raise TypeError
 
 
 def deserialize2(data: bytes, /) -> Any:
@@ -46,12 +45,16 @@ def deserialize2(data: bytes, /) -> Any:
     return _object_hook(loads(data))
 
 
-_DATETIME_PATTERN = re.compile(r"^\[dt\](.+)$")
+_DATE_PATTERN = re.compile(rf"^{escape(_DATE_PREFIX)}(.+)$")
+_DATETIME_PATTERN = re.compile(rf"^{escape(_DATETIME_PREFIX)}(.+)$")
 
 
 def _object_hook(obj: Any, /) -> Any:
-    if isinstance(obj, str) and (match := _DATETIME_PATTERN.search(obj)):
-        return parse_zoned_datetime(match.group(1))
+    if isinstance(obj, str):
+        if match := _DATE_PATTERN.search(obj):
+            return parse_date(match.group(1))
+        if match := _DATETIME_PATTERN.search(obj):
+            return parse_zoned_datetime(match.group(1))
     if isinstance(obj, dict):
         return {k: _object_hook(v) for k, v in obj.items()}
     if isinstance(obj, list):
