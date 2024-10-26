4398228a8f0c4194de2bcaaff9c8df76d161ce5a
Derek Wan <d.wan@icloud.com>
2024-10-27 01:56:48 +0900

    save

--- src/tests/test_tracer.py
+++ src/tests/test_tracer.py
@@ -6,6 +6,7 @@ from typing import Literal, NoReturn, cast
 from pytest import approx, fixture, raises
 from treelib import Node
 
+from tests.conftest import FLAKY
 from utilities.iterables import one
 from utilities.tracer import _NodeData, get_tracer_trees, set_tracer_trees, tracer
 
@@ -46,19 +47,21 @@ def fails() -> NoReturn:
 
 
 class TestTracer:
+    @FLAKY
     def test_main(self) -> None:
         _ = outer1()
         trees = get_tracer_trees()
         tree = one(trees)
         root: Node = tree[tree.root]
-        self._check_node(root, "tests.test_tracer", "outer1", 0.04, 0.3, "success")
+        self._check_node(root, "tests.test_tracer", "outer1", 0.04, 0.5, "success")
         mid1, mid2 = cast(list[Node], tree.children(root.identifier))
-        self._check_node(mid1, "tests.test_tracer", "mid1", 0.01, 0.3, "success")
-        self._check_node(mid2, "tests.test_tracer", "mid2", 0.02, 0.3, "success")
+        self._check_node(mid1, "tests.test_tracer", "mid1", 0.01, 0.5, "success")
+        self._check_node(mid2, "tests.test_tracer", "mid2", 0.02, 0.5, "success")
         assert len(tree.children(mid1.identifier)) == 0
         (inner,) = cast(list[Node], tree.children(mid2.identifier))
-        self._check_node(inner, "tests.test_tracer", "inner", 0.01, 0.3, "success")
+        self._check_node(inner, "tests.test_tracer", "inner", 0.01, 0.5, "success")
 
+    @FLAKY
     def test_multiple_calls(self) -> None:
         _ = inner()
         _ = inner()
@@ -66,15 +69,16 @@ class TestTracer:
         assert len(trees) == 2
         for tree in trees:
             root: Node = tree[tree.root]
-            self._check_node(root, "tests.test_tracer", "inner", 0.01, 0.3, "success")
+            self._check_node(root, "tests.test_tracer", "inner", 0.01, 0.5, "success")
 
+    @FLAKY
     def test_error(self) -> None:
         with raises(ValueError, match="Always fails"):
             _ = fails()
         trees = get_tracer_trees()
         tree = one(trees)
         root: Node = tree[tree.root]
-        self._check_node(root, "tests.test_tracer", "fails", 0.01, 0.3, "failure")
+        self._check_node(root, "tests.test_tracer", "fails", 0.01, 0.5, "failure")
 
     def _check_node(
         self,
