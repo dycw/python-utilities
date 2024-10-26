47d6159d8669a82ef247c7cac25f7806ee532794
Derek Wan <d.wan@icloud.com>
2024-10-27 02:32:22 +0900

    save

--- src/tests/test_tracer2.py
+++ src/tests/test_tracer2.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-from time import sleep
-from typing import Literal, NoReturn, cast
+import asyncio
+import time
+from typing import Literal, cast
 
 from pytest import fixture, raises
 from treelib import Node
 
 from utilities.iterables import one
 from utilities.tracer2 import _NodeData, get_tracer_trees, set_tracer_trees, tracer2
+from utilities.zoneinfo import HongKong
 
 
 @fixture(autouse=True)
@@ -17,32 +19,48 @@ def set_tracer_tree_per_function() -> None:
 
 @tracer2
 def outer1() -> None:
-    sleep(0.01)  # 0.01
+    time.sleep(0.01)  # 0.01
     mid1()  # 0.01
     mid2()  # 0.02
 
 
 @tracer2
 def mid1() -> None:
-    sleep(0.01)  # 0.01
+    time.sleep(0.01)  # 0.01
 
 
 @tracer2
 def mid2() -> None:
-    sleep(0.01)  # 0.01
+    time.sleep(0.01)  # 0.01
     inner()  # 0.01
 
 
 @tracer2
 def inner() -> None:
-    sleep(0.01)  # 0.01
+    time.sleep(0.01)  # 0.01
 
 
 @tracer2
-def fails() -> NoReturn:
-    sleep(0.01)  # 0.01
-    msg = "Always fails"
-    raise ValueError(msg)
+async def async_outer1() -> None:
+    await asyncio.sleep(0.01)  # 0.01
+    await async_mid1()  # 0.01
+    await async_mid2()  # 0.02
+
+
+@tracer2
+async def async_mid1() -> None:
+    await asyncio.sleep(0.01)  # 0.01
+
+
+@tracer2
+async def async_mid2() -> None:
+    await asyncio.sleep(0.01)  # 0.01
+    await async_inner()  # 0.01
+
+
+@tracer2
+async def async_inner() -> None:
+    await asyncio.sleep(0.01)  # 0.01
 
 
 class TestTracer:
@@ -59,37 +77,76 @@ class TestTracer:
         (inner,) = cast(list[Node], tree.children(mid2.identifier))
         self._check_node(inner, "tests.test_tracer2", "inner", 0.01, "success")
 
+    async def test_async(self) -> None:
+        _ = await async_outer1()
+        trees = get_tracer_trees()
+        tree = one(trees)
+        root: Node = tree[tree.root]
+        self._check_node(root, "tests.test_tracer2", "async_outer1", 0.04, "success")
+        mid1, mid2 = cast(list[Node], tree.children(root.identifier))
+        self._check_node(mid1, "tests.test_tracer2", "async_mid1", 0.01, "success")
+        self._check_node(mid2, "tests.test_tracer2", "async_mid2", 0.02, "success")
+        assert len(tree.children(mid1.identifier)) == 0
+        (inner,) = cast(list[Node], tree.children(mid2.identifier))
+        self._check_node(inner, "tests.test_tracer2", "async_inner", 0.01, "success")
+
     def test_multiple_calls(self) -> None:
-        _ = inner()
-        _ = inner()
+        @tracer2
+        def func() -> None:
+            time.sleep(0.01)
+
+        _ = func()
+        _ = func()
         trees = get_tracer_trees()
         assert len(trees) == 2
         for tree in trees:
             root: Node = tree[tree.root]
-            self._check_node(root, "tests.test_tracer2", "inner", 0.01, "success")
+            self._check_node(
+                root,
+                "tests.test_tracer2",
+                "TestTracer.test_multiple_calls.<locals>.func",
+                0.02,
+                "success",
+            )
+
+    def test_time_zone(self) -> None:
+        @tracer2(time_zone=HongKong)
+        def func() -> None:
+            return
+
+        _ = func()
+        tree = one(get_tracer_trees())
+        root: Node = tree[tree.root]
+        data = cast(_NodeData, root.data)
+        assert data["start_time"].tzinfo is HongKong
+        assert data["end_time"].tzinfo is HongKong
 
     def test_error(self) -> None:
+        @tracer2
+        def func() -> None:
+            msg = "Always fails"
+            raise ValueError(msg)
+
         with raises(ValueError, match="Always fails"):
-            _ = fails()
-        trees = get_tracer_trees()
-        tree = one(trees)
+            _ = func()
+        tree = one(get_tracer_trees())
         root: Node = tree[tree.root]
-        self._check_node(root, "tests.test_tracer2", "fails", 0.01, "failure")
+        data = cast(_NodeData, root.data)
+        assert data["outcome"] == "failure"
+        assert data.get("error") is ValueError
 
     def _check_node(
         self,
         node: Node,
         module: str,
-        name: str,
+        qualname: str,
         duration: float,
         outcome: Literal["success", "failure"],
         /,
     ) -> None:
-        assert node.tag == f"{module}:{name}"
+        assert node.tag == f"{module}:{qualname}"
         data = cast(_NodeData, node.data)
         assert data["module"] == module
-        assert data["name"] == name
-        data_duration = data.get("duration")
-        assert data_duration is not None
-        assert data_duration.total_seconds() <= 2 * duration
+        assert data["qualname"] == qualname
+        assert data["duration"].total_seconds() <= 2 * duration
         assert data["outcome"] == outcome
--- src/utilities/tracer2.py
+++ src/utilities/tracer2.py
@@ -18,6 +18,7 @@ from typing import (
 from treelib import Tree
 
 from utilities.datetime import get_now
+from utilities.sentinel import sentinel
 from utilities.zoneinfo import UTC
 
 if TYPE_CHECKING:
@@ -52,11 +53,11 @@ _TRACER_CONTEXT: ContextVar[_TracerData] = ContextVar(
 
 class _NodeData(TypedDict):
     module: str
-    name: str
+    qualname: str
     kwargs: StrMapping
     start_time: dt.datetime
-    end_time: NotRequired[dt.datetime]
-    duration: NotRequired[dt.timedelta]
+    end_time: dt.datetime
+    duration: dt.timedelta
     outcome: Literal["success", "failure"]
     error: NotRequired[type[Exception]]
 
@@ -75,20 +76,58 @@ def tracer2(
         result = partial(tracer2, time_zone=time_zone)
         return cast(Callable[[_F], _F], result)
 
+    base_data = _NodeData(
+        module=func.__module__,
+        qualname=func.__qualname__,
+        kwargs=sentinel,
+        start_time=sentinel,
+        end_time=sentinel,
+        duration=sentinel,
+        outcome=sentinel,
+    )
+    tag = ":".join([base_data["module"], base_data["qualname"]])
+
     if iscoroutinefunction(func):
-        raise NotImplementedError
+
+        @wraps(func)
+        async def wrapped_async(*args: Any, **kwargs: Any) -> Any:
+            node_data = base_data.copy()
+            start_time = node_data["start_time"] = get_now(time_zone=time_zone)
+            node_data["kwargs"] = kwargs
+            tracer_data: _TracerData = _TRACER_CONTEXT.get()
+            if (tree := tracer_data.get("tree")) is None:
+                tree_use = tracer_data["tree"] = Tree()
+                tracer_data["trees"].append(tree_use)
+            else:
+                tree_use = tree
+            parent_node = tracer_data.get("node")
+            child = tree_use.create_node(tag=tag, parent=parent_node, data=node_data)
+            token = _TRACER_CONTEXT.set(
+                _TracerData(trees=tracer_data["trees"], tree=tree_use, node=child)
+            )
+            try:
+                result = await func(*args, **kwargs)
+            except Exception as error:
+                node_data["outcome"] = "failure"
+                node_data["error"] = type(error)
+                raise
+            else:
+                node_data["outcome"] = "success"
+                return result
+            finally:
+                end_time = node_data["end_time"] = get_now(time_zone=time_zone)
+                node_data["duration"] = end_time - start_time
+                if tree is None:
+                    del tracer_data["tree"]
+                _TRACER_CONTEXT.reset(token)
+
+        return cast(Any, wrapped_async)
 
     @wraps(func)
-    def wrapped(*args: Any, **kwargs: Any) -> Any:
-        start_time = get_now(time_zone=time_zone)
-        node_data = _NodeData(
-            module=func.__module__,
-            name=func.__name__,
-            kwargs=kwargs,
-            start_time=start_time,
-            outcome="failure",
-        )
-        tag = ":".join([node_data["module"], node_data["name"]])
+    def wrapped_sync(*args: Any, **kwargs: Any) -> Any:
+        node_data = base_data.copy()
+        start_time = node_data["start_time"] = get_now(time_zone=time_zone)
+        node_data["kwargs"] = kwargs
         tracer_data: _TracerData = _TRACER_CONTEXT.get()
         if (tree := tracer_data.get("tree")) is None:
             tree_use = tracer_data["tree"] = Tree()
@@ -116,7 +155,7 @@ def tracer2(
                 del tracer_data["tree"]
             _TRACER_CONTEXT.reset(token)
 
-    return cast(Any, wrapped)
+    return cast(Any, wrapped_sync)
 
 
 def get_tracer_trees() -> list[Tree]:
