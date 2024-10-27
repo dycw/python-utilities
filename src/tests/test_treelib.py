1230f45358d64dabbdc7bbf88d8343fdd000c7d1
Derek Wan <d.wan@icloud.com>
2024-10-27 15:32:38 +0900

    save

--- src/tests/test_treelib.py
+++ src/tests/test_treelib.py
@@ -10,16 +10,30 @@ from utilities.treelib import filter_tree
 @fixture
 def tree() -> Tree:
     tree = Tree()
-    _ = tree.create_node("Root", "root")
-    _ = tree.create_node("Child1", "child1", parent="root")
-    _ = tree.create_node("Child2", "child2", parent="root")
-    _ = tree.create_node("Grandchild1", "grandchild1", parent="child1")
-    _ = tree.create_node("Grandchild2", "grandchild2", parent="child1")
-    _ = tree.create_node("Grandchild3", "grandchild3", parent="child2")
+    _ = tree.create_node("Root", "r", data={"num": 1})
+    _ = tree.create_node("Child1", "c1", parent="r", data={"num": 2})
+    _ = tree.create_node("Child2", "c2", parent="r", data={"num": 3})
+    _ = tree.create_node("Grandchild1", "gc1", parent="c1", data={"num": 4})
+    _ = tree.create_node("Grandchild2", "gc2", parent="c1", data={"num": 5})
+    _ = tree.create_node("Grandchild3", "gc3", parent="c2", data={"num": 6})
     return tree
 
 
 class TestFilterTree:
+    def test_no_filter(self, *, tree: Tree, capsys: CaptureFixture) -> None:
+        subtree = filter_tree(tree)
+        print(str(subtree))  # noqa: T201
+        out = capsys.readouterr().out.strip("\n")
+        expected = strip_and_dedent("""
+            Root
+            ├── Child1
+            │   ├── Grandchild1
+            │   └── Grandchild2
+            └── Child2
+                └── Grandchild3
+        """)
+        assert out == expected
+
     def test_tag(self, *, tree: Tree, capsys: CaptureFixture) -> None:
         subtree = filter_tree(tree, tag=lambda t: t != "Grandchild3")
         print(str(subtree))  # noqa: T201
@@ -33,8 +47,21 @@ class TestFilterTree:
         """)
         assert out == expected
 
-    def test_tag(self, *, tree: Tree, capsys: CaptureFixture) -> None:
-        subtree = filter_tree(tree, tag=lambda t: t != "Grandchild3")
+    def test_identifier(self, *, tree: Tree, capsys: CaptureFixture) -> None:
+        subtree = filter_tree(tree, identifier=lambda id_: id_ != "gc3")
+        print(str(subtree))  # noqa: T201
+        out = capsys.readouterr().out.strip("\n")
+        expected = strip_and_dedent("""
+            Root
+            ├── Child1
+            │   ├── Grandchild1
+            │   └── Grandchild2
+            └── Child2
+        """)
+        assert out == expected
+
+    def test_data(self, *, tree: Tree, capsys: CaptureFixture) -> None:
+        subtree = filter_tree(tree, data=lambda data: data["num"] <= 5)
         print(str(subtree))  # noqa: T201
         out = capsys.readouterr().out.strip("\n")
         expected = strip_and_dedent("""
--- src/utilities/treelib.py
+++ src/utilities/treelib.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, Any, cast
 
 from treelib import Node, Tree
 
@@ -16,11 +16,12 @@ def filter_tree(
     *,
     tag: Callable[[str], bool] | None = None,
     identifier: Callable[[str], bool] | None = None,
+    data: Callable[[Any], bool] | None = None,
 ) -> Tree:
     """Filter a tree."""
     subtree = Tree()
     _filter_tree_add(
-        tree, subtree, ensure_str(tree.root), tag=tag, identifier=identifier
+        tree, subtree, ensure_str(tree.root), tag=tag, identifier=identifier, data=data
     )
     return subtree
 
@@ -34,9 +35,17 @@ def _filter_tree_add(
     parent_id: str | None = None,
     tag: Callable[[str], bool] | None = None,
     identifier: Callable[[str], bool] | None = None,
+    data: Callable[[Any], bool] | None = None,
 ) -> None:
     node = cast(Node, old.get_node(node_id))
-    if (tag is not None) and tag(ensure_str(node.tag)):
+    predicates: set[bool] = set()
+    if tag is not None:
+        predicates.add(tag(ensure_str(node.tag)))
+    if identifier is not None:
+        predicates.add(identifier(ensure_str(node.identifier)))
+    if data is not None:
+        predicates.add(data(node.data))
+    if all(predicates):
         _ = new.create_node(node.tag, node.identifier, parent=parent_id, data=node.data)
         for child in old.children(node_id):
             _filter_tree_add(
@@ -45,7 +54,8 @@ def _filter_tree_add(
                 child.identifier,
                 parent_id=node.identifier,
                 tag=tag,
-                identifier=ident,
+                identifier=identifier,
+                data=data,
             )
 
 
