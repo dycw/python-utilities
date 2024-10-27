f29b4e0c2537bea6346321a3110f827557d1a7ab
Derek Wan <d.wan@icloud.com>
2024-10-27 09:04:39 +0900

    save

--- src/tests/test_tracer.py
+++ src/tests/test_tracer.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 import asyncio
 import time
-from typing import Literal, cast
+from typing import cast
 
-from pytest import fixture, raises
+from pytest import approx, fixture, raises
 from treelib import Node
 
 from utilities.iterables import one
@@ -17,78 +17,87 @@ def set_tracer_tree_per_function() -> None:
     set_tracer_trees([])
 
 
-@tracer
-def outer1() -> None:
-    time.sleep(0.01)  # 0.01
-    mid1()  # 0.01
-    mid2()  # 0.02
-
-
-@tracer
-def mid1() -> None:
-    time.sleep(0.01)  # 0.01
-
-
-@tracer
-def mid2() -> None:
-    time.sleep(0.01)  # 0.01
-    inner()  # 0.01
-
-
-@tracer
-def inner() -> None:
-    time.sleep(0.01)  # 0.01
-
-
-@tracer
-async def async_outer1() -> None:
-    await asyncio.sleep(0.01)  # 0.01
-    await async_mid1()  # 0.01
-    await async_mid2()  # 0.02
-
-
-@tracer
-async def async_mid1() -> None:
-    await asyncio.sleep(0.01)  # 0.01
-
-
-@tracer
-async def async_mid2() -> None:
-    await asyncio.sleep(0.01)  # 0.01
-    await async_inner()  # 0.01
-
-
-@tracer
-async def async_inner() -> None:
-    await asyncio.sleep(0.01)  # 0.01
-
-
 class TestTracer:
-    def test_main(self) -> None:
-        _ = outer1()
-        trees = get_tracer_trees()
-        tree = one(trees)
+    def test_sync(self) -> None:
+        @tracer
+        def outer() -> None:
+            time.sleep(0.01)  # 0.01
+            mid1()  # 0.01
+            mid2()  # 0.02
+
+        @tracer
+        def mid1() -> None:
+            time.sleep(0.01)  # 0.01
+
+        @tracer
+        def mid2() -> None:
+            time.sleep(0.01)  # 0.01
+            inner()  # 0.01
+
+        @tracer
+        def inner() -> None:
+            time.sleep(0.01)  # 0.01
+
+        outer()
+        tree = one(get_tracer_trees())
         root: Node = tree[tree.root]
-        self._check_node(root, "tests.test_tracer", "outer1", 0.04, "success")
-        mid1, mid2 = cast(list[Node], tree.children(root.identifier))
-        self._check_node(mid1, "tests.test_tracer", "mid1", 0.01, "success")
-        self._check_node(mid2, "tests.test_tracer", "mid2", 0.02, "success")
-        assert len(tree.children(mid1.identifier)) == 0
-        (inner,) = cast(list[Node], tree.children(mid2.identifier))
-        self._check_node(inner, "tests.test_tracer", "inner", 0.01, "success")
+        self._check_node(
+            root, "tests.test_tracer", "TestTracer.test_sync.<locals>.outer", 0.04
+        )
+        node_mid1, node_mid2 = cast(list[Node], tree.children(root.identifier))
+        self._check_node(
+            node_mid1, "tests.test_tracer", "TestTracer.test_sync.<locals>.mid1", 0.01
+        )
+        self._check_node(
+            node_mid2, "tests.test_tracer", "TestTracer.test_sync.<locals>.mid2", 0.02
+        )
+        assert len(tree.children(node_mid1.identifier)) == 0
+        (node_inner,) = cast(list[Node], tree.children(node_mid2.identifier))
+        self._check_node(
+            node_inner, "tests.test_tracer", "TestTracer.test_sync.<locals>.inner", 0.01
+        )
 
     async def test_async(self) -> None:
-        _ = await async_outer1()
-        trees = get_tracer_trees()
-        tree = one(trees)
+        @tracer
+        async def outer() -> None:
+            await asyncio.sleep(0.01)  # 0.01
+            await mid1()  # 0.01
+            await mid2()  # 0.02
+
+        @tracer
+        async def mid1() -> None:
+            await asyncio.sleep(0.01)  # 0.01
+
+        @tracer
+        async def mid2() -> None:
+            await asyncio.sleep(0.01)  # 0.01
+            await inner()  # 0.01
+
+        @tracer
+        async def inner() -> None:
+            await asyncio.sleep(0.01)  # 0.01
+
+        await outer()
+        tree = one(get_tracer_trees())
         root: Node = tree[tree.root]
-        self._check_node(root, "tests.test_tracer", "async_outer1", 0.04, "success")
-        mid1, mid2 = cast(list[Node], tree.children(root.identifier))
-        self._check_node(mid1, "tests.test_tracer", "async_mid1", 0.01, "success")
-        self._check_node(mid2, "tests.test_tracer", "async_mid2", 0.02, "success")
-        assert len(tree.children(mid1.identifier)) == 0
-        (inner,) = cast(list[Node], tree.children(mid2.identifier))
-        self._check_node(inner, "tests.test_tracer", "async_inner", 0.01, "success")
+        self._check_node(
+            root, "tests.test_tracer", "TestTracer.test_async.<locals>.outer", 0.04
+        )
+        node_mid1, node_mid2 = cast(list[Node], tree.children(root.identifier))
+        self._check_node(
+            node_mid1, "tests.test_tracer", "TestTracer.test_async.<locals>.mid1", 0.01
+        )
+        self._check_node(
+            node_mid2, "tests.test_tracer", "TestTracer.test_async.<locals>.mid2", 0.02
+        )
+        assert len(tree.children(node_mid1.identifier)) == 0
+        (node_inner,) = cast(list[Node], tree.children(node_mid2.identifier))
+        self._check_node(
+            node_inner,
+            "tests.test_tracer",
+            "TestTracer.test_async.<locals>.inner",
+            0.01,
+        )
 
     def test_multiple_calls(self) -> None:
         @tracer
@@ -106,7 +115,6 @@ class TestTracer:
                 "tests.test_tracer",
                 "TestTracer.test_multiple_calls.<locals>.func",
                 0.02,
-                "success",
             )
 
     def test_time_zone(self) -> None:
@@ -136,17 +144,11 @@ class TestTracer:
         assert data.get("error") is ValueError
 
     def _check_node(
-        self,
-        node: Node,
-        module: str,
-        qualname: str,
-        duration: float,
-        outcome: Literal["success", "failure"],
-        /,
+        self, node: Node, module: str, qualname: str, duration: float, /
     ) -> None:
         assert node.tag == f"{module}:{qualname}"
         data = cast(_NodeData, node.data)
         assert data["module"] == module
         assert data["qualname"] == qualname
-        assert data["duration"].total_seconds() <= 2 * duration
-        assert data["outcome"] == outcome
+        assert data["duration"].total_seconds() == approx(duration, abs=1.0)
+        assert data["outcome"] == "success"
