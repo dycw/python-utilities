ec078b6a5c5bfd7860aba24c56a43902ff2a97f8
Derek Wan <d.wan@icloud.com>
2024-10-27 18:20:13 +0900

    update

--- src/utilities/treelib.py
+++ src/utilities/treelib.py
@@ -1,23 +1,65 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING, Generic, TypeVar
 
-from treelib import Node, Tree
+import treelib
+from typing_extensions import override
 
+from utilities.functions import ensure_not_none
 from utilities.text import ensure_str
 
 if TYPE_CHECKING:
     from collections.abc import Callable
 
+_T = TypeVar("_T")
+
+
+class Tree(treelib.Tree, Generic[_T]):
+    """Typed version of `Tree`."""
+
+    @override
+    def __getitem__(self, key: str) -> Node[_T]:
+        return super().__getitem__(key)
+
+    @override
+    def get_node(self, nid: str) -> Node[_T] | None:
+        return super().get_node(nid)
+
+
+class Node(treelib.Node, Generic[_T]):
+    """Typed version of `Node`."""
+
+    data: _T
+
+    @property
+    @override
+    def identifier(self) -> str:
+        return ensure_not_none(super().identifier)
+
+    @identifier.setter
+    @override
+    def identifier(self, value: str) -> None:
+        self._identifier = value
+
+    @property
+    @override
+    def tag(self) -> str | None:
+        return super().tag
+
+    @tag.setter
+    @override
+    def tag(self, value: str | None) -> None:
+        self._tag = value
+
 
 def filter_tree(
-    tree: Tree,
+    tree: Tree[_T],
     /,
     *,
     tag: Callable[[str], bool] | None = None,
     identifier: Callable[[str], bool] | None = None,
-    data: Callable[[Any], bool] | None = None,
-) -> Tree:
+    data: Callable[[_T], bool] | None = None,
+) -> Tree[_T]:
     """Filter a tree."""
     subtree = Tree()
     _filter_tree_add(
@@ -27,22 +69,22 @@ def filter_tree(
 
 
 def _filter_tree_add(
-    old: Tree,
-    new: Tree,
+    old: Tree[_T],
+    new: Tree[_T],
     node_id: str,
     /,
     *,
     parent_id: str | None = None,
     tag: Callable[[str], bool] | None = None,
     identifier: Callable[[str], bool] | None = None,
-    data: Callable[[Any], bool] | None = None,
+    data: Callable[[_T], bool] | None = None,
 ) -> None:
-    node = cast(Node, old.get_node(node_id))
+    node = old[node_id]
     predicates: set[bool] = set()
     if tag is not None:
         predicates.add(tag(ensure_str(node.tag)))
     if identifier is not None:
-        predicates.add(identifier(ensure_str(node.identifier)))
+        predicates.add(identifier(node.identifier))
     if data is not None:
         predicates.add(data(node.data))
     if all(predicates):
