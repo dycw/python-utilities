99bffd7669846a1ee10168640dfc7cc3236c25ef
Derek Wan <d.wan@icloud.com>
2024-11-04 09:02:05 +0900

    save

--- src/tests/test_loguru_functions.py
+++ src/tests/test_loguru_functions.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from loguru import logger
 from tenacity import retry, wait_fixed
 
-from utilities.loguru import LogLevel, log
 from utilities.tenacity import before_sleep_log
 
 # eventkit
@@ -11,60 +10,6 @@ from utilities.tenacity import before_sleep_log
 
 def func_test_eventkit(n: int, /) -> None:
     logger.trace("n={n}", n=n)
-
-
-# loguru
-
-
-def func_test_log_disable(x: int, /) -> int:
-    with log(disable=True):
-        return x + 1
-
-
-def func_test_log_main(x: int, /) -> int:
-    with log():
-        return x + 1
-
-
-def func_test_log_non_default_level(x: int, /) -> int:
-    with log(level=LogLevel.DEBUG):
-        return x + 1
-
-
-def func_test_log_context_outer(x: int, /) -> int:
-    with log(context={"context_key": "context_value"}):
-        return func_test_log_context_inner(x + 1)
-
-
-def func_test_log_context_inner(x: int, /) -> int:
-    with log(key="value"):
-        return x + 1
-
-
-def func_test_log_exit_variable(x: int, /) -> int:
-    with log() as log_cap:
-        return log_cap(x + 1)
-
-
-def func_test_log_exit_variable_disable(x: int, /) -> int:
-    with log(disable=True) as log_cap:
-        return log_cap(x + 1)
-
-
-def func_test_log_error(x: int, /) -> int | None:
-    with log():
-        if x % 2 == 0:
-            return x + 1
-        msg = f"Got an odd number: {x}"
-        raise ValueError(msg)
-
-
-def func_test_log_error_expected(x: int, /) -> int | None:
-    with log(error_expected=ValueError):
-        if x % 2 == 0:
-            return x + 1
-        msg = f"Got an odd number: {x}"
-        raise ValueError(msg)
 
 
 # tenacity
