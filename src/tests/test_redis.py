179da1c80edf1136e9df7958fcf31fc2bc8a2dfd
Derek Wan <d.wan@icloud.com>
2024-11-09 10:30:43 +0900

    save

--- src/tests/test_redis.py
+++ src/tests/test_redis.py
@@ -3,15 +3,14 @@ from __future__ import annotations
 from asyncio import get_running_loop, sleep
 from typing import TYPE_CHECKING
 
-import redis
-import redis.asyncio
 from hypothesis import HealthCheck, Phase, given, settings
 from hypothesis.strategies import DataObject, booleans, data
+from redis.asyncio import Redis
 
 from tests.conftest import SKIPIF_CI_AND_NOT_LINUX
 from tests.test_orjson2 import _Object, objects
 from utilities.functions import get_class_name
-from utilities.hypothesis import int64s, redis_cms, text_ascii
+from utilities.hypothesis import int64s, text_ascii, yield_test_redis
 from utilities.orjson import deserialize, serialize
 from utilities.redis import (
     RedisHashMapKey,
@@ -19,6 +18,7 @@ from utilities.redis import (
     publish,
     subscribe,
     subscribe_messages,
+    yield_redis,
 )
 
 if TYPE_CHECKING:
@@ -29,66 +29,42 @@ class TestRedisKey:
     @given(data=data(), value=booleans())
     @SKIPIF_CI_AND_NOT_LINUX
     async def test_main(self, *, data: DataObject, value: bool) -> None:
-        async with redis_cms(data) as container:
-            key = RedisKey(name=container.key, type=bool)
-            match container.client:
-                case redis.Redis():
-                    assert key.get(db=15) is None
-                    _ = key.set(value, db=15)
-                    assert key.get(db=15) is value
-                case redis.asyncio.Redis():
-                    assert await key.get_async(db=15) is None
-                    _ = await key.set_async(value, db=15)
-                    assert await key.get_async(db=15) is value
+        async with yield_test_redis(data) as test:
+            key = RedisKey(name=test.key, type=bool)
+            assert await key.get(test.redis) is None
+            _ = await key.set(test.redis, value)
+            assert await key.get(test.redis) is value
 
     @given(data=data(), value=booleans())
     @SKIPIF_CI_AND_NOT_LINUX
     async def test_using_client(self, *, data: DataObject, value: bool) -> None:
-        async with redis_cms(data) as container:
-            key = RedisKey(name=container.key, type=bool)
-            match container.client:
-                case redis.Redis() as client:
-                    assert key.get(client=client) is None
-                    _ = key.set(value, client=client)
-                    assert key.get(client=client) is value
-                case redis.asyncio.Redis() as client:
-                    assert await key.get_async(client=client) is None
-                    _ = await key.set_async(value, client=client)
-                    assert await key.get_async(client=client) is value
+        async with yield_test_redis(data) as test:
+            key = RedisKey(name=test.key, type=bool)
+            assert await key.get(test.redis) is None
+            _ = await key.set(test.redis, value)
+            assert await key.get(test.redis) is value
 
 
 class TestRedisHashMapKey:
     @given(data=data(), key=int64s(), value=booleans())
     @SKIPIF_CI_AND_NOT_LINUX
     async def test_main(self, *, data: DataObject, key: int, value: bool) -> None:
-        async with redis_cms(data) as container:
-            hash_map_key = RedisHashMapKey(name=container.key, key=int, value=bool)
-            match container.client:
-                case redis.Redis():
-                    assert hash_map_key.hget(key, db=15) is None
-                    _ = hash_map_key.hset(key, value, db=15)
-                    assert hash_map_key.hget(key, db=15) is value
-                case redis.asyncio.Redis():
-                    assert await hash_map_key.hget_async(key, db=15) is None
-                    _ = await hash_map_key.hset_async(key, value, db=15)
-                    assert await hash_map_key.hget_async(key, db=15) is value
+        async with yield_test_redis(data) as test:
+            hm_key = RedisHashMapKey(name=test.key, key=int, value=bool)
+            assert await hm_key.hget(test.redis, key) is None
+            _ = await hm_key.hset(test.redis, key, value)
+            assert await hm_key.hget(test.redis, key) is value
 
     @given(data=data(), key=int64s(), value=booleans())
     @SKIPIF_CI_AND_NOT_LINUX
     async def test_using_client(
         self, *, data: DataObject, key: int, value: bool
     ) -> None:
-        async with redis_cms(data) as container:
-            hash_map_key = RedisHashMapKey(name=container.key, key=int, value=bool)
-            match container.client:
-                case redis.Redis() as client:
-                    assert hash_map_key.hget(key, client=client) is None
-                    _ = hash_map_key.hset(key, value, client=client)
-                    assert hash_map_key.hget(key, client=client) is value
-                case redis.asyncio.Redis() as client:
-                    assert await hash_map_key.hget_async(key, client=client) is None
-                    _ = await hash_map_key.hset_async(key, value, client=client)
-                    assert await hash_map_key.hget_async(key, client=client) is value
+        async with yield_test_redis(data) as test:
+            hm_key = RedisHashMapKey(name=test.key, key=int, value=bool)
+            assert await hm_key.hget(test.redis, key) is None
+            _ = await hm_key.hset(test.redis, key, value)
+            assert await hm_key.hget(test.redis, key) is value
 
 
 class TestPublishAndSubscribe:
@@ -106,11 +82,12 @@ class TestPublishAndSubscribe:
     async def test_main(
         self, *, capsys: CaptureFixture, channel: str, obj: _Object
     ) -> None:
-        client = redis.asyncio.Redis()
+        client = Redis()
+        await subscribe(client.pubsub(), channel, deserializer=deserialize)
 
         async def listener() -> None:
             async for msg in subscribe(
-                channel, pubsub=client.pubsub(), deserializer=deserialize
+                client.pubsub(), channel, deserializer=deserialize
             ):
                 print(msg)  # noqa: T201
 
@@ -142,10 +119,10 @@ class TestSubscribeMessages:
     async def test_main(
         self, *, capsys: CaptureFixture, channel: str, message: str
     ) -> None:
-        client = redis.asyncio.Redis()
+        client = Redis()
 
         async def listener() -> None:
-            async for msg in subscribe_messages(channel, pubsub=client.pubsub()):
+            async for msg in subscribe_messages(client.pubsub(), channel):
                 print(msg)  # noqa: T201
 
         task = get_running_loop().create_task(listener())
@@ -158,4 +135,9 @@ class TestSubscribeMessages:
             assert out == expected
         finally:
             _ = task.cancel()
-            await client.aclose()
+
+
+class TestYieldClient:
+    async def test_sync(self) -> None:
+        async with yield_redis() as client:
+            assert isinstance(client, Redis)
