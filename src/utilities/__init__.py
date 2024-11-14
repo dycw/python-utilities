780b0302e6b17fd56ef8bd06ddcedf3e33820fea
Derek Wan <d.wan@icloud.com>
2024-11-15 08:29:37 +0900

    Save

--- src/utilities/__init__.py
+++ src/utilities/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-__version__ = "0.69.2"
+__version__ = "0.69.3"
--- src/utilities/tenacity.py
+++ src/utilities/tenacity.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Literal, cast
+from typing import TYPE_CHECKING
 
-import tenacity
-from tenacity import RetryCallState
 from tenacity import wait_exponential_jitter as _wait_exponential_jitter
 from tenacity._utils import MAX_WAIT
 from typing_extensions import override
@@ -11,24 +9,7 @@ from typing_extensions import override
 from utilities.datetime import duration_to_float
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
-
-    from utilities.loguru import LogLevel
     from utilities.types import Duration
-
-_INFO: LogLevel = cast(Any, "INFO")
-
-
-def before_sleep_log(
-    *, level: LogLevel = _INFO, exc_info: bool = False
-) -> Callable[[RetryCallState], None]:
-    """Use `loguru` in around `before_sleep_log`."""
-    from utilities.loguru import get_logging_level_number
-
-    level_num = get_logging_level_number(level)
-    return tenacity.before_sleep_log(
-        cast(Any, _LoguruAdapter()), level_num, exc_info=exc_info
-    )
 
 
 class wait_exponential_jitter(_wait_exponential_jitter):  # noqa: N801
@@ -50,23 +31,4 @@ class wait_exponential_jitter(_wait_exponential_jitter):  # noqa: N801
         )
 
 
-class _LoguruAdapter:
-    """Proxy for `loguru`, for use in `tenacity`."""
-
-    def log(
-        self,
-        level: int,
-        msg: Any,
-        /,
-        *,
-        exc_info: BaseException | Literal[False] | None = None,
-    ) -> None:
-        from loguru import logger
-
-        from utilities.loguru import get_logging_level_name
-
-        level_name = get_logging_level_name(level)
-        logger.opt(exception=exc_info).log(level_name, msg)
-
-
-__all__ = ["before_sleep_log", "wait_exponential_jitter"]
+__all__ = ["wait_exponential_jitter"]
