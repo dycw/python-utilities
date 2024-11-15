a487800fa77c815cb1b776aafc401ba211fc418f
Derek Wan <d.wan@icloud.com>
2024-11-15 09:19:00 +0900

    save

--- src/tests/test_git.py
+++ src/tests/test_git.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from pathlib import Path
+from typing import TYPE_CHECKING
 
 from hypothesis import given
 from hypothesis.strategies import DataObject, data
@@ -11,14 +11,11 @@ from utilities.git import (
     get_branch_name,
     get_repo_name,
     get_repo_root,
-    get_repo_root_or_cwd_sub_path,
 )
-from utilities.hypothesis import (
-    git_repos,
-    settings_with_reduced_examples,
-    temp_paths,
-    text_ascii,
-)
+from utilities.hypothesis import git_repos, settings_with_reduced_examples, text_ascii
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 
 class TestGetBranchName:
@@ -50,46 +47,3 @@ class TestGetRepoRoot:
             GetRepoRootError, match="Path is not part of a `git` repository: .*"
         ):
             _ = get_repo_root(cwd=tmp_path)
-
-
-class TestGetRepoRootOrCwdSubPath:
-    @given(root=git_repos())
-    @settings_with_reduced_examples()
-    def test_exists(self, *, root: Path) -> None:
-        def get_file(root: Path, /) -> Path:
-            return Path(root, "file.txt")
-
-        result = get_repo_root_or_cwd_sub_path(get_file, cwd=root)
-        expected = Path(root, "file.txt").resolve()
-        assert result == expected
-
-    @given(root=temp_paths())
-    def test_does_not_exist(self, *, root: Path) -> None:
-        def get_file(root: Path, /) -> Path:
-            return Path(root, "file.txt")
-
-        result = get_repo_root_or_cwd_sub_path(get_file, cwd=root)
-        assert result is None
-
-    @given(root=temp_paths())
-    def test_missing(self, *, root: Path) -> None:
-        def get_file_1(root: Path, /) -> Path:
-            return Path(root, "file_1.txt")
-
-        def get_file_2(root: Path, /) -> Path:
-            return Path(root, "file_2.txt")
-
-        result = get_repo_root_or_cwd_sub_path(
-            get_file_1, cwd=root, if_missing=get_file_2
-        )
-        expected = Path(root, "file_2.txt")
-        assert result == expected
-
-
-class TestValidRepoPath:
-    @given(root=git_repos(), file_name=text_ascii())
-    @settings_with_reduced_examples()
-    def test_main(self, *, root: Path, file_name: str) -> None:
-        result = get_repo_root(cwd=root).joinpath(file_name)
-        expected = Path(root.resolve(), file_name)
-        assert result == expected
--- src/utilities/git.py
+++ src/utilities/git.py
@@ -4,20 +4,16 @@ from dataclasses import dataclass
 from pathlib import Path
 from re import IGNORECASE, search
 from subprocess import PIPE, CalledProcessError, check_output
-from typing import TYPE_CHECKING, TypeVar, overload
+from typing import TYPE_CHECKING
 
 from typing_extensions import override
 
 from utilities.pathlib import PWD
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
-
     from utilities.types import PathLike
 
 _GET_BRANCH_NAME = ["git", "rev-parse", "--abbrev-ref", "HEAD"]
-_T = TypeVar("_T")
-_U = TypeVar("_U")
 
 
 def get_branch_name(*, cwd: PathLike = PWD) -> str:
@@ -61,39 +57,4 @@ class GetRepoRootError(Exception):
         return f"Path is not part of a `git` repository: {self.cwd}"
 
 
-@overload
-def get_repo_root_or_cwd_sub_path(
-    if_exists: Callable[[Path], _T], /, *, cwd: PathLike = ..., if_missing: None = ...
-) -> _T | None: ...
-@overload
-def get_repo_root_or_cwd_sub_path(
-    if_exists: Callable[[Path], _T],
-    /,
-    *,
-    cwd: PathLike = ...,
-    if_missing: Callable[[Path], _U] = ...,
-) -> _T | _U: ...
-def get_repo_root_or_cwd_sub_path(
-    if_exists: Callable[[Path], _T],
-    /,
-    *,
-    cwd: PathLike = PWD,
-    if_missing: Callable[[Path], _U] | None = None,
-) -> _T | _U | None:
-    """Get a path under the repo root, if it exists, else under the CWD."""
-    try:
-        root = get_repo_root(cwd=cwd)
-    except (FileNotFoundError, GetRepoRootError):
-        if if_missing is None:
-            return None
-        return if_missing(Path(cwd))
-    return if_exists(root)
-
-
-__all__ = [
-    "GetRepoRootError",
-    "get_branch_name",
-    "get_repo_name",
-    "get_repo_root",
-    "get_repo_root_or_cwd_sub_path",
-]
+__all__ = ["GetRepoRootError", "get_branch_name", "get_repo_name", "get_repo_root"]
