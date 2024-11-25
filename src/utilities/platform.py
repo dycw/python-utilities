66d19e491cfb3eb33a0c303f1d89c25e2b57f0b0
Derek Wan <d.wan@icloud.com>
2024-11-25 09:19:41 +0900

    save

--- src/utilities/platform.py
+++ src/utilities/platform.py
@@ -1,34 +1,26 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from enum import StrEnum, unique
 from platform import system
-from typing import TYPE_CHECKING, assert_never
+from typing import TYPE_CHECKING, Literal, assert_never
 
 from typing_extensions import override
 
 if TYPE_CHECKING:
     from collections.abc import Iterable, Iterator
 
-
-@unique
-class System(StrEnum):
-    """An enumeration of the systems."""
-
-    windows = "windows"
-    mac = "mac"
-    linux = "linux"
+System = Literal["windows", "mac", "linux"]
 
 
 def get_system() -> System:
     """Get the system/OS name."""
     sys = system()
     if sys == "Windows":  # skipif-not-windows
-        return System.windows
+        return "windows"
     if sys == "Darwin":  # skipif-not-macos
-        return System.mac
+        return "mac"
     if sys == "Linux":  # skipif-not-linux
-        return System.linux
+        return "linux"
     raise GetSystemError(sys=sys)  # pragma: no cover
 
 
@@ -44,9 +36,9 @@ class GetSystemError(Exception):
 
 
 SYSTEM = get_system()
-IS_WINDOWS = SYSTEM is System.windows
-IS_MAC = SYSTEM is System.mac
-IS_LINUX = SYSTEM is System.linux
+IS_WINDOWS = SYSTEM == "windows"
+IS_MAC = SYSTEM == "mac"
+IS_LINUX = SYSTEM == "linux"
 IS_NOT_WINDOWS = not IS_WINDOWS
 IS_NOT_MAC = not IS_MAC
 IS_NOT_LINUX = not IS_LINUX
@@ -55,11 +47,11 @@ IS_NOT_LINUX = not IS_LINUX
 def maybe_yield_lower_case(text: Iterable[str], /) -> Iterator[str]:
     """Yield lower-cased text if the platform is case-insentive."""
     match SYSTEM:
-        case System.windows:  # skipif-not-windows
+        case "windows":  # skipif-not-windows
             yield from (t.lower() for t in text)
-        case System.mac:  # skipif-not-macos
+        case "mac":  # skipif-not-macos
             yield from (t.lower() for t in text)
-        case System.linux:  # skipif-not-linux
+        case "linux":  # skipif-not-linux
             yield from text
         case _ as never:  # pyright: ignore[reportUnnecessaryComparison]
             assert_never(never)
