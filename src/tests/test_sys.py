3a5fcc062eefbab726ff15b5b4aa2d6c5ab3e57d
Derek Wan <d.wan@icloud.com>
2024-11-30 16:47:53 +0900

    save

--- src/tests/test_sys.py
+++ src/tests/test_sys.py
@@ -1,6 +1,38 @@
 from __future__ import annotations
 
+import sys
+from subprocess import CalledProcessError, check_output
+
+from pytest import raises
+
 from utilities.sys import VERSION_MAJOR_MINOR
+
+
+class TestExceptHook:
+    def test_custom_excepthook(self) -> None:
+        code = """
+import sys
+from io import StringIO
+
+buffer = StringIO()
+
+def custom_excepthook(exc_type, exc_value, exc_traceback):
+    print("Custom exception hook called", file=buffer)
+    print(f"Exception type: {exc_type}", file=buffer)
+    print(f"Exception value: {exc_value}", file=buffer)
+    print(buffer.getvalue())
+
+sys.excepthook = custom_excepthook
+
+raise ValueError("Test exception")
+    """
+
+        # Run the code in a subprocess and capture the output
+        with raises(CalledProcessError) as exc_info:
+            _ = check_output([sys.executable, "-c", code], text=True)
+        error = exc_info.value
+        assert "Custom exception hook called" in error.stdout
+        assert "Exception value: Test exception" in error.stdout
 
 
 class TestVersionMajorMinor:
