b1b26c7ed7bbce77c2bcd1a3cb54fc015a3e668d
Derek Wan <d.wan@icloud.com>
2024-12-01 13:45:30 +0900

    save

--- src/utilities/foo.py
+++ src/utilities/foo.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 import sys
 from functools import partial
+from itertools import chain
 from logging import getLogger
 
-from utilities.foo_funcs import func_decorated_first
 from utilities.sys import log_exception_paths
 from utilities.traceback import trace
 
@@ -13,13 +13,25 @@ sys.excepthook = partial(log_exception_paths, logger=_LOGGER)
 
 
 @trace
-def calls_func_first(a: int, b: int, /, *args: int, c: int = 0, **kwargs: int) -> int:
+def first(a: int, b: int, /, *args: int, c: int = 0, **kwargs: int) -> int:
     a *= 2
     b *= 2
     args = tuple(2 * arg for arg in args)
     c *= 2
     kwargs = {k: 2 * v for k, v in kwargs.items()}
-    return func_decorated_first(a, b, *args, c, **kwargs)
+    return second(a, b, *args, c, **kwargs)
 
 
-_ = calls_func_first(1, 2, 3, 4, c=5, d=6, e=7)
+@trace
+def second(a: int, b: int, /, *args: int, c: int = 0, **kwargs: int) -> int:
+    a *= 2
+    b *= 2
+    args = tuple(2 * arg for arg in args)
+    c *= 2
+    kwargs = {k: 2 * v for k, v in kwargs.items()}
+    result = sum(chain([a, b], args, [c], kwargs.values()))
+    assert result % 10 == 0, f"Result ({result}) must be divisible by 10"
+    return result
+
+
+_ = first(1, 2, 3, 4, c=5, d=6, e=7)
