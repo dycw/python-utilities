37c788f9d1dd1f8521cdc48ac55d3fc68b1538cf
Derek Wan <d.wan@icloud.com>
2024-12-01 18:27:58 +0900

    save

--- src/tests/test_beartype.py
+++ src/tests/test_beartype.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 from asyncio import sleep
+from dataclasses import dataclass
 from typing import Any, cast
 
-from beartype.roar import BeartypeCallHintReturnViolation
+from beartype.roar import (
+    BeartypeCallHintParamViolation,
+    BeartypeCallHintReturnViolation,
+)
 from pytest import raises
 
 from utilities.beartype import beartype_cond
@@ -48,3 +52,24 @@ class TestBeartypeCond:
         enable = True
         with raises(BeartypeCallHintReturnViolation):
             _ = await func(1, 2)
+
+    def test_class_main(self) -> None:
+        @beartype_cond
+        @dataclass(kw_only=True, slots=True)
+        class Example:
+            x: int = 0
+
+        with raises(BeartypeCallHintParamViolation):
+            _ = Example(x=cast(Any, "0"))
+        _ = Example(x=0)
+
+    def test_class_enable(self) -> None:
+        enable = True
+
+        @beartype_cond(enable=lambda: enable)
+        @dataclass(kw_only=True, slots=True)
+        class Example:
+            x: int = 0
+
+        with raises(BeartypeCallHintParamViolation):
+            _ = Example(x=cast(Any, "0"))
