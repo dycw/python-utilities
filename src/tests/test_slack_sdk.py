e581515f4cb970aea5560fcd7630e99e2d826851
Derek Wan <d.wan@icloud.com>
2024-12-02 21:43:13 +0900

    Add `SlackHandler` (#929)

--- src/tests/test_slack_sdk.py
+++ src/tests/test_slack_sdk.py
@@ -1,16 +1,34 @@
 from __future__ import annotations
 
-from slack_sdk.webhook import WebhookClient
+from asyncio import sleep
+from logging import DEBUG, getLogger
+
+from aiohttp import InvalidUrlClientError
+from pytest import raises
 from slack_sdk.webhook.async_client import AsyncWebhookClient
 
-from utilities.slack_sdk import _get_client_async, _get_client_sync
+from utilities.slack_sdk import SlackHandler, _get_client, send_to_slack
 
 
 class TestGetClient:
-    def test_sync(self) -> None:
-        client = _get_client_sync("url")
-        assert isinstance(client, WebhookClient)
-
-    def test_async(self) -> None:
-        client = _get_client_async("url")
+    def test_main(self) -> None:
+        client = _get_client("url")
         assert isinstance(client, AsyncWebhookClient)
+
+
+class TestSendToSlack:
+    async def test_main(self) -> None:
+        with raises(InvalidUrlClientError, match="url"):
+            await send_to_slack("url", "message")
+
+
+class TestSlackHandler:
+    async def test_main(self) -> None:
+        name = TestSlackHandler.test_main.__qualname__
+        logger = getLogger(name)
+        logger.setLevel(DEBUG)
+        handler = SlackHandler("url")
+        handler.setLevel(DEBUG)
+        logger.addHandler(handler)
+        logger.debug("message")
+        await sleep(0.1)
--- src/utilities/slack_sdk.py
+++ src/utilities/slack_sdk.py
@@ -1,47 +1,104 @@
 from __future__ import annotations
 
+from asyncio import CancelledError, Lock, Queue, QueueEmpty, Task, get_event_loop
 from dataclasses import dataclass
 from http import HTTPStatus
+from logging import NOTSET, Handler, LogRecord, getLogger
 from typing import TYPE_CHECKING
 
-from slack_sdk.webhook import WebhookClient, WebhookResponse
 from slack_sdk.webhook.async_client import AsyncWebhookClient
 from typing_extensions import override
 
-from utilities.datetime import MINUTE, duration_to_float
+from utilities.asyncio import sleep_dur
+from utilities.datetime import MINUTE, SECOND, duration_to_float
 from utilities.functools import cache
 from utilities.math import safe_round
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from slack_sdk.webhook import WebhookResponse
+
     from utilities.types import Duration
 
+_LOGGER = getLogger(__name__)
 _TIMEOUT = MINUTE
+_FREQ = SECOND
 
 
-def send_slack_sync(text: str, /, *, url: str, timeout: Duration = _TIMEOUT) -> None:
-    """Send a message to Slack, synchronously."""
-    client = _get_client_sync(url, timeout=timeout)  # pragma: no cover
-    response = client.send(text=text)  # pragma: no cover
-    _check_status_code(text, response)  # pragma: no cover
+class SlackHandler(Handler):
+    """Handler for sending messages to Slack."""
+
+    _url: str
+    _timeout: Duration
+    _lock: Lock
+    _callback: Callable[[], None] | None = None
+    _queue: Queue[str]
+    _task: Task[None]
+
+    @override
+    def __init__(
+        self,
+        url: str,
+        /,
+        *,
+        level: int = NOTSET,
+        timeout: Duration = _TIMEOUT,
+        freq: Duration = _FREQ,
+        callback: Callable[[], None] | None = None,
+    ) -> None:
+        super().__init__(level=level)
+        self._url = url
+        self._timeout = timeout
+        self._freq = freq
+        self._callback = callback
+        self._lock = Lock()
+        self._queue = Queue()
+        self._task = get_event_loop().create_task(self._process_queue())
+
+    @override
+    def emit(self, record: LogRecord) -> None:
+        try:
+            self._queue.put_nowait(self.format(record))
+        except Exception:  # noqa: BLE001  # pragma: no cover
+            self.handleError(record)
+
+    async def _process_queue(self) -> None:
+        while True:
+            messages: list[str] = []
+            async with self._lock:
+                while True:
+                    try:
+                        messages.append(self._queue.get_nowait())
+                    except QueueEmpty:
+                        break
+            if len(messages) >= 1:  # pragma: no cover
+                _LOGGER.debug("Sending %s messages(s)", len(messages))
+                text = "\n".join(messages)
+                try:
+                    await send_to_slack(self._url, text, timeout=self._timeout)
+                except CancelledError:
+                    break
+                except Exception:
+                    _LOGGER.exception("Slack handler error")
+                else:
+                    if self._callback is not None:
+                        self._callback()
+            await sleep_dur(duration=self._freq)
 
 
-async def send_slack_async(
-    text: str, /, *, url: str, timeout: Duration = _TIMEOUT
+async def send_to_slack(
+    url: str, text: str, /, *, timeout: Duration = _TIMEOUT
 ) -> None:
     """Send a message via Slack."""
-    client = _get_client_async(url, timeout=timeout)  # pragma: no cover
-    response = await client.send(text=text)  # pragma: no cover
-    _check_status_code(text, response)  # pragma: no cover
-
-
-def _check_status_code(text: str, response: WebhookResponse, /) -> None:
-    """Check that a chunk was successfully sent."""
+    client = _get_client(url, timeout=timeout)
+    response = await client.send(text=text)
     if response.status_code != HTTPStatus.OK:  # pragma: no cover
-        raise SendSlackError(text=text, response=response)
+        raise SendToSlackError(text=text, response=response)
 
 
 @dataclass(kw_only=True, slots=True)
-class SendSlackError(Exception):
+class SendToSlackError(Exception):
     text: str
     response: WebhookResponse
 
@@ -53,19 +110,10 @@ class SendSlackError(Exception):
 
 
 @cache
-def _get_client_sync(url: str, /, *, timeout: Duration = _TIMEOUT) -> WebhookClient:
-    """Get the webhook client."""
-    timeout_use = safe_round(duration_to_float(timeout))
-    return WebhookClient(url, timeout=timeout_use)
-
-
-@cache
-def _get_client_async(
-    url: str, /, *, timeout: Duration = _TIMEOUT
-) -> AsyncWebhookClient:
-    """Get the engine/sessionmaker for the required database."""
+def _get_client(url: str, /, *, timeout: Duration = _TIMEOUT) -> AsyncWebhookClient:
+    """Get the Slack client."""
     timeout_use = safe_round(duration_to_float(timeout))
     return AsyncWebhookClient(url, timeout=timeout_use)
 
 
-__all__ = ["send_slack_async", "send_slack_sync"]
+__all__ = ["SendToSlackError", "SlackHandler", "send_to_slack"]
