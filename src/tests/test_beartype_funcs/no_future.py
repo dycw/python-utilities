64b7282db0ae5fa1555312c2079afd7f6d760295
Derek Wan <d.wan@icloud.com>
2024-12-04 14:19:19 +0900

    save

--- src/tests/test_beartype_funcs/no_future.py
+++ src/tests/test_beartype_funcs/no_future.py
@@ -1,1 +1,15 @@
-asdf
+from dataclasses import dataclass
+
+from utilities.beartype import beartype_cond
+
+
+@beartype_cond
+@dataclass(kw_only=True, slots=True)
+class Outer:
+    inner: "Inner"
+
+
+@beartype_cond
+@dataclass(kw_only=True, slots=True)
+class Inner:
+    x: int = 0
