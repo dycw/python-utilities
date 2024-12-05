6cc0ef8f852ed15367de43dc855329f94cd51959
Derek Wan <d.wan@icloud.com>
2024-12-05 20:28:13 +0900

    save

--- src/tests/test_uuid.py
+++ src/tests/test_uuid.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
+from re import search
 from typing import TYPE_CHECKING
 
 from hypothesis import given
-from hypothesis.strategies import uuids
+from hypothesis.strategies import sampled_from, uuids
 
-from utilities.uuid import UUID_PATTERN
+from utilities.uuid import UUID_EXACT_PATTERN, UUID_PATTERN
 
 if TYPE_CHECKING:
     from uuid import UUID
 
 
 class TestUUIDPattern:
-    @given(uuid=uuids())
-    def test_main(self, *, uuid: UUID) -> None:
-        assert UUID_PATTERN.match(str(uuid))
+    @given(pattern=sampled_from([UUID_PATTERN, UUID_EXACT_PATTERN]), uuid=uuids())
+    def test_main(self, *, pattern: str, uuid: UUID) -> None:
+        assert search(pattern, str(uuid))
