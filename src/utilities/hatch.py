23e0e7a82aa801a4fed25e5fb2bc5c00ce4479ae
Derek Wan <d.wan@icloud.com>
2024-12-13 21:39:01 +0900

    save

--- src/utilities/hatch.py
+++ src/utilities/hatch.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
-from subprocess import check_output
+from subprocess import CalledProcessError, check_output
 
 
 def get_version() -> str | None:
     """Get the version."""
-    result = check_output(["hatch", "version"], text=True)
-    return None if result is None else result.strip("\n")
+    try:
+        result = check_output(["hatch", "version"], text=True)
+    except CalledProcessError:  # pragma: no cover
+        return None
+    return result.strip("\n")
 
 
 __all__ = ["get_version"]
