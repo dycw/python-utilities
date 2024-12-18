d65b50bb3d806012058cf3454c9e33a3d35dfdc6
Derek Wan <d.wan@icloud.com>
2024-12-18 20:33:02 +0900

    save

--- src/tests/test_polars.py
+++ src/tests/test_polars.py
@@ -1032,7 +1032,6 @@ class TestMapOverDataFrame:
         expected = 2 * df
         assert_frame_equal(result, expected)
 
-    @mark.only
     def test_dataframe_nested(self) -> None:
         df = DataFrame(
             data=[
