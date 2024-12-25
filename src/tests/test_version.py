7168a733e4b55fc21c3ed04b618023e2563f67e3
Derek Wan <d.wan@icloud.com>
2024-12-25 10:27:24 +0900

    save

--- src/tests/test_version.py
+++ src/tests/test_version.py
@@ -1,9 +1,19 @@
 from __future__ import annotations
-from hypothesis import given,integers
 
-from re import search
+from hypothesis import given
+from hypothesis.strategies import integers, none
+from pytest import raises
 
-from utilities.version import Version, get_hatch_version
+from utilities.hypothesis import text_ascii
+from utilities.version import (
+    Version,
+    _VersionEmptySuffixError,
+    _VersionNegativeMajorVersionError,
+    _VersionNegativeMinorVersionError,
+    _VersionNegativePatchVersionError,
+    get_hatch_version,
+    parse_version,
+)
 
 
 class TestGetHatchVersion:
@@ -11,5 +21,45 @@ class TestGetHatchVersion:
         version = get_hatch_version()
         assert isinstance(version, Version)
 
+
 class TestParseVersion:
-    @given(major=integers(min_value=0))
+    @given(
+        major=integers(min_value=0),
+        minor=integers(min_value=0),
+        patch=integers(min_value=0),
+        suffix=text_ascii(min_size=1) | none(),
+    )
+    def test_main(
+        self, *, major: int, minor: int, patch: int, suffix: str | None
+    ) -> None:
+        version = Version(major=major, minor=minor, patch=patch, suffix=suffix)
+        parsed = parse_version(str(version))
+        assert parsed == version
+
+    @given(major=integers(max_value=-1))
+    def test_error_negative_major_version(self, *, major: int) -> None:
+        with raises(
+            _VersionNegativeMajorVersionError,
+            match="Major version must be non-negative; got .*",
+        ):
+            _ = Version(major=major)
+
+    @given(minor=integers(max_value=-1))
+    def test_error_negative_minor_version(self, *, minor: int) -> None:
+        with raises(
+            _VersionNegativeMinorVersionError,
+            match="Minor version must be non-negative; got .*",
+        ):
+            _ = Version(minor=minor)
+
+    @given(patch=integers(max_value=-1))
+    def test_error_negative_patch_version(self, *, patch: int) -> None:
+        with raises(
+            _VersionNegativePatchVersionError,
+            match="Patch version must be non-negative; got .*",
+        ):
+            _ = Version(patch=patch)
+
+    def test_error_empy_suffix(self) -> None:
+        with raises(_VersionEmptySuffixError, match="Suffix must be non-empty; got .*"):
+            _ = Version(suffix="")
--- src/utilities/version.py
+++ src/utilities/version.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
+import re
 from dataclasses import dataclass
 from subprocess import CalledProcessError, check_output
 from typing import TYPE_CHECKING, Self
 
 from typing_extensions import override
 
-from utilities.git import get_ref_tags
 from utilities.pathlib import PWD
 
 if TYPE_CHECKING:
     from utilities.types import PathLike
+
+
+_PATTERN = re.compile(r"^(\d+)\.(\d+)\.(\d+)(?:-(\w+))?")
 
 
 @dataclass(repr=False, kw_only=True, slots=True)
@@ -25,6 +28,12 @@ class Version:
     def __post_init__(self) -> None:
         if self.major < 0:
             raise _VersionNegativeMajorVersionError(major=self.major)
+        if self.minor < 0:
+            raise _VersionNegativeMinorVersionError(minor=self.minor)
+        if self.patch < 0:
+            raise _VersionNegativePatchVersionError(patch=self.patch)
+        if (self.suffix is not None) and (len(self.suffix) == 0):
+            raise _VersionEmptySuffixError(suffix=self.suffix)
 
     @override
     def __repr__(self) -> str:
@@ -41,7 +50,47 @@ class Version:
 
     def bump_patch(self) -> Self:
         return type(self)(major=self.major, minor=self.minor, patch=self.patch + 1)
+
+
 @dataclass(kw_only=True, slots=True)
+class VersionError(Exception): ...
+
+
+@dataclass(kw_only=True, slots=True)
+class _VersionNegativeMajorVersionError(VersionError):
+    major: int
+
+    @override
+    def __str__(self) -> str:
+        return f"Major version must be non-negative; got {self.major}"
+
+
+@dataclass(kw_only=True, slots=True)
+class _VersionNegativeMinorVersionError(VersionError):
+    minor: int
+
+    @override
+    def __str__(self) -> str:
+        return f"Minor version must be non-negative; got {self.minor}"
+
+
+@dataclass(kw_only=True, slots=True)
+class _VersionNegativePatchVersionError(VersionError):
+    patch: int
+
+    @override
+    def __str__(self) -> str:
+        return f"Patch version must be non-negative; got {self.patch}"
+
+
+@dataclass(kw_only=True, slots=True)
+class _VersionEmptySuffixError(VersionError):
+    suffix: str
+
+    @override
+    def __str__(self) -> str:
+        return f"Suffix must be non-empty; got {self.suffix!r}"
+
 
 def get_git_origin_master_version() -> Version:
     """Get the version according to the `git` `origin/master` tag."""
@@ -55,7 +104,7 @@ def get_hatch_version() -> Version:
 
 def get_version(*, cwd: PathLike = PWD) -> str:
     """Get the version."""
-    hatch_version = get_hatch_version()
+    get_hatch_version()
     try:
         output = check_output(["hatch", "version"], text=True)
     except CalledProcessError:  # pragma: no cover
@@ -65,6 +114,30 @@ def get_version(*, cwd: PathLike = PWD) -> str:
 
 def parse_version(version: str, /) -> Version:
     """Parse a string into a version object."""
+    result = _PATTERN.search(version)
+    if not result:
+        raise ParseVersionError(version=version)
+    major_str, minor_str, patch_str, suffix = result.groups()
+    return Version(
+        major=int(major_str), minor=int(minor_str), patch=int(patch_str), suffix=suffix
+    )
 
 
-__all__ = ["get_git_origin_master_version", "get_hatch_version", "get_version"]
+@dataclass(kw_only=True, slots=True)
+class ParseVersionError(Exception):
+    version: str
+
+    @override
+    def __str__(self) -> str:
+        return f"Invalid version string: {self.version!r}"
+
+
+__all__ = [
+    "ParseVersionError",
+    "Version",
+    "VersionError",
+    "get_git_origin_master_version",
+    "get_hatch_version",
+    "get_version",
+    "parse_version",
+]
