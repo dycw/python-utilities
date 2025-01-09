890fff878ceb0e3eb32ec8300aad2f6038cb7f69
Derek Wan <d.wan@icloud.com>
2025-01-09 18:35:21 +0900

    Saved at 2025-01-09 18:35:21 (Thu)

--- src/utilities/traceback.py
+++ src/utilities/traceback.py
@@ -490,54 +490,42 @@ def get_rich_traceback(
             raise ImpossibleCaseError(case=[f"{error}"])
         case [err]:
             err_recast = cast(_TBaseExc, err)
-            return _get_rich_traceback_one(err_recast, git_ref=git_ref)
+            return _get_rich_traceback_non_chain(err_recast, git_ref=git_ref)
         case errs:
             errs_recast = cast(list[_TBaseExc], errs)
             return ExcChainTB(
                 errors=[
-                    _get_rich_traceback_one(e, git_ref=git_ref) for e in errs_recast
+                    _get_rich_traceback_non_chain(e, git_ref=git_ref)
+                    for e in errs_recast
                 ],
                 git_ref=git_ref,
             )
 
 
-def _get_rich_traceback_one(
+def _get_rich_traceback_non_chain(
     error: ExceptionGroup[Any] | _TBaseExc, /, *, git_ref: str = MASTER
 ) -> ExcGroupTB[_TBaseExc] | ExcTB[_TBaseExc] | _TBaseExc:
     """Get a rich traceback, for a non-chained error."""
     match error:
         case ExceptionGroup() as exc_group:
-            exc_group_or_exc_tb = _get_rich_traceback_base_exception(exc_group)
+            exc_group_or_exc_tb = _get_rich_traceback_base_one(exc_group)
             errors = [
-                _get_rich_traceback_one(e, git_ref=git_ref)
+                _get_rich_traceback_non_chain(e, git_ref=git_ref)
                 for e in always_iterable(exc_group.exceptions)
             ]
             return ExcGroupTB(
                 exc_group=exc_group_or_exc_tb, errors=errors, git_ref=git_ref
             )
         case BaseException() as base_exc:
-            if isinstance(base_exc, _HasExceptionPath):
-                frames = [
-                    _Frame(
-                        module=f.module,
-                        name=f.name,
-                        code_line=f.code_line,
-                        line_num=f.line_num,
-                        args=f.extra.args,
-                        kwargs=f.extra.kwargs,
-                        locals=f.locals,
-                    )
-                    for f in base_exc.exc_tb.frames
-                ]
-                return ExcTB(frames=frames, error=base_exc, git_ref=git_ref)
-            return error
+            return _get_rich_traceback_base_one(base_exc, git_ref=git_ref)
         case _ as never:
             assert_never(never)
 
 
-def _get_rich_traceback_base_exception(
+def _get_rich_traceback_base_one(
     error: _TBaseExc, /, *, git_ref: str = MASTER
 ) -> ExcTB[_TBaseExc] | _TBaseExc:
+    """Get a rich traceback, for a single exception."""
     if isinstance(error, _HasExceptionPath):
         frames = [
             _Frame(
@@ -552,7 +540,7 @@ def _get_rich_traceback_base_exception(
             for f in error.exc_tb.frames
         ]
         return ExcTB(frames=frames, error=error, git_ref=git_ref)
-    return error  # pragma: no cover
+    return error
 
 
 @overload
