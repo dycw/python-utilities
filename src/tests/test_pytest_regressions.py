d171d52c707b02fd3332cd8adfac50a5f21517dd
Derek Wan <d.wan@icloud.com>
2025-01-23 09:48:18 +0900

    Saved at 2025-01-23 09:48:18 (Thu)

--- src/tests/test_pytest_regressions.py
+++ src/tests/test_pytest_regressions.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
-from tests.test_operator import DataClass1
+from hypothesis import HealthCheck, given, settings
+from hypothesis.strategies import sampled_from
+
+from tests.test_operator import DataClass1, DataClass2Inner, DataClass2Outer, DataClass3
 from utilities.pytest_regressions import orjson_regression_fixture
 
 if TYPE_CHECKING:
@@ -18,3 +21,20 @@ class TestOrjsonRegressionFixture:
     ) -> None:
         obj = DataClass1(x=0)
         orjson_regression_fixture.check(obj)
+
+    def test_dataclass2(
+        self, *, orjson_regression_fixture: OrjsonRegressionFixture
+    ) -> None:
+        obj = DataClass2Outer(inner=DataClass2Inner(x=0))
+        orjson_regression_fixture.check(obj)
+
+    @given(truth=sampled_from(["true", "false"]))
+    @settings(suppress_health_check={HealthCheck.function_scoped_fixture})
+    def test_dataclass3(
+        self,
+        *,
+        truth: Literal["true", "false"],
+        orjson_regression_fixture: OrjsonRegressionFixture,
+    ) -> None:
+        obj = DataClass3(truth=truth)
+        orjson_regression_fixture.check(obj, suffix=truth)
--- src/utilities/pytest_regressions.py
+++ src/utilities/pytest_regressions.py
@@ -21,37 +21,23 @@ _PATH_TESTS = Path("src", "tests")
 class OrjsonRegressionFixture:
     """Implementation of `orjson_regression` fixture."""
 
-    def __init__(
-        self,
-        *,
-        path_tests: PathLike | None = None,
-        node_id_path: PathLike | None = None,
-        request: FixtureRequest,
-    ) -> None:
+    def __init__(self, path: PathLike, /, *, request: FixtureRequest) -> None:
         super().__init__()
-        path_tests_use = _get_path_tests() if path_tests is None else Path(path_tests)
-        if node_id_path is not None:
-            path_use = path_tests_use.joinpath(node_id_path)
+        path = Path(path)
+        datadir = path.parent
         self._file_regression = FileRegressionFixture(
-            datadir=path_use, original_datadir=path_use, request=request
+            datadir=datadir, original_datadir=datadir, request=request
         )
+        self._basename = path.name
 
-    def check(
-        self,
-        obj: Any,
-        /,
-        *,
-        request_and_suffix: tuple[FixtureRequest, str] | None = None,
-    ) -> None:
+    def check(self, obj: Any, /, *, suffix: str | None = None) -> None:
         """Serialize the object and compare it to a previously saved baseline."""
         from utilities.orjson import serialize
 
         data = serialize(obj)
-        if request_and_suffix is None:
-            basename = None
-        else:
-            request, suffix = request_and_suffix
-            basename = f"{request.node.name}_{suffix}"
+        basename = self._basename
+        if suffix is not None:
+            basename = f"{basename}__{suffix}"
         self._file_regression.check(
             data, extension=".json", basename=basename, binary=True
         )
@@ -60,13 +46,6 @@ class OrjsonRegressionFixture:
 @fixture
 def orjson_regression_fixture(*, request: FixtureRequest) -> OrjsonRegressionFixture:
     """Fixture to provide an instance of ObjectRegressionFixture using path_regression."""
-    path_tests = _get_path_tests()
-    node_id_path = node_id_to_path(request.node.nodeid, ".json", head=_PATH_TESTS)
-    return OrjsonRegressionFixture(
-        path_tests=path_tests, node_id_path=node_id_path, request=request
-    )
-
-
-def _get_path_tests() -> Path:
-    """Get the path to the tests folder."""
-    return get_repo_root().joinpath(_PATH_TESTS)
+    tail = node_id_to_path(request.node.nodeid, head=_PATH_TESTS)
+    path = get_repo_root().joinpath(_PATH_TESTS, "regressions", tail)
+    return OrjsonRegressionFixture(path, request=request)
