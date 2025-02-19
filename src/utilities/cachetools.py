8e4096cc1a47fdbae54f36929ae117e99e2ea8f8
Derek Wan <d.wan@icloud.com>
2025-02-19 11:46:56 +0900

    Saved at 2025-02-19 11:46:56 (Wed)

--- src/utilities/cachetools.py
+++ src/utilities/cachetools.py
@@ -3,15 +3,13 @@ from __future__ import annotations
 from collections.abc import Callable, Iterable, Iterator, MutableSet
 from math import inf
 from time import monotonic
-from typing import TYPE_CHECKING, Any, TypeVar, cast
+from typing import TYPE_CHECKING, Any, TypeVar
 
 import cachetools
 from cachetools.func import ttl_cache
 from typing_extensions import override
 
 from utilities.datetime import datetime_duration_to_float
-from utilities.functions import identity
-from utilities.functools import lru_cache
 
 if TYPE_CHECKING:
     from utilities.types import Duration
@@ -99,14 +97,19 @@ class TTLSet(MutableSet[_T]):
 
 
 def cache(
-    *, max_size: int | None = None, max_duration: Duration | None = None
+    *,
+    max_size: int | None = None,
+    max_duration: Duration | None = None,
+    timer: Callable[[], float] = monotonic,
+    typed_: bool = False,
 ) -> Callable[[_F], _F]:
     """Decorate a function with `max_size` and/or `ttl` settings."""
-    if max_duration is not None:
-        return ttl_cache(maxsize=max_size, ttl=datetime_duration_to_float(max_duration))
-    if max_size is not None:
-        return cast(Any, lru_cache(max_size=max_size))
-    return identity
+    return ttl_cache(
+        maxsize=inf if max_size is None else max_size,
+        ttl=inf if max_duration is None else datetime_duration_to_float(max_duration),
+        timer=timer,
+        typed=typed_,
+    )
 
 
 __all__ = ["TTLCache", "TTLSet", "cache"]
