2b09c4ae88410cef5c16a3d5348475fcfcc7ac1b
Derek Wan <d.wan@icloud.com>
2025-02-26 12:29:05 +0900

    Add `missing_ok` to `yield_modules` and related functions (#1091)

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -5,9 +5,17 @@ from operator import le, lt
 from re import search
 from typing import TYPE_CHECKING, Any
 
-from pytest import mark, param
+from hypothesis import given
+from hypothesis.strategies import sampled_from
+from pytest import raises
 
-from tests.modules import package_with, package_without, standalone, with_imports
+from tests.modules import (
+    package_missing,
+    package_with,
+    package_without,
+    standalone,
+    with_imports,
+)
 from utilities.functions import get_class_name
 from utilities.modules import (
     is_installed,
@@ -22,66 +30,70 @@ if TYPE_CHECKING:
 
 
 class TestIsInstalled:
-    @mark.parametrize(
-        ("module", "expected"), [param("importlib", True), param("invalid", False)]
-    )
-    def test_main(self, *, module: str, expected: int) -> None:
+    @given(case=sampled_from([("importlib", True), ("invalid", False)]))
+    def test_main(self, *, case: tuple[str, int]) -> None:
+        module, expected = case
         result = is_installed(module)
         assert result is expected
 
 
 class TestYieldModules:
-    @mark.parametrize(
-        ("module", "recursive", "expected"),
-        [
-            param(standalone, False, 1),
-            param(standalone, True, 1),
-            param(package_without, False, 2),
-            param(package_without, True, 2),
-            param(package_with, False, 3),
-            param(package_with, True, 6),
-        ],
+    @given(
+        case=sampled_from([
+            (standalone, False, 1),
+            (standalone, True, 1),
+            (package_without, False, 2),
+            (package_without, True, 2),
+            (package_with, False, 3),
+            (package_with, True, 6),
+        ])
     )
-    def test_main(self, *, module: ModuleType, recursive: bool, expected: int) -> None:
+    def test_main(self, *, case: tuple[ModuleType, bool, int]) -> None:
+        module, recursive, expected = case
         res = list(yield_modules(module, recursive=recursive))
         assert len(res) == expected
 
+    def test_missing_ok(self) -> None:
+        _ = list(
+            yield_modules(
+                package_missing, missing_ok=["missing_package"], recursive=True
+            )
+        )
+
+    def test_error(self) -> None:
+        with raises(ModuleNotFoundError, match="No module named 'missing_package'"):
+            _ = list(yield_modules(package_missing, recursive=True))
+
 
 class TestYieldModuleContents:
-    @mark.parametrize(
-        ("module", "recursive", "factor"),
-        [
-            param(standalone, False, 1),
-            param(standalone, True, 1),
-            param(package_without, False, 2),
-            param(package_without, True, 2),
-            param(package_with, False, 2),
-            param(package_with, True, 5),
-        ],
-    )
-    @mark.parametrize(
-        ("type_", "predicate", "expected"),
-        [
-            param(int, None, 3),
-            param(float, None, 3),
-            param((int, float), None, 6),
-            param(type, None, 3),
-            param(int, partial(le, 0), 2),
-            param(int, partial(lt, 0), 1),
-            param(float, partial(le, 0), 2),
-            param(float, partial(lt, 0), 1),
-        ],
+    @given(
+        case1=sampled_from([
+            (standalone, False, 1),
+            (standalone, True, 1),
+            (package_without, False, 2),
+            (package_without, True, 2),
+            (package_with, False, 2),
+            (package_with, True, 5),
+        ]),
+        case2=sampled_from([
+            (int, None, 3),
+            (float, None, 3),
+            ((int, float), None, 6),
+            (type, None, 3),
+            (int, partial(le, 0), 2),
+            (int, partial(lt, 0), 1),
+            (float, partial(le, 0), 2),
+            (float, partial(lt, 0), 1),
+        ]),
     )
     def test_main(
         self,
         *,
-        module: ModuleType,
-        type_: type[Any] | tuple[type[Any], ...] | None,
-        recursive: bool,
-        predicate: Callable[[Any], bool],
-        expected: int,
-        factor: int,
+        case1: tuple[ModuleType, bool, int],
+        case2: tuple[type[Any] | tuple[type[Any], ...], Callable[[Any], bool], int],
     ) -> None:
+        module, recursive, factor = case1
+        type_, predicate, expected = case2
         res = list(
             yield_module_contents(
                 module, type=type_, recursive=recursive, predicate=predicate
@@ -94,42 +106,37 @@ class TestYieldModuleSubclasses:
     def predicate(self: Any, /) -> bool:
         return bool(search("1", get_class_name(self)))
 
-    @mark.parametrize(
-        ("module", "recursive", "factor"),
-        [
-            param(standalone, False, 1),
-            param(standalone, True, 1),
-            param(package_without, False, 2),
-            param(package_without, True, 2),
-            param(package_with, False, 2),
-            param(package_with, True, 5),
-        ],
-    )
-    @mark.parametrize(
-        ("type_", "predicate", "expected"),
-        [
-            param(int, None, 1),
-            param(int, predicate, 0),
-            param(float, None, 2),
-            param(float, predicate, 1),
-        ],
+    @given(
+        case1=sampled_from([
+            (standalone, False, 1),
+            (standalone, True, 1),
+            (package_without, False, 2),
+            (package_without, True, 2),
+            (package_with, False, 2),
+            (package_with, True, 5),
+        ]),
+        case2=sampled_from([
+            (int, None, 1),
+            (int, predicate, 0),
+            (float, None, 2),
+            (float, predicate, 1),
+        ]),
     )
     def test_main(
         self,
         *,
-        module: ModuleType,
-        type_: type[Any],
-        recursive: bool,
-        predicate: Callable[[type[Any]], bool],
-        expected: int,
-        factor: int,
+        case1: tuple[ModuleType, bool, int],
+        case2: tuple[type[Any], Callable[[Any], bool], int],
     ) -> None:
+        module, recursive, factor = case1
+        type_, predicate, expected = case2
         it = yield_module_subclasses(
             module, type_, recursive=recursive, predicate=predicate
         )
         assert len(list(it)) == (factor * expected)
 
-    @mark.parametrize(("is_module", "expected"), [param(True, 1), param(False, 2)])
-    def test_is_module(self, *, is_module: bool, expected: int) -> None:
+    @given(case=sampled_from([(True, 1), (False, 2)]))
+    def test_is_module(self, *, case: tuple[bool, int]) -> None:
+        is_module, expected = case
         it = yield_module_subclasses(with_imports, object, is_module=is_module)
         assert len(list(it)) == expected
