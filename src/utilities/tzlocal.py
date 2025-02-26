51e7c6c867634535cec0c02a967e58dd1c383376
Derek Wan <d.wan@icloud.com>
2025-02-26 12:53:59 +0900

    save

--- src/utilities/tzlocal.py
+++ src/utilities/tzlocal.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
-from logging import getLogger
 from typing import TYPE_CHECKING
 
 from tzlocal import get_localzone
+
+from utilities.logging import temp_logger
 
 if TYPE_CHECKING:
     from zoneinfo import ZoneInfo
 
 
 def get_local_time_zone() -> ZoneInfo:
-    logger = getLogger("tzlocal")
-    return get_localzone()
+    with temp_logger("tzlocal", disabled=True):
+        return get_localzone()
 
 
 __all__ = ["get_local_time_zone"]
