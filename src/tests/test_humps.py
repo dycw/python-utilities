4a884bcebefbd9d79d94c49bf3f95d76d70f41b5
Derek Wan <d.wan@icloud.com>
2025-03-07 11:35:40 +0900

    Saved at 2025-03-07 11:35:40 (Fri)

--- src/tests/test_humps.py
+++ src/tests/test_humps.py
@@ -1,37 +1,57 @@
 from __future__ import annotations
 
-from pytest import mark, param
+from hypothesis import given
+from hypothesis.strategies import sampled_from
 
 from utilities.humps import snake_case
 
 
 class TestSnakeCase:
-    @mark.parametrize(
-        ("text", "expected"),
-        [
-            param("Product", "product"),
-            param("SpecialGuest", "special_guest"),
-            param("ApplicationController", "application_controller"),
-            param("Area51Controller", "area51_controller"),
-            param("HTMLTidy", "html_tidy"),
-            param("HTMLTidyGenerator", "html_tidy_generator"),
-            param("FreeBSD", "free_bsd"),
-            param("HTML", "html"),
-            param("text", "text"),
-            param("Text", "text"),
-            param("text123", "text123"),
-            param("Text123", "text123"),
-            param("OneTwo", "one_two"),
-            param("One Two", "one_two"),
-            param("One  Two", "one_two"),
-            param("One   Two", "one_two"),
-            param("One_Two", "one_two"),
-            param("One__Two", "one_two"),
-            param("One___Two", "one_two"),
-            param("NoHTML", "no_html"),
-            param("HTMLVersion", "html_version"),
-        ],
+    @given(
+        case=sampled_from([
+            ("API", "api"),
+            ("APIResponse", "api_response"),
+            ("ApplicationController", "application_controller"),
+            ("Area51Controller", "area51_controller"),
+            ("FreeBSD", "free_bsd"),
+            ("HTML", "html"),
+            ("HTMLTidy", "html_tidy"),
+            ("HTMLTidyGenerator", "html_tidy_generator"),
+            ("HTMLVersion", "html_version"),
+            ("NoHTML", "no_html"),
+            ("One   Two", "one_two"),
+            ("One  Two", "one_two"),
+            ("One Two", "one_two"),
+            ("OneTwo", "one_two"),
+            ("One_Two", "one_two"),
+            ("One__Two", "one_two"),
+            ("One___Two", "one_two"),
+            ("Product", "product"),
+            ("SpecialGuest", "special_guest"),
+            ("Text", "text"),
+            ("Text123", "text123"),
+            ("_APIResponse_", "_api_response_"),
+            ("_API_", "_api_"),
+            ("__APIResponse__", "_api_response_"),
+            ("__API__", "_api_"),
+            ("__impliedVolatility_", "_implied_volatility_"),
+            ("_itemID", "_item_id"),
+            ("_lastPrice__", "_last_price_"),
+            ("_symbol", "_symbol"),
+            ("aB", "a_b"),
+            ("changePct", "change_pct"),
+            ("changePct_", "change_pct_"),
+            ("impliedVolatility", "implied_volatility"),
+            ("lastPrice", "last_price"),
+            ("memMB", "mem_mb"),
+            ("sizeX", "size_x"),
+            ("symbol", "symbol"),
+            ("testNTest", "test_n_test"),
+            ("text", "text"),
+            ("text123", "text123"),
+        ])
     )
-    def test_main(self, *, text: str, expected: str) -> None:
+    def test_main(self, *, case: tuple[str, str]) -> None:
+        text, expected = case
         result = snake_case(text)
         assert result == expected
--- src/utilities/humps.py
+++ src/utilities/humps.py
@@ -1,13 +1,19 @@
 from __future__ import annotations
 
+import re
 from re import search
 
-from humps import decamelize
+_ACRONYM_PATTERN = re.compile(r"([A-Z\d]+)(?=[A-Z\d]|$)")
+_SPACES_PATTERN = re.compile(r"\s+")
+_SPLIT_PATTERN = re.compile(r"([\-_]*[A-Z][^A-Z]*[\-_]*)")
 
 
 def snake_case(text: str, /) -> str:
     """Convert text into snake case."""
-    text = decamelize(text)
+    text = _SPACES_PATTERN.sub("", text)
+    if not text.isupper():
+        text = _ACRONYM_PATTERN.sub(lambda m: m.group(0).title(), text)
+        text = "_".join(s for s in _SPLIT_PATTERN.split(text) if s)
     while search("__", text):
         text = text.replace("__", "_")
     return text.lower()
