5a1e624074c99f73f057cb13117554a6e577f8af
Derek Wan <d.wan@icloud.com>
2025-03-12 14:36:50 +0900

    Saved at 2025-03-12 14:36:50 (Wed)

--- src/tests/test_contextvars.py
+++ src/tests/test_contextvars.py
@@ -4,6 +4,7 @@ from bdb import BdbQuit
 
 from pytest import raises
 
+from tests.conftest import SKIPIF_CI
 from utilities.contextvars import (
     _GLOBAL_BREAKPOINT,
     global_breakpoint,
@@ -16,6 +17,7 @@ class TestGlobalBreakpoint:
         _ = _GLOBAL_BREAKPOINT.set(False)
         global_breakpoint()
 
+    @SKIPIF_CI
     def test_enabled(self) -> None:
         set_global_breakpoint()
         with raises(BdbQuit, match=""):
