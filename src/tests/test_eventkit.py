89dcf2294a868c1a9d5efedca42c46435b13f461
Derek Wan <d.wan@icloud.com>
2025-03-27 17:40:26 +0900

    Saved at 2025-03-27 17:40:26 (Thu)

--- src/tests/test_eventkit.py
+++ src/tests/test_eventkit.py
@@ -1,24 +1,24 @@
 from __future__ import annotations
 
 from asyncio import sleep
-from functools import wraps
 from io import StringIO
 from logging import DEBUG, StreamHandler, getLogger
 from re import search
 from typing import TYPE_CHECKING, Literal, ParamSpec, TypeVar
 
 from eventkit import Event
-from hypothesis import HealthCheck, given
+from hypothesis import HealthCheck, given, settings
 from hypothesis.strategies import integers, sampled_from
 from pytest import CaptureFixture, mark
 
 from utilities.eventkit import add_listener
 from utilities.functions import identity
-from utilities.hypothesis import settings_with_reduced_examples, text_ascii
+from utilities.hypothesis import settings_with_reduced_examples, temp_paths, text_ascii
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
     from pathlib import Path
+
+    from pytest import CaptureFixture
 
 
 _P = ParamSpec("_P")
@@ -52,9 +52,41 @@ _R = TypeVar("_R")
         await sleep(0.01)
         assert counter == n
 
+    @given(root=temp_paths(), sync_or_async=sampled_from(["sync", "async"]))
+    @settings(suppress_health_check={HealthCheck.function_scoped_fixture})
+    @mark.only
+    async def test_no_error_handler_but_run_into_error(
+        self, *, root: Path, sync_or_async: Literal["sync", "async"]
+    ) -> None:
+        logger = getLogger(str(root))
+        logger.setLevel(DEBUG)
+        handler = StreamHandler(buffer := StringIO())
+        handler.setLevel(DEBUG)
+        logger.addHandler(handler)
+        event = Event()
+
+        match sync_or_async:
+            case "sync":
+
+                def listener_sync() -> None: ...
+
+                _ = add_listener(event, listener_sync, error_logger=str(root))
+            case "async":
+
+                async def listener_async() -> None:
+                    await sleep(0.01)
+
+                _ = add_listener(event, listener_async, error_logger=str(root))
+
+        event.emit(None)
+        await sleep(0.01)
+        pattern = r"listener_a?sync\(\) takes 0 positional arguments but 1 was given"
+        contents = buffer.getvalue()
+        assert search(pattern, contents)
+
     @given(name=text_ascii(min_size=1), n=integers(min_value=1))
     @mark.only
-    def test_custom_error_handler(self, *, name: str, n: int) -> None:
+    def test_with_error_handler(self, *, name: str, n: int) -> None:
         event = Event(name=name)
         assert event.name() == name
         counter = 0
@@ -83,69 +115,7 @@ _R = TypeVar("_R")
     @given(
         name=text_ascii(min_size=1), case=sampled_from(["sync", "async/sync", "async"])
     )
-    async def test_with_error_handler(
-        self, *, name: str, case: Literal["sync", "async/sync", "async"]
-    ) -> None:
-        event = Event(name=name)
-        assert event.name() == name
-        called = False
-        log: set[tuple[str, type[Exception]]] = set()
-
-        def listener_sync(is_success: bool, /) -> None:  # noqa: FBT001
-            if is_success:
-                nonlocal called
-                called |= True
-            else:
-                raise ValueError
-
-        def error_sync(event: Event, exception: Exception, /) -> None:
-            nonlocal log
-            log.add((event.name(), type(exception)))
-
-        async def listener_async(is_success: bool, /) -> None:  # noqa: FBT001
-            if is_success:
-                nonlocal called
-                called |= True
-                await sleep(0.01)
-            else:
-                raise ValueError
-
-        async def error_async(event: Event, exception: Exception, /) -> None:
-            nonlocal log
-            log.add((event.name(), type(exception)))
-            await sleep(0.01)
-
-        match case:
-            case "sync":
-                _ = add_listener(event, listener_sync, error=error_sync)
-            case "async/sync":
-                _ = add_listener(event, listener_async, error=error_sync)
-            case "async":
-                _ = add_listener(event, listener_async, error=error_async)
-        event.emit(True)  # noqa: FBT003
-        await sleep(0.01)
-        assert called
-        assert log == set()
-        event.emit(False)  # noqa: FBT003
-        await sleep(0.01)
-        assert log == {(name, ValueError)}
-
-    @given(
-        case=sampled_from([
-            "no/sync",
-            "no/async",
-            "have/sync",
-            "have/async/sync",
-            "have/async",
-        ])
-    )
-    async def test_ignore(
-        self,
-        *,
-        case: Literal[
-            "no/sync", "no/async", "have/sync", "have/async/sync", "have/async"
-        ],
-    ) -> None:
+    async def test_error_ignore(self, *, capsys: CaptureFixture, n: int) -> None:
         event = Event()
         called = False
         log: set[tuple[str, type[Exception]]] = set()
