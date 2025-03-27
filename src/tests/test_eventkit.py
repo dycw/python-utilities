c24a95797909dfb9a432a7de6449e80c0834c64b
Derek Wan <d.wan@icloud.com>
2025-03-27 16:34:21 +0900

    Saved at 2025-03-27 16:34:21 (Thu)

--- src/tests/test_eventkit.py
+++ src/tests/test_eventkit.py
@@ -7,11 +7,11 @@ from typing import TYPE_CHECKING, ClassVar, Literal
 from eventkit import Event
 from hypothesis import HealthCheck, given
 from hypothesis.strategies import integers, sampled_from
-from pytest import CaptureFixture
+from pytest import CaptureFixture, mark
 
 from utilities.eventkit import add_listener
 from utilities.functions import identity
-from utilities.hypothesis import settings_with_reduced_examples
+from utilities.hypothesis import settings_with_reduced_examples, text_ascii
 
 if TYPE_CHECKING:
     from pytest import CaptureFixture
@@ -20,44 +20,61 @@ if TYPE_CHECKING:
 class TestAddListener:
     datetime: ClassVar[str] = r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \| "
 
+    @mark.only
     @given(sync_or_async=sampled_from(["sync", "async"]), n=integers())
     async def test_main(
         self, *, sync_or_async: Literal["sync", "async"], n: int
     ) -> None:
         event = Event()
+        counter = 0
         match sync_or_async:
             case "sync":
 
                 def listener_sync(n: int, /) -> None:
-                    print(f"n={n}")  # noqa: T201
+                    nonlocal counter
+                    counter += n
 
                 _ = add_listener(event, listener_sync)
             case "async":
 
                 async def listener_async(n: int, /) -> None:
+                    nonlocal counter
+                    counter += n
                     await sleep(0.01)
-                    print(f"n={n}")  # noqa: T201
 
                 _ = add_listener(event, listener_async)
 
         event.emit(n)
+        await sleep(0.01)
+        assert counter == n
 
-    @given(n=integers())
-    @settings_with_reduced_examples(
-        suppress_health_check={HealthCheck.function_scoped_fixture}
-    )
-    def test_custom_error_handler(self, *, capsys: CaptureFixture, n: int) -> None:
-        event = Event()
+    @given(name=text_ascii(min_size=1), n=integers(min_value=1))
+    @mark.only
+    def test_custom_error_handler(self, *, name: str, n: int) -> None:
+        event = Event(name=name)
+        assert event.name() == name
+        counter = 0
+        log: set[tuple[str, type[Exception]]] = set()
+
+        def listener(n: int, /) -> None:
+            if n >= 0:
+                nonlocal counter
+                counter += n
+            else:
+                msg = "'n' must be non-negative"
+                raise ValueError(msg)
 
         def error(event: Event, exception: Exception, /) -> None:
-            _ = (event, exception)
-            print("Custom handler")  # noqa: T201
+            nonlocal log
+            log.add((event.name(), type(exception)))
 
-        _ = add_listener(event, identity, error=error)
-        event.emit(n, n)
-        out = capsys.readouterr().out
-        *_, line = out.splitlines()
-        assert line == "Custom handler"
+        _ = add_listener(event, listener, error=error)
+        event.emit(n)
+        assert counter == n
+        assert log == set()
+        event.emit(-n)
+        assert counter == n
+        assert log == {(name, ValueError)}
 
     @given(n=integers())
     @settings_with_reduced_examples(
