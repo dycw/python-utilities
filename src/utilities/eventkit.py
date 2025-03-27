72f16bc4292a956646374a144b8af2d7d55f32a0
Derek Wan <d.wan@icloud.com>
2025-03-27 16:06:44 +0900

    Saved at 2025-03-27 16:06:44 (Thu)

--- src/utilities/eventkit.py
+++ src/utilities/eventkit.py
@@ -1,56 +1,76 @@
 from __future__ import annotations
 
-import sys
-from functools import partial
-from typing import TYPE_CHECKING, Any
+from asyncio import iscoroutinefunction
+from functools import wraps
+from typing import TYPE_CHECKING, Any, cast
 
-from utilities.functions import get_class_name
+from utilities.functions import apply_decorators
+from utilities.iterables import always_iterable
 
 if TYPE_CHECKING:
     from collections.abc import Callable
 
     from eventkit import Event
 
+    from utilities.types import Coroutine1, MaybeCoroutine1, MaybeIterable, TCallable
+
 
 def add_listener(
     event: Event,
-    listener: Callable[..., Any],
+    listener: Callable[..., MaybeCoroutine1[None]],
     /,
     *,
-    error: Callable[[Event, Exception], None] | None = None,
+    error: Callable[[Event, Exception], MaybeCoroutine1[None]] | None = None,
     error_ignore: type[Exception] | tuple[type[Exception], ...] | None = None,
+    error_decorators: MaybeIterable[Callable[[TCallable], TCallable]] | None = None,
     done: Callable[..., Any] | None = None,
     keep_ref: bool = False,
 ) -> Event:
     """Connect a listener to an event."""
-    error_default = partial(_add_listener_error)
     if error is None:
-        error_use = partial(error_default, ignore=error_ignore)
+        listener_use = listener
+    elif (not iscoroutinefunction(listener)) and iscoroutinefunction(error):
+        raise ValueError
+    elif not iscoroutinefunction(listener):
+        listener_typed = cast("Callable[..., None]", listener)
+        error_typed = cast("Callable[[Event, Exception], None]", error)
+
+        @wraps(listener)
+        def listener_sync(*args: Any, **kwargs: Any) -> None:
+            try:
+                return listener_typed(*args, **kwargs)
+            except Exception as exc:  # noqa: BLE001
+                if (error_ignore is not None) and isinstance(exc, error_ignore):
+                    return None
+                return error_typed(event, exc)
+
+        listener_use = listener_sync
     else:
+        listener_typed = cast("Callable[..., Coroutine1[None]]", listener)
 
-        def combined(event: Event, exception: Exception, /) -> None:
-            error_default(event, exception, ignore=error_ignore)
-            error(event, exception)
+        @wraps(listener)
+        async def listener_async(*args: Any, **kwargs: Any) -> None:
+            try:
+                return await listener_typed(*args, **kwargs)
+            except Exception as exc:  # noqa: BLE001
+                if (error_ignore is not None) and isinstance(exc, error_ignore):
+                    return None
+                if iscoroutinefunction(error):
+                    error_typed = cast(
+                        "Callable[[Event, Exception], Coroutine1[None]]", error
+                    )
+                    return await error_typed(event, exc)
+                error_typed = cast("Callable[[Event, Exception], None]", error)
+                return error_typed(event, exc)
 
-        error_use = combined
-    return event.connect(listener, error=error_use, done=done, keep_ref=keep_ref)
+        listener_use = listener_async
 
+    if error_decorators is not None:
+        listener_use = apply_decorators(
+            listener_use, *always_iterable(error_decorators)
+        )
 
-def _add_listener_error(
-    event: Event,
-    exception: Exception,
-    /,
-    *,
-    ignore: type[Exception] | tuple[type[Exception], ...] | None = None,
-) -> None:
-    """Run callback in the case of an error."""
-    if (ignore is not None) and isinstance(exception, ignore):
-        return
-    type_name = get_class_name(exception)
-    event_name = event.name()
-    desc = f"Raised a {type_name} whilst running {event_name!r}"
-    msg = f"{desc}:\n{event=}\n{exception=}"
-    _ = sys.stdout.write(f"{msg}\n")
+    return event.connect(listener_use, done=done, keep_ref=keep_ref)
 
 
 __all__ = ["add_listener"]
