7094dd18fe54ed363ef922e1e307ad9212c5673e
Derek Wan <d.wan@icloud.com>
2025-03-29 19:41:38 +0900

    Saved at 2025-03-29 19:41:38 (Sat)

--- src/tests/test_logging.py
+++ src/tests/test_logging.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from io import StringIO
-from logging import DEBUG, NOTSET, FileHandler, Logger, StreamHandler, getLogger
+from logging import DEBUG, INFO, NOTSET, FileHandler, Logger, StreamHandler, getLogger
 from pathlib import Path
 from re import search
 from time import sleep
@@ -48,13 +48,13 @@ class TestAddFilters:
     @mark.parametrize("expected", [param(True), param(False)])
     def test_main(self, *, expected: bool, tmp_path: Path) -> None:
         logger = getLogger(str(tmp_path))
-        logger.setLevel(DEBUG)
         logger.addHandler(handler := StreamHandler(buffer := StringIO()))
-        handler.setLevel(DEBUG)
+        logger.setLevel(INFO)
         add_filters(handler, lambda _: expected)
         assert len(handler.filters) == 1
-        logger.debug("message")
-        assert (buffer.getvalue() != "") is expected
+        logger.info("message")
+        result = buffer.getvalue() != ""
+        assert result is expected
 
     def test_no_handlers(self) -> None:
         handler = StreamHandler()
@@ -64,9 +64,9 @@ class TestAddFilters:
 
 
 class TestBasicConfig:
-    def test_main(self) -> None:
+    def test_main(self, *, tmp_path: Path) -> None:
         basic_config()
-        logger = getLogger(__name__)
+        logger = getLogger(str(tmp_path))
         logger.info("message")
 
 
@@ -189,8 +189,8 @@ class TestGetDefaultLoggingPath:
 
 
 class TestGetLogger:
-    def test_logger(self) -> None:
-        logger = getLogger(__name__)
+    def test_logger(self, *, tmp_path: Path) -> None:
+        logger = getLogger(str(tmp_path))
         result = get_logger(logger=logger)
         assert result is logger
 
