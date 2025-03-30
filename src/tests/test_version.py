5df6c398207d65659c6169716acf7ad571f3c984
Derek Wan <d.wan@icloud.com>
2025-03-30 18:36:07 +0900

    Saved at 2025-03-30 18:36:07 (Sun)

--- src/tests/test_version.py
+++ src/tests/test_version.py
@@ -29,6 +29,10 @@ class TestGetVersion:
     def test_version(self, *, version: Version) -> None:
         assert get_version(version=version) == version
 
+    @given(version=versions())
+    def test_callable(self, *, version: Version) -> None:
+        assert get_version(version=lambda: version) == version
+
 
 class TestParseVersion:
     @given(version=versions())
