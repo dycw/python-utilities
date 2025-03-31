f6a2557ee38d05a69d70a1febe9e8cecee040369
Derek Wan <d.wan@icloud.com>
2025-03-31 12:46:52 +0900

    Saved at 2025-03-31 12:46:52 (Mon)

--- src/tests/scripts/test_queue_processor/__init__.py
+++ src/tests/scripts/test_queue_processor/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from asyncio import run, sleep
+from asyncio import CancelledError, run, sleep
 from logging import getLogger
 from typing import override
 
@@ -14,12 +14,14 @@ _LOGGER = getLogger(__name__)
 
 class Processor(QueueProcessor[int]):
     @override
-    async def _process_item(self, item: int, /) -> None:
-        _LOGGER.info("Processing %d; %d left...", item, len(self))
-        if bernoulli(true=0.33):
+    async def _process_item(self, _: int, /) -> None:
+        _LOGGER.info("Processing:   -> %d", len(self))
+        if bernoulli(true=0.25):
             msg = "Encountered a random failure!"
             raise ValueError(msg)
         await sleep(1)
+        if self.empty():
+            raise CancelledError
 
 
 def main() -> None:
@@ -30,14 +32,18 @@ def main() -> None:
 
 
 async def populate(processor: Processor, /) -> None:
-    while len(processor) <= 4:
+    while len(processor) <= 9:
         init = len(processor)
         processor.enqueue(SYSTEM_RANDOM.randint(0, 9))
         post = len(processor)
-        _LOGGER.info("%d -> %d items", init, post)
+        _LOGGER.info("Populating: %d -> %d", init, post)
         await sleep(0.1 + 0.4 * SYSTEM_RANDOM.random())
 
 
+def callback(_: int, error: Exception, /) -> None:
+    _LOGGER.error(get_class_name(error))
+
+
 async def _main() -> None:
-    async with Processor() as processor:
+    async with Processor(process_item_failure=callback) as processor:
         await populate(processor)
