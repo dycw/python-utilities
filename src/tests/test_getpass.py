b75fdc7e22643381074ce7c81ad208804c318dcd
Derek Wan <d.wan@icloud.com>
2025-04-11 11:39:52 +0900

    Saved at 2025-04-11 11:39:52 (Fri)

--- src/tests/test_getpass.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from utilities.getpass import USER
-
-
-class TestUser:
-    def test_main(self) -> None:
-        assert isinstance(USER, str)
--- src/tests/test_socket.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from utilities.socket import HOSTNAME
-
-
-class TestHostname:
-    def test_main(self) -> None:
-        assert isinstance(HOSTNAME, str)
