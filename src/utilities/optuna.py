94bd6c8e604c70fb0309f40bd86d0e2a01a07b07
Derek Wan <d.wan@icloud.com>
2025-04-14 10:56:34 +0900

    Add `create_sqlite_study` (#1230)

--- src/utilities/optuna.py
+++ src/utilities/optuna.py
@@ -1,18 +1,56 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
+from optuna import create_study
+from sqlalchemy import URL
+
 if TYPE_CHECKING:
-    from collections.abc import Callable
+    from collections.abc import Callable, Sequence
 
     from optuna import Study, Trial
+    from optuna.pruners import BasePruner
+    from optuna.samplers import BaseSampler
+    from optuna.study import StudyDirection
 
-    from utilities.types import TDataclass
+    from utilities.types import PathLike, TDataclass
+
+
+def create_sqlite_study(
+    path: PathLike,
+    /,
+    *,
+    sampler: BaseSampler | None = None,
+    pruner: BasePruner | None = None,
+    study_name: str | None = None,
+    direction: str | StudyDirection | None = None,
+    directions: Sequence[str | StudyDirection] | None = None,
+) -> Study:
+    """Create a study backed by SQLite."""
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    url = URL.create("sqlite", database=str(path))
+    return create_study(
+        storage=url.render_as_string(hide_password=False),
+        sampler=sampler,
+        pruner=pruner,
+        study_name=study_name,
+        direction=direction,
+        load_if_exists=True,
+        directions=directions,
+    )
+
+
+##
 
 
 def get_best_params(study: Study, cls: type[TDataclass], /) -> TDataclass:
     """Get the best params as a dataclass."""
     return cls(**study.best_params)
+
+
+##
 
 
 def make_objective(
@@ -28,9 +66,12 @@ def make_objective(
     return inner
 
 
+##
+
+
 def suggest_bool(trial: Trial, name: str, /) -> bool:
     """Suggest a boolean value."""
     return trial.suggest_categorical(name, [True, False])
 
 
-__all__ = ["get_best_params", "make_objective", "suggest_bool"]
+__all__ = ["create_sqlite_study", "get_best_params", "make_objective", "suggest_bool"]
