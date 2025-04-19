95da2b7b8a547b07195d2383a3207ebd5c98b888
Derek Wan <d.wan@icloud.com>
2025-04-19 22:47:12 +0900

    Saved at 2025-04-19 22:47:12 (Sat)

--- src/utilities/parse.py
+++ src/utilities/parse.py
@@ -2,22 +2,27 @@ from __future__ import annotations
 
 import datetime as dt
 from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
 from typing import Any, override
 
 from utilities.datetime import is_subclass_date_not_datetime
+from utilities.enum import ParseEnumError, parse_enum
 from utilities.functions import is_subclass_int_not_bool
 from utilities.text import ParseBoolError, parse_bool
-from utilities.whenever import ParseDateError
+from utilities.version import ParseVersionError, Version, parse_version
 
 
-def parse_text(cls: Any, text: str, /) -> Any:
+def parse_text(cls: Any, text: str, /, *, case_sensitive: bool = False) -> Any:
     """Parse text."""
     if isinstance(cls, type):
-        return _parse_text_type(cls, text)
+        return _parse_text_type(cls, text, case_sensitive=case_sensitive)
     return None
 
 
-def _parse_text_type(cls: type[Any], text: str, /) -> Any:
+def _parse_text_type(
+    cls: type[Any], text: str, /, *, case_sensitive: bool = False
+) -> Any:
     """Parse text."""
     if issubclass(cls, str):
         return text
@@ -36,8 +41,23 @@ def _parse_text_type(cls: type[Any], text: str, /) -> Any:
             return float(text)
         except ValueError:
             raise ParseTextError(cls=cls, text=text) from None
+    if issubclass(cls, Enum):
+        try:
+            return parse_enum(text, cls, case_sensitive=case_sensitive)
+        except ParseEnumError:
+            raise ParseTextError(cls=cls, text=text) from None
+    if issubclass(cls, Path):
+        try:
+            return Path(text).expanduser()
+        except TypeError:
+            raise ParseTextError(cls=cls, text=text) from None
+    if issubclass(cls, Version):
+        try:
+            return parse_version(text)
+        except ParseVersionError:
+            raise ParseTextError(cls=cls, text=text) from None
     if is_subclass_date_not_datetime(cls):
-        from utilities.whenever import parse_date
+        from utilities.whenever import ParseDateError, parse_date
 
         try:
             return parse_date(text)
@@ -64,8 +84,7 @@ def _parse_text_type(cls: type[Any], text: str, /) -> Any:
             return parse_timedelta(text)
         except ParseTimedeltaError:
             raise ParseTextError(cls=cls, text=text) from None
-
-    return None
+    raise ParseTextError(cls=cls, text=text) from None
 
 
 @dataclass
