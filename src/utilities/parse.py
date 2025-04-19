c41e88e7f21f43061b8361b92ceee2bfaf6c0f9a
Derek Wan <d.wan@icloud.com>
2025-04-19 22:29:12 +0900

    Saved at 2025-04-19 22:29:12 (Sat)

--- src/utilities/parse.py
+++ src/utilities/parse.py
@@ -5,8 +5,9 @@ from dataclasses import dataclass
 from typing import Any, override
 
 from utilities.datetime import is_subclass_date_not_datetime
+from utilities.functions import is_subclass_int_not_bool
 from utilities.text import ParseBoolError, parse_bool
-from utilities.whenever import ParseDateError, ParseLocalDateTimeError
+from utilities.whenever import ParseDateError
 
 
 def parse_text(cls: Any, text: str, /) -> Any:
@@ -20,7 +21,12 @@ def _parse_text_type(cls: type[Any], text: str, /) -> Any:
     """Parse text."""
     if issubclass(cls, str):
         return text
-    if issubclass(cls, bool):
+    if is_subclass_int_not_bool(cls, bool):
+        try:
+            return parse_bool(text)
+        except ParseBoolError:
+            raise ParseTextError(cls=cls, text=text) from None
+    if issubclass(cls, int):
         try:
             return parse_bool(text)
         except ParseBoolError:
@@ -33,11 +39,11 @@ def _parse_text_type(cls: type[Any], text: str, /) -> Any:
         except ParseDateError:
             raise ParseTextError(cls=cls, text=text) from None
     if issubclass(cls, dt.datetime):
-        from utilities.whenever import parse_local_datetime, parse_zoned_datetime
+        from utilities.whenever import ParseDateTimeError, parse_datetime
 
         try:
-            return parse_local_datetime(text)
-        except ParseLocalDateTimeError:
+            return parse_datetime(text)
+        except ParseDateTimeError:
             raise ParseTextError(cls=cls, text=text) from None
     if issubclass(cls, bool):
         return parse_bool(text)
