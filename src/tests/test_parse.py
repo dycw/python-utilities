0f4d402747065e61ab278754bc7dd5bf943851ba
Derek Wan <d.wan@icloud.com>
2025-04-19 23:21:06 +0900

    Saved at 2025-04-19 23:21:06 (Sat)

--- src/tests/test_parse.py
+++ src/tests/test_parse.py
@@ -1,15 +1,38 @@
 from __future__ import annotations
 
+import datetime as dt
 from pathlib import Path
 
 from hypothesis import given
-from hypothesis.strategies import booleans, floats, integers, sampled_from
+from hypothesis.strategies import (
+    booleans,
+    dates,
+    floats,
+    integers,
+    sampled_from,
+    times,
+    timezones,
+)
 from pytest import raises
 
 from tests.test_operator import TruthEnum
-from utilities.hypothesis import paths, text_ascii
+from utilities.hypothesis import (
+    local_datetimes,
+    paths,
+    text_ascii,
+    timedeltas_2w,
+    versions,
+    zoned_datetimes,
+)
 from utilities.math import is_equal
 from utilities.parse import ParseTextError, parse_text
+from utilities.version import Version
+from utilities.whenever import (
+    serialize_date,
+    serialize_datetime,
+    serialize_time,
+    serialize_timedelta,
+)
 
 
 class TestParseText:
@@ -18,6 +41,18 @@ class TestParseText:
         text = str(value)
         result = parse_text(bool, text)
         assert result is value
+
+    @given(date=dates())
+    def test_date(self, *, date: dt.date) -> None:
+        text = serialize_date(date)
+        result = parse_text(dt.date, text)
+        assert result == date
+
+    @given(datetime=local_datetimes() | zoned_datetimes(time_zone=timezones()))
+    def test_datetime(self, *, datetime: dt.datetime) -> None:
+        text = serialize_datetime(datetime)
+        result = parse_text(dt.datetime, text)
+        assert result == datetime
 
     @given(truth=sampled_from(TruthEnum))
     def test_enum(self, *, truth: TruthEnum) -> None:
@@ -48,11 +83,43 @@ class TestParseText:
         result = parse_text(str, text)
         assert result == text
 
+    @given(time=times())
+    def test_time(self, *, time: dt.time) -> None:
+        text = serialize_time(time)
+        result = parse_text(dt.time, text)
+        assert result == time
+
+    @given(timedelta=timedeltas_2w())
+    def test_timedelta(self, *, timedelta: dt.timedelta) -> None:
+        text = serialize_timedelta(timedelta)
+        result = parse_text(dt.timedelta, text)
+        assert result == timedelta
+
+    @given(version=versions())
+    def test_version(self, *, version: Version) -> None:
+        text = str(version)
+        result = parse_text(Version, text)
+        assert result == version
+
     def test_error_bool(self) -> None:
         with raises(
             ParseTextError, match="Unable to parse <class 'bool'>; got 'invalid'"
         ):
             _ = parse_text(bool, "invalid")
+
+    def test_error_date(self) -> None:
+        with raises(
+            ParseTextError,
+            match=r"Unable to parse <class 'datetime\.date'>; got 'invalid'",
+        ):
+            _ = parse_text(dt.date, "invalid")
+
+    def test_error_datetime(self) -> None:
+        with raises(
+            ParseTextError,
+            match=r"Unable to parse <class 'datetime\.datetime'>; got 'invalid'",
+        ):
+            _ = parse_text(dt.datetime, "invalid")
 
     def test_error_enum(self) -> None:
         with raises(
@@ -71,3 +138,24 @@ class TestParseText:
             ParseTextError, match="Unable to parse <class 'int'>; got 'invalid'"
         ):
             _ = parse_text(int, "invalid")
+
+    def test_error_time(self) -> None:
+        with raises(
+            ParseTextError,
+            match=r"Unable to parse <class 'datetime\.time'>; got 'invalid'",
+        ):
+            _ = parse_text(dt.time, "invalid")
+
+    def test_error_timedelta(self) -> None:
+        with raises(
+            ParseTextError,
+            match=r"Unable to parse <class 'datetime\.timedelta'>; got 'invalid'",
+        ):
+            _ = parse_text(dt.timedelta, "invalid")
+
+    def test_error_version(self) -> None:
+        with raises(
+            ParseTextError,
+            match=r"Unable to parse <class 'utilities\.version\.Version'>; got 'invalid'",
+        ):
+            _ = parse_text(Version, "invalid")
