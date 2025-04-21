622a83b9943d7221dd5c9cf7f8b6c911968c1c32
Derek Wan <d.wan@icloud.com>
2025-04-21 18:04:59 +0900

    Saved at 2025-04-21 18:04:59 (Mon)

--- src/utilities/python_dotenv.py
+++ src/utilities/python_dotenv.py
@@ -17,8 +17,7 @@ if TYPE_CHECKING:
     from collections.abc import Set as AbstractSet
     from pathlib import Path
 
-    from utilities.parse import _Extra
-    from utilities.types import PathLike, StrMapping, TDataclass
+    from utilities.types import ParseTextExtra, PathLike, StrMapping, TDataclass
 
 
 def load_settings(
@@ -31,7 +30,7 @@ def load_settings(
     warn_name_errors: bool = False,
     head: bool = False,
     case_sensitive: bool = False,
-    extra_parsers: _Extra | None = None,
+    extra_parsers: ParseTextExtra | None = None,
 ) -> TDataclass:
     """Load a set of settings from the `.env` file."""
     path = get_repo_root(cwd=cwd).joinpath(".env")
