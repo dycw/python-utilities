7f320bb9612d72c16ec085b83ed590fd32de4002
Derek Wan <d.wan@icloud.com>
2025-04-27 12:30:35 +0900

    Saved at 2025-04-27 12:30:35 (Sun)

--- src/utilities/types.py
+++ src/utilities/types.py
@@ -50,7 +50,7 @@ type MaybeStr[_T] = _T | str
 type MaybeType[_T] = _T | type[_T]
 type StrMapping = Mapping[str, Any]
 type StrStrMapping = Mapping[str, str]
-type TypeLike = type[Any] | tuple[type[Any], ...]
+type TypeLike[_T] = type[_T] | tuple[type[_T], ...]
 type TupleOrStrMapping = tuple[Any, ...] | StrMapping
 
 
