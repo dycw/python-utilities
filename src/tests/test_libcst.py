bec9dfd80950c2c42b94a54f54e8a47eef5f6f7e
Derek Wan <d.wan@icloud.com>
2025-05-25 16:42:27 +0900

    save

--- src/tests/test_libcst.py
+++ src/tests/test_libcst.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+from libcst import SimpleStatementLine
 
-class TestFoo:
+from utilities.libcst import generate_from_import
+
+
+class TestGenerateFromImport:
     def test_main(self) -> None:
-        pass
+        imp = generate_from_import("foo", "bar")
+        result = SimpleStatementLine([imp])
+        expected = "from foo import bar"
+        assert result == expected
