675949c020100a3b9ab694df45e040a939164aa7
Derek Wan <d.wan@icloud.com>
2025-05-25 16:24:26 +0900

    Commited by derek@DW-Swift at 2025-05-25 16:24:26 (Sun)

--- src/utilities/importlib.py
+++ src/utilities/importlib.py
@@ -4,11 +4,13 @@ from importlib import import_module
 from importlib.util import find_spec
 
 
-def is_valid_import(module: str, name: str) -> bool:
+def is_valid_import(module: str, /, *, name: str | None = None) -> bool:
     """Check if an import is valid."""
     spec = find_spec(module)
     if spec is None:
         return False
+    if name is None:
+        return True
     mod = import_module(module)
     return hasattr(mod, name)
 
