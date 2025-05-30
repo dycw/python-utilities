4de027b0643f7108b02d29326b7e6e21556d6e09
Derek Wan <d.wan@icloud.com>
2025-05-31 06:34:07 +0900

    Commited by derekwan@DW-Mac at 2025-05-31 06:34:07 (Sat)

--- src/utilities/contextlib.py
+++ src/utilities/contextlib.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 
+import re
+from contextlib import contextmanager
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator
     from types import TracebackType
 
 
 class NoOpContextManager:
+    """Context-manager for no-op."""
+
     def __enter__(self) -> None:
         return None
 
@@ -18,6 +23,22 @@ class NoOpContextManager:
     ) -> bool:
         _ = (exc_type, exc_val, traceback)
         return False
+
+
+##
+
+
+_SUPER_OBJECT_HAS_NO_ATTRIBUTE = re.compile(r"'super' object has no attribute '\w+'")
+
+
+@contextmanager
+def suppress_super_object_has_no_attribute() -> Iterator[None]:
+    """Suppress the super() attribute error, for mix-ins."""
+    try:
+        super().__post_init__()  # pyright: ignore[reportAttributeAccessIssue]
+    except AttributeError as error:
+        if not _SUPER_OBJECT_HAS_NO_ATTRIBUTE.search(error.args[0]):
+            raise
 
 
 __all__ = ["NoOpContextManager"]
