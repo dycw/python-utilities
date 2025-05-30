a3123ef3a7e292b552c96fd4f52a5d656f237798
Derek Wan <d.wan@icloud.com>
2025-05-31 07:39:34 +0900

    Commited by derekwan@DW-Mac at 2025-05-31 07:39:33 (Sat)

--- src/tests/test_psutil.py
+++ src/tests/test_psutil.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from pytest import approx, mark, param
+
 from utilities.datetime import MILLISECOND, SECOND
 from utilities.psutil import MemoryMonitorService
 
@@ -10,10 +12,19 @@ if TYPE_CHECKING:
 
 
 class TestMemoryMonitorService:
-    async def test_main(self, *, tmp_path: Path) -> None:
+    @mark.parametrize("console", [param(True), param(False)])
+    async def test_main(self, *, console: bool, tmp_path: Path) -> None:
         path = tmp_path.joinpath("memory.txt")
         service = MemoryMonitorService(
-            freq=10 * MILLISECOND, backoff=100 * MILLISECOND, timeout=SECOND, path=path
+            freq=100 * MILLISECOND,
+            backoff=100 * MILLISECOND,
+            timeout=SECOND,
+            path=path,
+            console=str(tmp_path) if console else None,
         )
         async with service.with_auto_start:
-            pass
+            ...
+        assert path.exists()
+        with path.open() as fh:
+            lines = fh.readlines()
+        assert len(lines) == approx(10, rel=0.5)
