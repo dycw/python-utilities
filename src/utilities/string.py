f05c32a71a64d4adda800973bf87d62babc7c6ac
Derek Wan <d.wan@icloud.com>
2025-05-30 09:01:48 +0900

    Commited by derekwan@DW-Mac at 2025-05-30 09:01:48 (Fri)

--- src/utilities/string.py
+++ src/utilities/string.py
@@ -3,17 +3,17 @@ from __future__ import annotations
 from os import environ
 from pathlib import Path
 from string import Template
-from typing import assert_never
+from typing import Any, assert_never
 
 
-def substitute_environ(path_or_text: Path | str, /) -> str:
+def substitute_environ(path_or_text: Path | str, /, **kwargs: Any) -> str:
     """Substitute the environment variables in a file."""
     match path_or_text:
         case Path() as path:
             with path.open() as fh:
-                return substitute_environ(fh.read())
+                return substitute_environ(fh.read(), **kwargs)
         case str() as text:
-            return Template(text).substitute(environ)
+            return Template(text).substitute(environ, **kwargs)
         case _ as never:
             assert_never(never)
 
