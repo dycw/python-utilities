6f5ea4486219b3602ac3f3504109e9cfdc24458b
Derek Wan <d.wan@icloud.com>
2025-05-31 06:40:26 +0900

    Commited by derekwan@DW-Mac at 2025-05-31 06:40:26 (Sat)

--- src/utilities/contextlib.py
+++ src/utilities/contextlib.py
@@ -32,10 +32,10 @@ _SUPER_OBJECT_HAS_NO_ATTRIBUTE = re.compile(r"'super' object has no attribute '\
 
 
 @contextmanager
-def suppress_super_object_has_no_attribute() -> Iterator[None]:
+def suppress_super_object_attribute_error() -> Iterator[None]:
     """Suppress the super() attribute error, for mix-ins."""
     try:
-        super().__post_init__()  # pyright: ignore[reportAttributeAccessIssue]
+        yield
     except AttributeError as error:
         if not _SUPER_OBJECT_HAS_NO_ATTRIBUTE.search(error.args[0]):
             raise
