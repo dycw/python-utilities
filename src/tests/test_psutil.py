5750cb0aedf34894443e8db851b84106e82a4b0d
Derek Wan <d.wan@icloud.com>
2025-05-31 08:06:04 +0900

    Commited by derekwan@DW-Mac at 2025-05-31 08:06:04 (Sat)

--- src/tests/test_psutil.py
+++ src/tests/test_psutil.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from math import isfinite, isnan
 from typing import TYPE_CHECKING
 
 from pytest import approx, mark, param
@@ -18,12 +19,16 @@ class TestMemoryUsage:
         assert memory.virtual_total_mb >= 0
         assert memory.virtual_total >= 0
         assert memory.virtual_total_mb >= 0
-        assert 0.0 <= memory.virtual_pct <= 1.0
+        assert (
+            isfinite(memory.virtual_pct) and (0.0 <= memory.virtual_pct <= 1.0)
+        ) or isnan(memory.virtual_pct)
         assert memory.swap_total >= 0
         assert memory.swap_total_mb >= 0
         assert memory.swap_total >= 0
         assert memory.swap_total_mb >= 0
-        assert 0.0 <= memory.swap_pct <= 1.0
+        assert (isfinite(memory.swap_pct) and (0.0 <= memory.swap_pct <= 1.0)) or isnan(
+            memory.swap_pct
+        )
 
 
 class TestMemoryMonitorService:
