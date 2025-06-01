04f3f54cf14a77456e4105ced659633f63ad71d4
Derek Wan <d.wan@icloud.com>
2025-06-02 01:04:27 +0900

    Commited by derekwan@DW-Mac at 2025-06-02 01:04:27 (Mon)

--- src/utilities/pottery.py
+++ src/utilities/pottery.py
@@ -8,38 +8,44 @@ from pottery import AIORedlock
 from pottery.exceptions import ReleaseUnlockedLock
 
 from utilities.datetime import MILLISECOND, SECOND, datetime_duration_to_float
+from utilities.iterables import always_iterable
 
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator
 
     from redis.asyncio import Redis
 
-    from utilities.types import Duration
+    from utilities.types import Duration, MaybeIterable
 
 
 @asynccontextmanager
 async def yield_aio_redlock(
-    redis: Redis,
+    redis_or_redises: MaybeIterable[Redis],
     key: str,
     /,
     *,
     duration: Duration = 10 * SECOND,
     sleep: Duration = MILLISECOND,
 ) -> AsyncIterator[None]:
-    duration_use = datetime_duration_to_float(duration)
-    sleep_use = datetime_duration_to_float(sleep)
-    while True:
+    """Yield a locked resource."""
+    masters = set(always_iterable(redis_or_redises))  # skipif-ci-and-not-linux
+    duration_use = datetime_duration_to_float(duration)  # skipif-ci-and-not-linux
+    sleep_use = datetime_duration_to_float(sleep)  # skipif-ci-and-not-linux
+    while True:  # skipif-ci-and-not-linux
         lock = AIORedlock(
             key=key,
-            masters={redis},
+            masters=masters,
             auto_release_time=duration_use,
             context_manager_timeout=duration_use,
         )
         if await lock.acquire():
             break
         await asyncio.sleep(sleep_use)
-    try:
+    try:  # skipif-ci-and-not-linux
         yield
-    finally:
+    finally:  # skipif-ci-and-not-linux
         with suppress(ReleaseUnlockedLock):
             await lock.release()
+
+
+__all__ = ["yield_aio_redlock"]
