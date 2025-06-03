c33d467695daeb75d3c37c660b96844f3a8e3cab
Derek Wan <d.wan@icloud.com>
2025-06-04 00:28:05 +0900

    Commited by derekwan@DW-Mac at 2025-06-04 00:28:05 (Wed)

--- src/utilities/slack_sdk.py
+++ src/utilities/slack_sdk.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING, Any, Self, override
 
 from slack_sdk.webhook.async_client import AsyncWebhookClient
 
-from utilities.asyncio import Looper, LooperTimeoutError, timeout_dur
+from utilities.asyncio import Looper, timeout_dur
 from utilities.datetime import MINUTE, SECOND, datetime_duration_to_float
 from utilities.functools import cache
 from utilities.math import safe_round
@@ -25,9 +25,6 @@ _TIMEOUT: Duration = MINUTE
 
 
 ##
-
-
-_SLEEP: Duration = SECOND
 
 
 async def _send_adapter(url: str, text: str, /) -> None:
@@ -49,7 +46,6 @@ class SlackHandlerService(Handler, Looper[str]):
         backoff: Duration = SECOND,
         logger: str | None = None,
         timeout: Duration | None = None,
-        timeout_error: type[Exception] = LooperTimeoutError,
         _debug: bool = False,
         level: int = NOTSET,
         sender: Callable[[str, str], Coroutine1[None]] = _send_adapter,
@@ -63,7 +59,6 @@ class SlackHandlerService(Handler, Looper[str]):
             backoff=backoff,
             logger=logger,
             timeout=timeout,
-            timeout_error=timeout_error,
             _debug=_debug,
         )
         Looper.__post_init__(self)
