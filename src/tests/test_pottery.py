f97667b1e0b38efb049eb9495ad7fbf06f88475f
Derek Wan <d.wan@icloud.com>
2025-06-03 11:11:31 +0900

    Commited by derekwan@DW-Mac at 2025-06-03 11:11:31 (Tue)

--- src/tests/test_pottery.py
+++ src/tests/test_pottery.py
@@ -2,12 +2,10 @@ from __future__ import annotations
 
 from asyncio import TaskGroup, sleep
 
-from hypothesis import Phase, given
-
 from tests.conftest import SKIPIF_CI_AND_NOT_LINUX
 from tests.test_redis import yield_test_redis
-from utilities.hypothesis import settings_with_reduced_examples, unique_strs
 from utilities.pottery import yield_locked_resource
+from utilities.text import unique_str
 from utilities.timer import Timer
 
 
@@ -22,11 +20,9 @@ async def _asyncio_runner(num_tasks: int, key: str) -> None:
 
 
 class TestYieldLockedResource:
-    @given(key=unique_strs())
-    @settings_with_reduced_examples(phases={Phase.generate})
     @SKIPIF_CI_AND_NOT_LINUX
-    async def test_single_process(self, *, key: str) -> None:
+    async def test_single_process(self) -> None:
         with Timer() as timer:
-            await _asyncio_runner(3, key)
+            await _asyncio_runner(3, unique_str())
         min_time = 0.3
         assert min_time <= float(timer) <= 3 * min_time
