313c96593bc4e1f6eb96b474ae998f3247b068e7
Derek Wan <d.wan@icloud.com>
2025-06-04 00:08:36 +0900

    Commited by derekwan@DW-Mac at 2025-06-04 00:08:36 (Wed)

--- src/utilities/fastapi.py
+++ src/utilities/fastapi.py
@@ -83,5 +83,15 @@ class PingReceiver(Looper[None]):
             return False
         return response.text if response.status_code == 200 else False  # skipif-ci
 
+    @override
+    async def _initialize_core(self) -> None:
+        await super()._initialize_core()  # skipif-ci
+        await self._server.serve()  # skipif-ci
+
+    @override
+    async def _tear_down_core(self) -> None:
+        await super()._tear_down_core()  # skipif-ci
+        await self._server.shutdown()  # skipif-ci
+
 
 __all__ = ["PingReceiver"]
