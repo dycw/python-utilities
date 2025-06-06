40c6825c896a2dce432bf87cadd4e0063ceedec7
Derek Wan <d.wan@icloud.com>
2025-06-06 19:04:29 +0900

    Commited by derekwan@DW-Mac at 2025-06-06 19:04:29 (Fri)

--- src/tests/test_whenever.py
+++ src/tests/test_whenever.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import datetime as dt
 from datetime import timezone
-from logging import getLogger, setLogRecordFactory
+from logging import DEBUG
 from re import escape
 from typing import TYPE_CHECKING
 from zoneinfo import ZoneInfo
@@ -21,7 +21,7 @@ from hypothesis.strategies import (
     timezones,
 )
 from pytest import raises
-from whenever import DateTimeDelta
+from whenever import DateTimeDelta, ZonedDateTime
 
 from tests.conftest import SKIPIF_CI_AND_WINDOWS
 from utilities.datetime import (
@@ -41,7 +41,6 @@ from utilities.hypothesis import (
     timedeltas_2w,
     zoned_datetimes,
 )
-from utilities.text import unique_str
 from utilities.tzdata import HongKong
 from utilities.whenever import (
     MAX_SERIALIZABLE_TIMEDELTA,
@@ -498,7 +497,17 @@ class TestToDateTimeDelta:
 
 
 class TestWheneverLogRecord:
-    def test_main(self) -> None:
-        logger = getLogger(unique_str())
-        setLogRecordFactory(WheneverLogRecord)
-        logger.warning("message")
+    def test_init(self) -> None:
+        _ = WheneverLogRecord("name", DEBUG, "pathname", 0, None, None, None)
+
+    def test_get_length(self) -> None:
+        assert isinstance(WheneverLogRecord._get_length(), int)
+
+    def test_get_now(self) -> None:
+        assert isinstance(WheneverLogRecord._get_now(), ZonedDateTime)
+
+    def test_get_time_zone(self) -> None:
+        assert isinstance(WheneverLogRecord._get_time_zone(), ZoneInfo)
+
+    def test_get_time_zone_key(self) -> None:
+        assert isinstance(WheneverLogRecord._get_time_zone_key(), str)
