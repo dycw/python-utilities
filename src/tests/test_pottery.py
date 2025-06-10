93e4f438282593e33b051a4553f0d68321ba0296
Derek Wan <d.wan@icloud.com>
2025-06-11 07:08:48 +0900

    Commited by derekwan@DW-Mac at 2025-06-11 07:08:48 (Wed)

--- src/tests/test_pottery.py
+++ src/tests/test_pottery.py
@@ -4,13 +4,13 @@ from asyncio import TaskGroup, sleep
 
 from tests.conftest import SKIPIF_CI_AND_NOT_LINUX
 from tests.test_redis import yield_test_redis
-from utilities.pottery import yield_locked_resource
+from utilities.pottery import yield_async_locked_resource
 from utilities.text import unique_str
 from utilities.timer import Timer
 
 
 async def _coroutine(key: str) -> None:
-    async with yield_test_redis() as redis, yield_locked_resource(redis, key):
+    async with yield_test_redis() as redis, yield_async_locked_resource(redis, key):
         await sleep(0.1)
 
 
@@ -19,9 +19,18 @@ async def _asyncio_runner(num_tasks: int, key: str) -> None:
         _ = [tg.create_task(_coroutine(key)) for _ in range(num_tasks)]
 
 
+class TestYieldAsyncLockedResource:
+    @SKIPIF_CI_AND_NOT_LINUX
+    async def test_main(self) -> None:
+        with Timer() as timer:
+            await _asyncio_runner(3, unique_str())
+        min_time = 0.3
+        assert min_time <= float(timer) <= 3 * min_time
+
+
 class TestYieldLockedResource:
     @SKIPIF_CI_AND_NOT_LINUX
-    async def test_single_process(self) -> None:
+    def test_main(self) -> None:
         with Timer() as timer:
             await _asyncio_runner(3, unique_str())
         min_time = 0.3
--- src/utilities/pottery.py
+++ src/utilities/pottery.py
@@ -1,25 +1,57 @@
 from __future__ import annotations
 
 import asyncio
-from contextlib import asynccontextmanager, suppress
+import time
+from contextlib import asynccontextmanager, contextmanager, suppress
 from typing import TYPE_CHECKING
 
-from pottery import AIORedlock
+import redis
+import redis.asyncio
+from pottery import AIORedlock, Redlock
 from pottery.exceptions import ReleaseUnlockedLock
-from redis.asyncio import Redis
 
 from utilities.datetime import MILLISECOND, SECOND, datetime_duration_to_float
 from utilities.iterables import always_iterable
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncIterator
+    from collections.abc import AsyncIterator, Iterator
 
     from utilities.types import Duration, MaybeIterable
 
 
+@contextmanager
+def yield_locked_resource(
+    redis_: MaybeIterable[redis.Redis],
+    key: str,
+    /,
+    *,
+    duration: Duration = 10 * SECOND,
+    sleep: Duration = MILLISECOND,
+) -> Iterator[None]:
+    """Yield a locked resource."""
+    masters = (  # skipif-ci-and-not-linux
+        {redis_} if isinstance(redis_, redis.Redis) else set(always_iterable(redis_))
+    )
+    duration_use = datetime_duration_to_float(duration)  # skipif-ci-and-not-linux
+    lock = Redlock(  # skipif-ci-and-not-linux
+        key=key,
+        masters=masters,
+        auto_release_time=duration_use,
+        context_manager_timeout=duration_use,
+    )
+    sleep_use = datetime_duration_to_float(sleep)  # skipif-ci-and-not-linux
+    while not lock.acquire():  # pragma: no cover
+        _ = time.sleep(sleep_use)
+    try:  # skipif-ci-and-not-linux
+        yield
+    finally:  # skipif-ci-and-not-linux
+        with suppress(ReleaseUnlockedLock):
+            lock.release()
+
+
 @asynccontextmanager
-async def yield_locked_resource(
-    redis: MaybeIterable[Redis],
+async def yield_async_locked_resource(
+    redis_: MaybeIterable[redis.asyncio.Redis],
     key: str,
     /,
     *,
@@ -28,7 +60,9 @@ async def yield_locked_resource(
 ) -> AsyncIterator[None]:
     """Yield a locked resource."""
     masters = (  # skipif-ci-and-not-linux
-        {redis} if isinstance(redis, Redis) else set(always_iterable(redis))
+        {redis_}
+        if isinstance(redis_, redis.asyncio.Redis)
+        else set(always_iterable(redis_))
     )
     duration_use = datetime_duration_to_float(duration)  # skipif-ci-and-not-linux
     lock = AIORedlock(  # skipif-ci-and-not-linux
@@ -47,4 +81,4 @@ async def yield_locked_resource(
             await lock.release()
 
 
-__all__ = ["yield_locked_resource"]
+__all__ = ["yield_async_locked_resource"]
