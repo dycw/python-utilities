0dee2ea8a51a59701bf2ce4c086f397bf60ad1ed
Derek Wan <d.wan@icloud.com>
2025-06-12 17:55:54 +0900

    Commited by derekwan@DW-Mac at 2025-06-12 17:55:54 (Thu)

--- src/utilities/datetime.py
+++ src/utilities/datetime.py
@@ -28,7 +28,14 @@ from utilities.zoneinfo import UTC, ensure_time_zone, get_time_zone_name
 if TYPE_CHECKING:
     from collections.abc import Iterator
 
-    from utilities.types import DateOrDateTime, Duration, MathRoundMode, TimeZoneLike
+    from utilities.types import (
+        DateOrDateTime,
+        Duration,
+        MaybeCallablePyDate,
+        MaybeCallablePyDateTime,
+        RoundMode,
+        TimeZoneLike,
+    )
 
 
 _DAYS_PER_YEAR = 365.25
