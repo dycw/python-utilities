90672ffce4a9c356d61029a5bda0cfb1285a9474
Derek Wan <d.wan@icloud.com>
2025-06-12 22:16:09 +0900

    Commited by derekwan@DW-Mac at 2025-06-12 22:16:09 (Thu)

--- src/utilities/typed_settings.py
+++ src/utilities/typed_settings.py
@@ -3,6 +3,15 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, TypeVar, override
 
 from typed_settings.converters import TSConverter
+from whenever import (
+    Date,
+    DateDelta,
+    DateTimeDelta,
+    PlainDateTime,
+    Time,
+    TimeDelta,
+    ZonedDateTime,
+)
 
 if TYPE_CHECKING:
     from collections.abc import Callable
@@ -22,29 +31,15 @@ class ExtendedTSConverter(TSConverter):
         strlist_sep: str | Callable[[str], list] | None = ":",
     ) -> None:
         super().__init__(resolve_paths=resolve_paths, strlist_sep=strlist_sep)
-        cases: list[tuple[type[Any], Callable[..., Any]]] = []
-        try:
-            from whenever import (
-                Date,
-                DateDelta,
-                DateTimeDelta,
-                PlainDateTime,
-                Time,
-                TimeDelta,
-                ZonedDateTime,
-            )
-        except ModuleNotFoundError:  # pragma: no cover
-            pass
-        else:
-            cases.extend([
-                (Date, Date.parse_common_iso),
-                (DateDelta, DateDelta.parse_common_iso),
-                (DateTimeDelta, DateTimeDelta.parse_common_iso),
-                (PlainDateTime, PlainDateTime.parse_common_iso),
-                (Time, Time.parse_common_iso),
-                (TimeDelta, TimeDelta.parse_common_iso),
-                (ZonedDateTime, ZonedDateTime.parse_common_iso),
-            ])
+        cases: list[tuple[type[Any], Callable[..., Any]]] = [
+            (Date, Date.parse_common_iso),
+            (DateDelta, DateDelta.parse_common_iso),
+            (DateTimeDelta, DateTimeDelta.parse_common_iso),
+            (PlainDateTime, PlainDateTime.parse_common_iso),
+            (Time, Time.parse_common_iso),
+            (TimeDelta, TimeDelta.parse_common_iso),
+            (ZonedDateTime, ZonedDateTime.parse_common_iso),
+        ]
         extras = {cls: _make_converter(cls, func) for cls, func in cases}
         self.scalar_converters |= extras
 
