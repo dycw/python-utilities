248232d12adda9294696084b0b8e36dbe36c736a
Derek Wan <d.wan@icloud.com>
2025-06-13 13:40:46 +0900

    Commited by derekwan@DW-Mac at 2025-06-13 13:40:46 (Fri)

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -16,7 +16,6 @@ from whenever import PlainDateTime
 from utilities.platform import IS_NOT_LINUX, IS_WINDOWS
 from utilities.re import ExtractGroupError, extract_group
 from utilities.text import strip_and_dedent
-from utilities.tzlocal import LOCAL_TIME_ZONE_NAME
 from utilities.whenever2 import MINUTE, get_now
 
 if TYPE_CHECKING:
@@ -100,9 +99,7 @@ def _is_to_drop(table: str, /) -> bool:
         datetime_str = extract_group(r"^(\d{8}T\d{6})_", table)
     except ExtractGroupError:
         return True
-    datetime = PlainDateTime.parse_common_iso(datetime_str).assume_tz(
-        LOCAL_TIME_ZONE_NAME
-    )
+    datetime = PlainDateTime.parse_common_iso(datetime_str)
     now = get_now()
     return (now - datetime) >= MINUTE
 
