d266062ae63f46ad98538d2903f683493a24b577
Derek Wan <d.wan@icloud.com>
2025-06-13 10:20:02 +0900

    Commited by derekwan@DW-Mac at 2025-06-13 10:20:02 (Fri)

--- src/utilities/pottery.py
+++ src/utilities/pottery.py
@@ -49,7 +49,7 @@ async def yield_access(
     lock: AIORedlock | None = None  # skipif-ci-and-not-linux
     try:  # skipif-ci-and-not-linux
         lock = await _get_first_available_lock(
-            key, locks, num=num, timeout_delay=timeout_acquire, sleep_delay=sleep_delay
+            key, locks, num=num, timeout=timeout_acquire, sleep=sleep_delay
         )
         yield
     finally:  # skipif-ci-and-not-linux
@@ -65,18 +65,18 @@ async def _get_first_available_lock(
     /,
     *,
     num: int = 1,
-    timeout_delay: TimeDelta | None = None,
-    sleep_delay: TimeDelta | None = None,
+    timeout: TimeDelta | None = None,
+    sleep: TimeDelta | None = None,
 ) -> AIORedlock:
     locks = list(locks)  # skipif-ci-and-not-linux
     error = _YieldAccessUnableToAcquireLockError(  # skipif-ci-and-not-linux
-        key=key, num=num, delay=timeout_delay
+        key=key, num=num, timeout=timeout
     )
-    async with timeout_td(timeout_delay, error=error):  # skipif-ci-and-not-linux
+    async with timeout_td(timeout, error=error):  # skipif-ci-and-not-linux
         while True:
             if (result := await _get_first_available_lock_if_any(locks)) is not None:
                 return result
-            await sleep_td(sleep_delay)
+            await sleep_td(sleep)
 
 
 async def _get_first_available_lock_if_any(
@@ -103,11 +103,11 @@ class _YieldAccessNumLocksError(YieldAccessError):
 
 @dataclass(kw_only=True, slots=True)
 class _YieldAccessUnableToAcquireLockError(YieldAccessError):
-    delay: TimeDelta | None
+    timeout: TimeDelta | None
 
     @override
     def __str__(self) -> str:
-        return f"Unable to acquire any 1 of {self.num} locks for {self.key!r} after {self.delay}"  # skipif-ci-and-not-linux
+        return f"Unable to acquire any 1 of {self.num} locks for {self.key!r} after {self.timeout}"  # skipif-ci-and-not-linux
 
 
 __all__ = ["YieldAccessError", "yield_access"]
