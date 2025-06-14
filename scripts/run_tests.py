81e83ef3d7cf839658f9ef1a4965a39f41229f58
Derek Wan <d.wan@icloud.com>
2025-06-14 20:51:48 +0900

    Commited by derekwan@DW-Mac at 2025-06-14 20:51:48 (Sat)

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -2,9 +2,11 @@
 from __future__ import annotations
 
 from logging import getLogger
+from tomllib import loads
 
 from click import command
 
+from utilities.git import get_repo_root
 from utilities.logging import basic_config
 
 _LOGGER = getLogger(__name__)
@@ -13,7 +15,8 @@ _LOGGER = getLogger(__name__)
 @command()
 def main() -> None:
     basic_config(obj=_LOGGER)
-    _LOGGER.info("hi")
+    dict_ = loads(get_repo_root().joinpath("pyproject.toml").read_text())
+    _LOGGER.info(dict_)
 
 
 if __name__ == "__main__":
