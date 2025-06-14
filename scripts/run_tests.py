fc74347f879fb2ad5487a05ac6c6204cda2716a9
Derek Wan <d.wan@icloud.com>
2025-06-14 22:26:21 +0900

    save

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 from __future__ import annotations
 
+import datetime as dt
 from logging import getLogger
-from pathlib import Path
-from shlex import quote
 from subprocess import CalledProcessError, check_call
 from time import sleep
 from tomllib import loads
@@ -14,46 +13,47 @@ from utilities.logging import basic_config
 from utilities.re import extract_group
 
 if TYPE_CHECKING:
-    from collections.abc import Collection
+    from pathlib import Path
 
 _LOGGER = getLogger(__name__)
 
 
 def main() -> None:
     basic_config(obj=_LOGGER)
-    root = get_repo_root()
-    groups: list[str] = loads(root.joinpath("pyproject.toml").read_text())[
-        "dependency-groups"
-    ]
-    path = root.joinpath("src", "tests")
+    path = get_repo_root().joinpath("src", "tests")
     for path_i in sorted(path.glob("test_*.py")):
-        _run_test(path_i, groups)
+        _run_test(path_i)
 
 
-def _run_test(path: Path, groups: Collection[str], /) -> None:
+def _run_test(path: Path, /) -> None:
+    root = get_repo_root()
+    hour = dt.datetime.now(dt.UTC).replace(minute=0, second=0, microsecond=0)
     group = extract_group(r"^test_(\w+)$", path.stem).replace("_", "-")
-    marker = Path(".pytest_cache", group)
-    if marker.exists():
+    marker = root.joinpath(".pytest_cache", f"{hour:%Y%m%dT%H}-{group}")
+    if (group == "pytest") or marker.exists():
         return
     _LOGGER.info("Testing %r...", str(path))
     cmd: list[str] = [
         "uv",
         "run",
-        f"--only-group={quote(group)}",
         "--only-group=core",
         "--only-group=hypothesis",
         "--only-group=pytest",
         "--isolated",
         "--managed-python",
     ]
-    if group in groups:
-        cmd.append(f"--only-group={group}")
-    if (test := f"{group}-test") in groups:
-        cmd.append(f"--only-group={test}")
-    cmd.extend(["pytest", "-nauto", str(path)])
     while True:
+        cmd_use: list[str] = cmd.copy()
+        groups: list[str] = loads(root.joinpath("pyproject.toml").read_text())[
+            "dependency-groups"
+        ]
+        if group in groups:
+            cmd_use.append(f"--only-group={group}")
+        if (test := f"{group}-test") in groups:
+            cmd_use.append(f"--only-group={test}")
+        cmd_use.extend(["pytest", "-nauto", str(path)])
         try:
-            code = check_call(cmd)
+            code = check_call(cmd_use)
         except CalledProcessError:
             sleep(1)
         else:
