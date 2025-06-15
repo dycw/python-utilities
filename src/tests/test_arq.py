a24696594b20df42713ab3762be8405c39aab976
Derek Wan <d.wan@icloud.com>
2025-06-16 05:55:06 +0900

    Commited by derekwan@DW-Mac at 2025-06-16 05:55:06 (Mon)

--- src/tests/test_arq.py
+++ src/tests/test_arq.py
@@ -7,6 +7,7 @@ from hypothesis import given
 from hypothesis.strategies import integers
 
 from utilities.arq import Worker, cron_raw, lift
+from utilities.hypothesis import settings_with_reduced_examples
 from utilities.iterables import one
 
 if TYPE_CHECKING:
@@ -20,6 +21,7 @@ if TYPE_CHECKING:
 
 class TestLift:
     @given(x=integers(), y=integers())
+    @settings_with_reduced_examples()
     async def test_main(self, *, x: int, y: int) -> None:
         @lift
         async def func(x: int, y: int, /) -> int:
@@ -32,6 +34,7 @@ class TestLift:
 
 class TestWorker:
     @given(x=integers(), y=integers())
+    @settings_with_reduced_examples()
     async def test_main(self, *, x: int, y: int) -> None:
         async def func(x: int, y: int, /) -> int:
             await sleep(0.01)
@@ -45,6 +48,7 @@ class TestWorker:
         assert result == (x + y)
 
     @given(x=integers(), y=integers())
+    @settings_with_reduced_examples()
     async def test_cron(self, *, x: int, y: int) -> None:
         async def func(x: int, y: int, /) -> int:
             await sleep(0.01)
