1b415c4ed67abd82d14ea3363b985db91a381604
Derek Wan <d.wan@icloud.com>
2025-06-16 05:15:47 +0900

    Commited by derekwan@DW-Mac at 2025-06-16 05:15:47 (Mon)

---
+++ src/utilities/arq.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any
+
+from arq.constants import default_queue_name, expires_extra_ms
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from datetime import timezone
+
+    from arq.connections import ArqRedis, RedisSettings
+    from arq.cron import CronJob
+    from arq.jobs import Deserializer, Serializer
+    from arq.typing import SecondsTimedelta, StartupShutdown, WorkerCoroutine
+    from arq.worker import Function
+
+
+@dataclass(kw_only=True)
+class Worker:
+    """Base class for all workers."""
+
+    functions: Sequence[Function | WorkerCoroutine] = ()
+    queue_name: str | None = default_queue_name
+    cron_jobs: Sequence[CronJob] | None = None
+    redis_settings: RedisSettings | None = None
+    redis_pool: ArqRedis | None = None
+    burst: bool = False
+    on_startup: StartupShutdown | None = None
+    on_shutdown: StartupShutdown | None = None
+    on_job_start: StartupShutdown | None = None
+    on_job_end: StartupShutdown | None = None
+    after_job_end: StartupShutdown | None = None
+    handle_signals: bool = True
+    job_completion_wait: int = 0
+    max_jobs: int = 10
+    job_timeout: SecondsTimedelta = 300
+    keep_result: SecondsTimedelta = 3600
+    keep_result_forever: bool = False
+    poll_delay: SecondsTimedelta = 0.5
+    queue_read_limit: int | None = None
+    max_tries: int = 5
+    health_check_interval: SecondsTimedelta = 3600
+    health_check_key: str | None = None
+    ctx: dict[Any, Any] | None = None
+    retry_jobs: bool = True
+    allow_abort_jobs: bool = False
+    max_burst_jobs: int = -1
+    job_serializer: Serializer | None = None
+    job_deserializer: Deserializer | None = None
+    expires_extra_ms: int = expires_extra_ms
+    timezone: timezone | None = None
+    log_results: bool = True
+
+
+__all__ = ["Worker"]
