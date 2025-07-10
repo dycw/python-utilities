7d8e4f4a10cf62d7aef60300a0a5e877ea5e84e8
Derek Wan <d.wan@icloud.com>
2025-07-11 08:37:14 +0900

    save

--- src/utilities/script.py
+++ src/utilities/script.py
@@ -1,21 +1,76 @@
 from __future__ import annotations
 
 from asyncio import run, sleep
-from contextlib import asynccontextmanager
-from typing import TYPE_CHECKING
+from contextlib import _GeneratorContextManager, asynccontextmanager, contextmanager
+from dataclasses import dataclass
+from functools import wraps
+from logging import getLogger
+from signal import SIGINT, default_int_handler, getsignal, signal
+from typing import TYPE_CHECKING, Any, TypeVar, override
 
 from utilities.logging import setup_logging
 from utilities.pathlib import get_repo_root
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncIterator
-from logging import getLogger
+    from collections.abc import AsyncIterator, Callable, Generator, Iterator
+    from signal import _HANDLER, _SIGNUM
+    from types import FrameType, TracebackType
+
 
 _LOGGER = getLogger(__name__)
+_G_co = TypeVar("_G_co", covariant=True)
 
 
-@asynccontextmanager
-async def context() -> AsyncIterator[None]:
+class EnhancedContextManager(_GeneratorContextManager[_G_co]):
+    sigint: _HANDLER | None
+
+    @override
+    def __init__(
+        self,
+        func: Callable[..., Generator[_G_co, None, None]],
+        args: tuple[Any, ...],
+        kwds: dict[str, Any],
+    ) -> None:
+        super().__init__(func, args, kwds)
+        self.sigint = None
+
+    @override
+    def __enter__(self) -> _G_co:
+        self.sigint = getsignal(SIGINT)
+
+        def handler(signum: int, frame: FrameType | None) -> None:
+            _ = (signum, frame)
+            _ = self.__exit__(None, None, None)
+
+        _ = signal(SIGINT, handler)
+        return super().__enter__()
+
+    @override
+    def __exit__(
+        self,
+        typ: type[BaseException] | None,
+        value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> bool | None:
+        _ = signal(SIGINT, self.sigint)
+        return super().__exit__(typ, value, traceback)
+
+
+T_co = TypeVar("T_co", covariant=True)
+
+
+def enhanced_context_manager[**P, T_co](
+    func: Callable[P, Iterator[T_co]], /
+) -> Callable[P, EnhancedContextManager[T_co]]:
+    @wraps(func)
+    def wrapped(*args: Any, **kwds: Any) -> EnhancedContextManager[Any]:
+        return EnhancedContextManager(func, args, kwds)
+
+    return wrapped
+
+
+@enhanced_context_manager
+def context() -> Iterator[None]:
     path = get_repo_root().joinpath("dummy")
     path.touch()
     yield
@@ -26,7 +81,7 @@ async def main() -> None:
     setup_logging(logger=_LOGGER, files_dir=".logs")
     _LOGGER.info("starting...")
     _LOGGER.info("sleeping for 5...")
-    async with context():
+    with context():
         await sleep(5)
     _LOGGER.info("finished")
 
