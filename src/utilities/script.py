88e09ac61c2b0dc28ba41ab5da88af9c3e60e7a5
Derek Wan <d.wan@icloud.com>
2025-07-11 09:01:00 +0900

    Commited by derekwan@DW-Mac at 2025-07-11 09:01:00 (Fri)

--- src/utilities/script.py
+++ src/utilities/script.py
@@ -1,70 +1,42 @@
 from __future__ import annotations
 
 from asyncio import run, sleep
-from contextlib import _GeneratorContextManager, asynccontextmanager, contextmanager
-from dataclasses import dataclass
-from functools import wraps
+from contextlib import _GeneratorContextManager, contextmanager
 from logging import getLogger
-from signal import SIGINT, default_int_handler, getsignal, signal
-from typing import TYPE_CHECKING, Any, TypeVar, override
+from signal import SIGINT, getsignal, signal
+from typing import TYPE_CHECKING
 
 from utilities.logging import setup_logging
 from utilities.pathlib import get_repo_root
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncIterator, Callable, Generator, Iterator
-    from signal import _HANDLER, _SIGNUM
-    from types import FrameType, TracebackType
+    from collections.abc import Callable, Iterator
+    from types import FrameType
 
 
 _LOGGER = getLogger(__name__)
-_G_co = TypeVar("_G_co", covariant=True)
-
-
-class EnhancedContextManager(_GeneratorContextManager[_G_co]):
-    sigint: _HANDLER | None
-
-    @override
-    def __init__(
-        self,
-        func: Callable[..., Generator[_G_co, None, None]],
-        args: tuple[Any, ...],
-        kwds: dict[str, Any],
-    ) -> None:
-        super().__init__(func, args, kwds)
-        self.sigint = None
-
-    @override
-    def __enter__(self) -> _G_co:
-        self.sigint = getsignal(SIGINT)
-
-        def handler(signum: int, frame: FrameType | None) -> None:
-            _ = (signum, frame)
-            _ = self.__exit__(None, None, None)
-
-        _ = signal(SIGINT, handler)
-        return super().__enter__()
-
-    @override
-    def __exit__(
-        self,
-        typ: type[BaseException] | None,
-        value: BaseException | None,
-        traceback: TracebackType | None,
-    ) -> bool | None:
-        _ = signal(SIGINT, self.sigint)
-        return super().__exit__(typ, value, traceback)
-
-
-T_co = TypeVar("T_co", covariant=True)
 
 
 def enhanced_context_manager[**P, T_co](
     func: Callable[P, Iterator[T_co]], /
-) -> Callable[P, EnhancedContextManager[T_co]]:
-    @wraps(func)
-    def wrapped(*args: Any, **kwds: Any) -> EnhancedContextManager[Any]:
-        return EnhancedContextManager(func, args, kwds)
+) -> Callable[P, _GeneratorContextManager[T_co]]:
+    make_cm = contextmanager(func)
+
+    @contextmanager
+    def wrapped(*args: P.args, **kwargs: P.kwargs) -> Iterator[T_co]:
+        orig_sigint = getsignal(SIGINT)
+        cm = make_cm(*args, **kwargs)
+
+        def handler(signum: int, frame: FrameType | None) -> None:
+            _ = (signum, frame)
+            _ = cm.__exit__(None, None, None)
+
+        _ = signal(SIGINT, handler)
+        try:
+            with cm as value:
+                yield value
+        finally:
+            _ = signal(SIGINT, orig_sigint)
 
     return wrapped
 
@@ -81,8 +53,8 @@ async def main() -> None:
     setup_logging(logger=_LOGGER, files_dir=".logs")
     _LOGGER.info("starting...")
     _LOGGER.info("sleeping for 5...")
-    with context() as asdf, context() as asdf:
-        await sleep(5)
+    with context():
+        await sleep(8)
     _LOGGER.info("finished")
 
 
