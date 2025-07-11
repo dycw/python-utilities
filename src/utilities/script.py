907d867c48fa25ed121ef99c1d47122c20585af8
Derek Wan <d.wan@icloud.com>
2025-07-11 09:12:26 +0900

    Commited by derekwan@DW-Mac at 2025-07-11 09:12:26 (Fri)

--- src/utilities/script.py
+++ src/utilities/script.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from asyncio import run, sleep
 from contextlib import _GeneratorContextManager, contextmanager
 from logging import getLogger
-from signal import SIGINT, getsignal, signal
+from signal import SIGABRT, SIGFPE, SIGILL, SIGINT, SIGSEGV, SIGTERM, getsignal, signal
 from typing import TYPE_CHECKING
 
 from utilities.logging import setup_logging
@@ -18,25 +18,55 @@ _LOGGER = getLogger(__name__)
 
 
 def enhanced_context_manager[**P, T_co](
-    func: Callable[P, Iterator[T_co]], /
+    func: Callable[P, Iterator[T_co]],
+    /,
+    *,
+    sigabrt: bool = True,
+    sigfpe: bool = True,
+    sigill: bool = True,
+    sigint: bool = True,
+    sigsegv: bool = True,
+    sigterm: bool = True,
 ) -> Callable[P, _GeneratorContextManager[T_co]]:
     make_cm = contextmanager(func)
 
     @contextmanager
     def wrapped(*args: P.args, **kwargs: P.kwargs) -> Iterator[T_co]:
-        orig_sigint = getsignal(SIGINT)
+        sigabrt0 = sigfpe0 = sigill0 = sigint0 = sigsegv0 = sigterm0 = None
         cm = make_cm(*args, **kwargs)
 
         def handler(signum: int, frame: FrameType | None) -> None:
             _ = (signum, frame)
             _ = cm.__exit__(None, None, None)
 
-        _ = signal(SIGINT, handler)
+        if sigabrt:
+            sigabrt0, _ = getsignal(SIGABRT), signal(SIGABRT, handler)
+        if sigfpe:
+            sigfpe0, _ = getsignal(SIGFPE), signal(SIGFPE, handler)
+        if sigill:
+            sigill0, _ = getsignal(SIGILL), signal(SIGILL, handler)
+        if sigint:
+            sigint0, _ = getsignal(SIGINT), signal(SIGINT, handler)
+        if sigsegv:
+            sigsegv0, _ = getsignal(SIGSEGV), signal(SIGSEGV, handler)
+        if sigterm:
+            sigterm0, _ = getsignal(SIGTERM), signal(SIGTERM, handler)
         try:
             with cm as value:
                 yield value
         finally:
-            _ = signal(SIGINT, orig_sigint)
+            if sigabrt:
+                _ = signal(SIGABRT, sigabrt0)
+            if sigfpe:
+                _ = signal(SIGFPE, sigfpe0)
+            if sigill:
+                _ = signal(SIGILL, sigill0)
+            if sigint:
+                _ = signal(SIGINT, sigint0)
+            if sigsegv:
+                _ = signal(SIGSEGV, sigsegv0)
+            if sigterm:
+                _ = signal(SIGTERM, sigterm0)
 
     return wrapped
 
