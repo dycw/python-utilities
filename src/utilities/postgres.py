92a713218cb17753eec71e92e53750d15f690fa9
Derek Wan <d.wan@icloud.com>
2025-07-16 22:05:59 +0900

    Commited by derekwan@DW-Mac at 2025-07-16 22:05:59 (Wed)

--- src/utilities/postgres.py
+++ src/utilities/postgres.py
@@ -31,7 +31,6 @@ async def pg_dump(
     path: PathLike,
     /,
     *,
-    docker: str | None = None,
     format_: _PGDumpFormat = "plain",
     jobs: int | None = None,
     schemas: MaybeListStr | None = None,
@@ -40,58 +39,26 @@ async def pg_dump(
     tables_exc: MaybeSequence[TableOrORMInstOrClass | str] | None = None,
     inserts: bool = False,
     on_conflict_do_nothing: bool = False,
-    logger: LoggerOrName | None = None,
+    docker: str | None = None,
     dry_run: bool = False,
+    logger: LoggerOrName | None = None,
 ) -> None:
     """Run `pg_dump`."""
     path = Path(path)
     path.parent.mkdir(parents=True, exist_ok=True)
-    if url.database is None:
-        raise _PGDumpDatabaseError(url=url)
-    if url.host is None:
-        raise _PGDumpHostError(url=url)
-    if url.port is None:
-        raise _PGDumpPortError(url=url)
-    parts: list[str] = []
-    if docker is not None:
-        parts.extend(["docker", "exec", "-it", docker])
-    parts.extend([
-        "pg_dump",
-        # general options
-        f"--dbname={url.database}",
-        f"--file={str(path)!r}",
-        f"--format={format_}",
-        "--verbose",
-        # output options
-        "--large-objects",
-        "--clean",
-        "--no-owner",
-        "--no-privileges",
-        "--if-exists",
-        # connection options
-        f"--host={url.host}",
-        f"--port={url.port}",
-        "--no-password",
-    ])
-    if (format_ == "directory") and (jobs is not None):
-        parts.append(f"--jobs={jobs}")
-    if schemas is not None:
-        parts.extend([f"--schema={s}" for s in always_iterable(schemas)])
-    if schemas_exc is not None:
-        parts.extend([f"--exclude-schema={s}" for s in always_iterable(schemas_exc)])
-    if tables is not None:
-        parts.extend([f"--table={_get_table_name(t)}" for t in always_iterable(tables)])
-    if tables_exc is not None:
-        parts.extend([
-            f"--exclude-table={_get_table_name(t)}" for t in always_iterable(tables_exc)
-        ])
-    if inserts:
-        parts.append("--inserts")
-    if on_conflict_do_nothing:
-        parts.append("--on-conflict-do-nothing")
-    if url.username is not None:
-        parts.append(f"--username={url.username}")
-    cmd = " ".join(parts)
+    cmd = _build_pg_dump(
+        url,
+        path,
+        format_=format_,
+        jobs=jobs,
+        schemas=schemas,
+        schemas_exc=schemas_exc,
+        tables=tables,
+        tables_exc=tables_exc,
+        inserts=inserts,
+        on_conflict_do_nothing=on_conflict_do_nothing,
+        docker=docker,
+    )
     if dry_run:
         if logger is not None:
             get_logger(logger=logger).info("Would run %r", str(cmd))
@@ -123,30 +90,61 @@ async def pg_dump(
                     rmtree(path, ignore_errors=True)
 
 
-@dataclass(kw_only=True, slots=True)
-class PGDumpError(Exception):
-    url: URL
-
-
-@dataclass(kw_only=True, slots=True)
-class _PGDumpDatabaseError(PGDumpError):
-    @override
-    def __str__(self) -> str:
-        return f"Expected URL to contain a 'database'; got {self.url}"
-
-
-@dataclass(kw_only=True, slots=True)
-class _PGDumpHostError(PGDumpError):
-    @override
-    def __str__(self) -> str:
-        return f"Expected URL to contain a 'host'; got {self.url}"
-
-
-@dataclass(kw_only=True, slots=True)
-class _PGDumpPortError(PGDumpError):
-    @override
-    def __str__(self) -> str:
-        return f"Expected URL to contain a 'port'; got {self.url}"
+def _build_pg_dump(
+    url: URL,
+    path: PathLike,
+    /,
+    *,
+    format_: _PGDumpFormat = "plain",
+    jobs: int | None = None,
+    schemas: MaybeListStr | None = None,
+    schemas_exc: MaybeListStr | None = None,
+    tables: MaybeSequence[TableOrORMInstOrClass | str] | None = None,
+    tables_exc: MaybeSequence[TableOrORMInstOrClass | str] | None = None,
+    inserts: bool = False,
+    on_conflict_do_nothing: bool = False,
+    docker: str | None = None,
+) -> str:
+    database, host, port = _extract_url(url)
+    parts: list[str] = [
+        "pg_dump",
+        # general options
+        f"--dbname={database}",
+        f"--file={str(path)!r}",
+        f"--format={format_}",
+        "--verbose",
+        # output options
+        "--large-objects",
+        "--clean",
+        "--no-owner",
+        "--no-privileges",
+        "--if-exists",
+        # connection options
+        f"--host={host}",
+        f"--port={port}",
+        "--no-password",
+    ]
+    if (format_ == "directory") and (jobs is not None):
+        parts.append(f"--jobs={jobs}")
+    if schemas is not None:
+        parts.extend([f"--schema={s}" for s in always_iterable(schemas)])
+    if schemas_exc is not None:
+        parts.extend([f"--exclude-schema={s}" for s in always_iterable(schemas_exc)])
+    if tables is not None:
+        parts.extend([f"--table={_get_table_name(t)}" for t in always_iterable(tables)])
+    if tables_exc is not None:
+        parts.extend([
+            f"--exclude-table={_get_table_name(t)}" for t in always_iterable(tables_exc)
+        ])
+    if inserts:
+        parts.append("--inserts")
+    if on_conflict_do_nothing:
+        parts.append("--on-conflict-do-nothing")
+    if url.username is not None:
+        parts.append(f"--username={url.username}")
+    if docker is not None:
+        parts = _wrap_docker(parts, docker)
+    return " ".join(parts)
 
 
 ##
@@ -157,6 +155,7 @@ async def pg_restore(
     path: PathLike,
     /,
     *,
+    psql: bool = False,
     database: str | None = None,
     docker: str | None = None,
     data_only: bool = False,
@@ -164,56 +163,22 @@ async def pg_restore(
     schemas: MaybeListStr | None = None,
     schemas_exc: MaybeListStr | None = None,
     tables: MaybeSequence[TableOrORMInstOrClass | str] | None = None,
-    logger: LoggerOrName | None = None,
     dry_run: bool = False,
+    logger: LoggerOrName | None = None,
 ) -> None:
     """Run `pg_restore`."""
-    match database, url.database:
-        case str() as database_use, _:
-            ...
-        case None, str() as database_use:
-            ...
-        case None, None:
-            raise _PGRestoreDatabaseError(url=url)
-        case _ as never:
-            assert_never(never)
-    if url.host is None:
-        raise _PGRestoreHostError(url=url)
-    if url.port is None:
-        raise _PGRestorePortError(url=url)
-    parts: list[str] = []
-    if docker is not None:
-        parts.extend(["docker", "exec", "-it", docker])
-    parts.extend([
-        "pg_restore",
-        # general options
-        f"--dbname={database_use}",
-        "--verbose",
-        # restore options
-        "--exit-on-error",
-        "--no-owner",
-        "--no-privileges",
-        # connection options
-        f"--host={url.host}",
-        f"--port={url.port}",
-        "--no-password",
-    ])
-    if data_only:
-        parts.append("--data-only")
-    else:
-        parts.extend(["--clean", "--if-exists"])
-    if jobs is not None:
-        parts.append(f"--jobs={jobs}")
-    if schemas is not None:
-        parts.extend([f"--schema={s}" for s in always_iterable(schemas)])
-    if schemas_exc is not None:
-        parts.extend([f"--exclude-schema={s}" for s in always_iterable(schemas_exc)])
-    if tables is not None:
-        parts.extend([f"--table={_get_table_name(t)}" for t in always_iterable(tables)])
-    if url.username is not None:
-        parts.append(f"--username={url.username}")
-    parts.append(str(path))
-    cmd = " ".join(parts)
+    cmd = _build_restore(
+        url,
+        path,
+        psql=psql,
+        database=database,
+        data_only=data_only,
+        jobs=jobs,
+        schemas=schemas,
+        schemas_exc=schemas_exc,
+        tables=tables,
+        docker=docker,
+    )
     if dry_run:
         if logger is not None:
             get_logger(logger=logger).info("Would run %r", str(cmd))
@@ -243,37 +208,156 @@ async def pg_restore(
                         )
 
 
+##
+
+
+def _build_restore(
+    url: URL,
+    path: PathLike,
+    /,
+    *,
+    psql: bool = False,
+    database: str | None = None,
+    data_only: bool = False,
+    jobs: int | None = None,
+    schemas: MaybeListStr | None = None,
+    schemas_exc: MaybeListStr | None = None,
+    tables: MaybeSequence[TableOrORMInstOrClass | str] | None = None,
+    docker: str | None = None,
+) -> str:
+    if psql:
+        return _build_restore_via_psql(url, path, database=database, docker=docker)
+    return _build_restore_via_pg_restore(
+        url,
+        path,
+        database=database,
+        data_only=data_only,
+        jobs=jobs,
+        schemas=schemas,
+        schemas_exc=schemas_exc,
+        tables=tables,
+        docker=docker,
+    )
+
+
+def _build_restore_via_pg_restore(
+    url: URL,
+    path: PathLike,
+    /,
+    *,
+    database: str | None = None,
+    data_only: bool = False,
+    jobs: int | None = None,
+    schemas: MaybeListStr | None = None,
+    schemas_exc: MaybeListStr | None = None,
+    tables: MaybeSequence[TableOrORMInstOrClass | str] | None = None,
+    docker: str | None = None,
+) -> str:
+    """Run `pg_restore`."""
+    url_database, host, port = _extract_url(url)
+    database_use = url_database if database is None else database
+    parts: list[str] = [
+        "pg_restore",
+        # general options
+        f"--dbname={database_use}",
+        "--verbose",
+        # restore options
+        "--exit-on-error",
+        "--no-owner",
+        "--no-privileges",
+        # connection options
+        f"--host={host}",
+        f"--port={port}",
+        "--no-password",
+    ]
+    if data_only:
+        parts.append("--data-only")
+    else:
+        parts.extend(["--clean", "--if-exists"])
+    if jobs is not None:
+        parts.append(f"--jobs={jobs}")
+    if schemas is not None:
+        parts.extend([f"--schema={s}" for s in always_iterable(schemas)])
+    if schemas_exc is not None:
+        parts.extend([f"--exclude-schema={s}" for s in always_iterable(schemas_exc)])
+    if tables is not None:
+        parts.extend([f"--table={_get_table_name(t)}" for t in always_iterable(tables)])
+    if url.username is not None:
+        parts.append(f"--username={url.username}")
+    if docker is not None:
+        parts = _wrap_docker(parts, docker)
+    parts.append(str(path))
+    return " ".join(parts)
+
+
+def _build_restore_via_psql(
+    url: URL,
+    path: PathLike,
+    /,
+    *,
+    database: str | None = None,
+    docker: str | None = None,
+) -> str:
+    """Run `psql`."""
+    url_database, host, port = _extract_url(url)
+    database_use = url_database if database is None else database
+    parts: list[str] = [
+        "psql",
+        # general options
+        f"--dbname={database_use}",
+        f"--file={str(path)!r}",
+        # connection options
+        f"--host={host}",
+        f"--port={port}",
+        "--no-password",
+    ]
+    if url.username is not None:
+        parts.append(f"--username={url.username}")
+    if docker is not None:
+        parts = _wrap_docker(parts, docker)
+    return " ".join(parts)
+
+
+##
+
+
+def _extract_url(url: URL, /) -> tuple[str, str, int]:
+    if url.database is None:
+        raise _ExtractURLDatabaseError(url=url)
+    if url.host is None:
+        raise _ExtractURLHostError(url=url)
+    if url.port is None:
+        raise _ExtractURLPortError(url=url)
+    return url.database, url.host, url.port
+
+
 @dataclass(kw_only=True, slots=True)
-class PGRestoreError(Exception):
+class ExtractURLError(Exception):
     url: URL
 
 
 @dataclass(kw_only=True, slots=True)
-class _PGRestoreDatabaseError(PGRestoreError):
+class _ExtractURLDatabaseError(ExtractURLError):
     @override
     def __str__(self) -> str:
         return f"Expected URL to contain a 'database'; got {self.url}"
 
 
 @dataclass(kw_only=True, slots=True)
-class _PGRestoreHostError(PGRestoreError):
+class _ExtractURLHostError(ExtractURLError):
     @override
     def __str__(self) -> str:
         return f"Expected URL to contain a 'host'; got {self.url}"
 
 
 @dataclass(kw_only=True, slots=True)
-class _PGRestorePortError(PGRestoreError):
+class _ExtractURLPortError(ExtractURLError):
     @override
     def __str__(self) -> str:
         return f"Expected URL to contain a 'port'; got {self.url}"
 
 
-##
-
-
 def _get_table_name(obj: TableOrORMInstOrClass | str, /) -> str:
-    """Get the table name from a Table or mapped class."""
     match obj:
         case Table() | DeclarativeBase() | type() as table_or_orm:
             return get_table_name(table_or_orm)
@@ -283,4 +367,8 @@ def _get_table_name(obj: TableOrORMInstOrClass | str, /) -> str:
             assert_never(never)
 
 
-__all__ = ["PGDumpError", "PGRestoreError", "pg_dump", "pg_restore"]
+def _wrap_docker(parts: list[str], container: str, /) -> list[str]:
+    return ["docker", "exec", "-it", container, *parts]
+
+
+__all__ = ["ExtractURLError", "pg_dump", "pg_restore"]
