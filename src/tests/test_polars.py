3bdc0f4de8e003df1190e37da54c833f12e27af2
Derek Wan <d.wan@icloud.com>
2025-08-19 08:49:13 +0900

    <derekwan/DW-Mac/20250819T084913 (Tue)>

--- src/tests/test_polars.py
+++ src/tests/test_polars.py
@@ -1426,17 +1426,17 @@ class TestDataClassToSchema:
 
 
 class TestDatetimeDTypes:
-    @given(
-        case=sampled_from([
-            (HongKong, DatetimeHongKong),
-            (Tokyo, DatetimeTokyo),
-            (USCentral, DatetimeUSCentral),
-            (USEastern, DatetimeUSEastern),
-            (UTC, DatetimeUTC),
-        ])
+    @mark.parametrize(
+        ("time_zone", "dtype"),
+        [
+            param(HongKong, DatetimeHongKong),
+            param(Tokyo, DatetimeTokyo),
+            param(USCentral, DatetimeUSCentral),
+            param(USEastern, DatetimeUSEastern),
+            param(UTC, DatetimeUTC),
+        ],
     )
-    def test_main(self, *, case: tuple[ZoneInfo, Datetime]) -> None:
-        time_zone, dtype = case
+    def test_main(self, *, time_zone: ZoneInfo, dtype: Datetime) -> None:
         name = to_time_zone_name(time_zone)
         expected = dtype.time_zone
         assert name == expected
