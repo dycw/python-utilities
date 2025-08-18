81847dcac1c772a4d09d039ba933eed54c2496b6
Derek Wan <d.wan@icloud.com>
2025-08-19 08:47:55 +0900

    <derekwan/DW-Mac/20250819T084755 (Tue)>

--- src/tests/test_zoneinfo.py
+++ src/tests/test_zoneinfo.py
@@ -105,7 +105,8 @@ class TestToTimeZoneName:
 
     def test_tz_info(self) -> None:
         result = to_time_zone_name(dt.UTC)
-        assert result == "UTC"
+        expected = UTC.key
+        assert result == expected
 
     @given(data=data(), time_zone=zone_infos())
     def test_py_zoned_date_time(self, *, data: DataObject, time_zone: ZoneInfo) -> None:
