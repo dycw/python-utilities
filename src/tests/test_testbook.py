bc4ffcb4cc851493cdb9668c408677fd45e4cd60
Derek Wan <d.wan@icloud.com>
2025-08-20 09:11:03 +0900

    <derekwan/DW-Mac/20250820T091103 (Wed)>

--- src/tests/test_testbook.py
+++ src/tests/test_testbook.py
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
 
 class TestBuildTestClass:
     @mark.parametrize("throttle", [param(HOUR), param(None)])
-    def test_main(self, *, tmp_path: Path) -> None:
+    def test_main(self, *, tmp_path: Path, throttle: bool) -> None:
         data = {"cells": []}
         _ = tmp_path.joinpath("notebook.ipynb").write_text(dumps(data))
-        _ = build_test_class(tmp_path)
+        _ = build_test_class(tmp_path, throttle=throttle)
