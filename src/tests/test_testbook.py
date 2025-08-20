14bd6e34703295955dc8dd2caa073858e108d301
Derek Wan <d.wan@icloud.com>
2025-08-20 09:37:14 +0900

    <derekwan/DW-Mac/20250820T093714 (Wed)>

--- src/tests/test_testbook.py
+++ src/tests/test_testbook.py
@@ -11,10 +11,12 @@ from utilities.whenever import HOUR
 if TYPE_CHECKING:
     from pathlib import Path
 
+    from utilities.types import Delta
+
 
 class TestBuildNotebookTester:
     @mark.parametrize("throttle", [param(HOUR), param(None)])
-    def test_main(self, *, tmp_path: Path, throttle: bool) -> None:
+    def test_main(self, *, tmp_path: Path, throttle: Delta | None) -> None:
         data = {"cells": []}
         _ = tmp_path.joinpath("notebook.ipynb").write_text(dumps(data))
         _ = build_notebook_tester(tmp_path, throttle=throttle)
