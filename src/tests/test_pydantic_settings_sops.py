0fcca432b1f592cff1fd6fce65357589677a805b
Derek Wan <d.wan@icloud.com>
2025-09-06 16:08:54 +0900

    2025-09-06 16:08:53 (Sat) > DW-Mac > derekwan

--- src/tests/test_pydantic_settings_sops.py
+++ src/tests/test_pydantic_settings_sops.py
@@ -8,15 +8,13 @@ from typing import TYPE_CHECKING, ClassVar
 from tests.conftest import SKIPIF_CI
 from utilities.iterables import one
 from utilities.os import temp_environ
-from utilities.pydantic_settings import load_settings
+from utilities.pydantic_settings import PathLikeOrWithSection, load_settings
 from utilities.pydantic_settings_sops import SopsBaseSettings
 from utilities.re import extract_group
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
     from pathlib import Path
-
-    from utilities.types import PathLike
 
 
 class TestSOPSBaseSettings:
@@ -40,7 +38,7 @@ class TestSOPSBaseSettings:
             _ = check_call(["sops", "encrypt", str(unencrypted_file)], stdout=file)
 
         class Settings(SopsBaseSettings):
-            secret_files: ClassVar[Sequence[PathLike]] = [encrypted_file]
+            secret_files: ClassVar[Sequence[PathLikeOrWithSection]] = [encrypted_file]
             x: int
             y: int
 
