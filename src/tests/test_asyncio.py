af7e4f74ebd25174330d448254101c2e79b52e3b
Derek Wan <d.wan@icloud.com>
2025-09-13 15:34:10 +0900

    2025-09-13 15:34:10 (Sat) > DW-Mac > derekwan

--- src/tests/test_asyncio.py
+++ src/tests/test_asyncio.py
@@ -351,7 +351,10 @@ class TestOneAsync:
 
     @mark.parametrize("args", [param([]), param(([], [])), param(([], [], []))])
     async def test_error_empty(self, *, args: tuple[Iterable[Any], ...]) -> None:
-        with raises(OneAsyncEmptyError, match=r"Iterable\(s\) .* must not be empty"):
+        with raises(
+            OneAsyncEmptyError,
+            match=re.compile(r"Iterable\(s\) .* must not be empty", flags=DOTALL),
+        ):
             _ = await one_async(*map(self._lift, args))
 
     @given(iterable=sets(integers(), min_size=2))
