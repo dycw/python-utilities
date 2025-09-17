108450050e995347c02ca4e1eda56a048fc75829
Derek Wan <d.wan@icloud.com>
2025-09-17 21:38:22 +0900

    2025-09-17 21:38:20 (Wed) > DW-Mac > derekwan

--- src/tests/test_jinja2.py
+++ src/tests/test_jinja2.py
@@ -1,11 +1,26 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from jinja2 import DictLoader
 
-if TYPE_CHECKING:
-    from pathlib import Path
+from utilities.jinja2 import EnhancedEnvironment
+from utilities.text import strip_and_dedent
 
 
 class TestEnhancedTemplate:
-    def test_main(self, *, tmp_path: Path) -> None:
-        pass
+    def test_main(self) -> None:
+        env = EnhancedEnvironment(
+            loader=DictLoader({
+                "test.j2": strip_and_dedent("""
+                    text   = '{{ text }}'
+                    pascal = '{{ text | pascal }}'
+                    snake  = '{{ text | snake }}'
+                """)
+            })
+        )
+        result = env.get_template("test.j2").render(text="multi-word string")
+        expected = strip_and_dedent("""
+            text   = 'multi-word string'
+            pascal = 'MultiWordString'
+            snake  = 'multi_word_string'
+        """)
+        assert result == expected
