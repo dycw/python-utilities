7aec4846b9a28f66bff466a765f9dda3f9c14c18
Derek Wan <d.wan@icloud.com>
2025-09-27 13:23:16 +0900

    2025-09-27 13:23:14 (Sat) > DW-Mac > derekwan

--- src/utilities/script.py
+++ src/utilities/script.py
@@ -1,17 +1,59 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING, ClassVar, override
 
-from utilities.pydantic_settings import BaseSettings, PathLikeOrWithSection
+from pydantic_settings import CliSettingsSource
+
+from utilities.pydantic_settings import (
+    BaseSettings,
+    CustomBaseSettings,
+    PathLikeOrWithSection,
+    load_settings,
+)
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
+    from pydantic_settings import PydanticBaseSettingsSource
 
-class Foo(BaseSettings):
-    toml_files: ClassVar[Sequence[PathLikeOrWithSection]] = [
-        Path(__file__).parent.joinpath("config.toml")
-    ]
+config = Path(__file__).parent.joinpath("config.toml")
+print(config)  # noqa: T201
+assert config.exists()
 
-    a: int
+
+class _Settings(CustomBaseSettings):
+    toml_files: ClassVar[Sequence[PathLikeOrWithSection]] = [config]
+
+    aaa: int
+
+    @classmethod
+    @override
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            *super().settings_customise_sources(
+                settings_cls,
+                init_settings,
+                env_settings,
+                dotenv_settings,
+                file_secret_settings,
+            ),
+            CliSettingsSource(settings_cls, cli_parse_args=True),
+        )
+
+
+def main() -> None:
+    print("script...")  # noqa: T201
+    settings = load_settings(_Settings)
+    print(settings)  # noqa: T201
+
+
+if __name__ == "__main__":
+    main()
