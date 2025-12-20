7ae9fa69f5af21663b2ff93d0151394160ea2c8e
Derek Wan <d.wan@icloud.com>
2025-12-20 12:08:30 +0900

    Add `subprocess.run` (#1738)

---
+++ src/tests/test_subprocess.py
@@ -0,0 +1,95 @@
+from __future__ import annotations
+
+from subprocess import CalledProcessError
+from typing import TYPE_CHECKING
+
+from pytest import raises
+
+from utilities.subprocess import run
+
+if TYPE_CHECKING:
+    from pytest import CaptureFixture
+
+
+class TestRun:
+    def test_main(self, *, capsys: CaptureFixture) -> None:
+        result = run("echo stdout; sleep 0.5; echo stderr 1>&2", shell=True)  # noqa: S604
+        assert result is None
+        cap = capsys.readouterr()
+        assert cap.out == ""
+        assert cap.err == ""
+
+    def test_print(self, *, capsys: CaptureFixture) -> None:
+        result = run("echo stdout; sleep 0.5; echo stderr 1>&2", shell=True, print=True)  # noqa: S604
+        assert result is None
+        cap = capsys.readouterr()
+        assert cap.out == "stdout\n"
+        assert cap.err == "stderr\n"
+
+    def test_print_stdout(self, *, capsys: CaptureFixture) -> None:
+        result = run(  # noqa: S604
+            "echo stdout; sleep 0.5; echo stderr 1>&2", shell=True, print_stdout=True
+        )
+        assert result is None
+        cap = capsys.readouterr()
+        assert cap.out == "stdout\n"
+        assert cap.err == ""
+
+    def test_print_stderr(self, *, capsys: CaptureFixture) -> None:
+        result = run(  # noqa: S604
+            "echo stdout; sleep 0.5; echo stderr 1>&2", shell=True, print_stderr=True
+        )
+        assert result is None
+        cap = capsys.readouterr()
+        assert cap.out == ""
+        assert cap.err == "stderr\n"
+
+    def test_return(self, *, capsys: CaptureFixture) -> None:
+        result = run(  # noqa: S604
+            "echo stdout; sleep 0.5; echo stderr 1>&2", shell=True, return_=True
+        )
+        expected = "stdout\nstderr\n"
+        assert result == expected
+        cap = capsys.readouterr()
+        assert cap.out == ""
+        assert cap.err == ""
+
+    def test_return_stdout(self, *, capsys: CaptureFixture) -> None:
+        result = run(  # noqa: S604
+            "echo stdout; sleep 0.5; echo stderr 1>&2", shell=True, return_stdout=True
+        )
+        expected = "stdout\n"
+        assert result == expected
+        cap = capsys.readouterr()
+        assert cap.out == ""
+        assert cap.err == ""
+
+    def test_return_stderr(self, *, capsys: CaptureFixture) -> None:
+        result = run(  # noqa: S604
+            "echo stdout; sleep 0.5; echo stderr 1>&2", shell=True, return_stderr=True
+        )
+        expected = "stderr\n"
+        assert result == expected
+        cap = capsys.readouterr()
+        assert cap.out == ""
+        assert cap.err == ""
+
+    def test_error(self, *, capsys: CaptureFixture) -> None:
+        with raises(CalledProcessError) as exc_info:
+            _ = run("echo stdout; echo stderr 1>&2; exit 1", shell=True)  # noqa: S604
+        assert exc_info.value.returncode == 1
+        assert exc_info.value.stdout == "stdout\n"
+        assert exc_info.value.stderr == "stderr\n"
+        cap = capsys.readouterr()
+        assert cap.out == ""
+        assert cap.err == ""
+
+    def test_error_and_print(self, *, capsys: CaptureFixture) -> None:
+        with raises(CalledProcessError) as exc_info:
+            _ = run("echo stdout; echo stderr 1>&2; exit 1", shell=True, print=True)  # noqa: S604
+        assert exc_info.value.returncode == 1
+        assert exc_info.value.stdout == "stdout\n"
+        assert exc_info.value.stderr == "stderr\n"
+        cap = capsys.readouterr()
+        assert cap.out == "stdout\n"
+        assert cap.err == "stderr\n"
---
+++ src/utilities/subprocess.py
@@ -0,0 +1,199 @@
+from __future__ import annotations
+
+import sys
+from contextlib import contextmanager
+from io import StringIO
+from subprocess import PIPE, CalledProcessError, Popen
+from threading import Thread
+from typing import IO, TYPE_CHECKING, Literal, assert_never, overload
+
+from utilities.errors import ImpossibleCaseError
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from utilities.types import StrStrMapping
+
+
+@overload
+def run(
+    cmd: str,
+    /,
+    *cmds: str,
+    executable: str | None = None,
+    shell: bool = False,
+    cwd: str | None = None,
+    env: StrStrMapping | None = None,
+    user: str | int | None = None,
+    group: str | int | None = None,
+    print: bool = False,
+    print_stdout: bool = False,
+    print_stderr: bool = False,
+    return_: Literal[True],
+    return_stdout: bool = False,
+    return_stderr: bool = False,
+) -> str: ...
+@overload
+def run(
+    cmd: str,
+    /,
+    *cmds: str,
+    executable: str | None = None,
+    shell: bool = False,
+    cwd: str | None = None,
+    env: StrStrMapping | None = None,
+    user: str | int | None = None,
+    group: str | int | None = None,
+    print: bool = False,
+    print_stdout: bool = False,
+    print_stderr: bool = False,
+    return_: bool = False,
+    return_stdout: Literal[True],
+    return_stderr: bool = False,
+) -> str: ...
+@overload
+def run(
+    cmd: str,
+    /,
+    *cmds: str,
+    executable: str | None = None,
+    shell: bool = False,
+    cwd: str | None = None,
+    env: StrStrMapping | None = None,
+    user: str | int | None = None,
+    group: str | int | None = None,
+    print: bool = False,
+    print_stdout: bool = False,
+    print_stderr: bool = False,
+    return_: bool = False,
+    return_stdout: bool = False,
+    return_stderr: Literal[True],
+) -> str: ...
+@overload
+def run(
+    cmd: str,
+    /,
+    *cmds: str,
+    executable: str | None = None,
+    shell: bool = False,
+    cwd: str | None = None,
+    env: StrStrMapping | None = None,
+    user: str | int | None = None,
+    group: str | int | None = None,
+    print: bool = False,
+    print_stdout: bool = False,
+    print_stderr: bool = False,
+    return_: Literal[False] = False,
+    return_stdout: Literal[False] = False,
+    return_stderr: Literal[False] = False,
+) -> None: ...
+@overload
+def run(
+    cmd: str,
+    /,
+    *cmds: str,
+    executable: str | None = None,
+    shell: bool = False,
+    cwd: str | None = None,
+    env: StrStrMapping | None = None,
+    user: str | int | None = None,
+    group: str | int | None = None,
+    print: bool = False,
+    print_stdout: bool = False,
+    print_stderr: bool = False,
+    return_: bool = False,
+    return_stdout: bool = False,
+    return_stderr: bool = False,
+) -> str | None: ...
+def run(
+    cmd: str,
+    /,
+    *cmds: str,
+    executable: str | None = None,
+    shell: bool = False,
+    cwd: str | None = None,
+    env: StrStrMapping | None = None,
+    user: str | int | None = None,
+    group: str | int | None = None,
+    print: bool = False,  # noqa: A002
+    print_stdout: bool = False,
+    print_stderr: bool = False,
+    return_: bool = False,
+    return_stdout: bool = False,
+    return_stderr: bool = False,
+) -> str | None:
+    buffer = StringIO()
+    stdout = StringIO()
+    stderr = StringIO()
+    with Popen(
+        [cmd, *cmds],
+        bufsize=1,
+        executable=executable,
+        stdout=PIPE,
+        stderr=PIPE,
+        shell=shell,
+        cwd=cwd,
+        env=env,
+        text=True,
+        user=user,
+        group=group,
+    ) as proc:
+        if proc.stdout is None:  # pragma: no cover
+            raise ImpossibleCaseError(case=[f"{proc.stdout=}"])
+        if proc.stderr is None:  # pragma: no cover
+            raise ImpossibleCaseError(case=[f"{proc.stderr=}"])
+        with (
+            _yield_write(
+                proc.stdout,
+                buffer,
+                stdout,
+                *([sys.stdout] if print or print_stdout else []),
+            ),
+            _yield_write(
+                proc.stderr,
+                buffer,
+                stderr,
+                *([sys.stderr] if print or print_stderr else []),
+            ),
+        ):
+            return_code = proc.wait()
+        match return_code, return_ or return_stdout, return_ or return_stderr:
+            case 0, True, True:
+                _ = buffer.seek(0)
+                return buffer.read()
+            case 0, True, False:
+                _ = stdout.seek(0)
+                return stdout.read()
+            case 0, False, True:
+                _ = stderr.seek(0)
+                return stderr.read()
+            case 0, False, False:
+                return None
+            case _, _, _:
+                _ = stdout.seek(0)
+                _ = stderr.seek(0)
+                raise CalledProcessError(
+                    return_code, cmd, output=stdout.read(), stderr=stderr.read()
+                )
+            case never:
+                assert_never(never)
+
+
+@contextmanager
+def _yield_write(input_: IO[str], /, *outputs: IO[str]) -> Iterator[None]:
+    thread = Thread(target=_run_target, args=(input_, *outputs), daemon=True)
+    thread.start()
+    try:
+        yield
+    finally:
+        thread.join()
+
+
+def _run_target(input_: IO[str], /, *outputs: IO[str]) -> None:
+    with input_:
+        for line in iter(input_.readline, ""):
+            for output in outputs:
+                _ = output.write(line)
+
+
+__all__ = ["run"]
