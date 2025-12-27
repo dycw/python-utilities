1bf4c1272e357023e63bb3b4b101c25bcc616fa8
Derek Wan <d.wan@icloud.com>
2025-12-27 12:24:49 +0900

    2025-12-27 12:24:49 (Sat)  > DW-Mac  > derekwan

--- src/utilities/permissions.py
+++ src/utilities/permissions.py
@@ -5,7 +5,13 @@ from re import search
 from typing import Literal, Self, override
 
 from utilities.dataclasses import replace_non_sentinel
+from utilities.functions import ensure_member
+from utilities.re import ExtractGroupsError, extract_groups
 from utilities.sentinel import Sentinel, sentinel
+from utilities.typing import get_args
+
+type _ZeroToSeven = Literal[0, 1, 2, 3, 4, 5, 6, 7]
+_ZERO_TO_SEVEN: list[_ZeroToSeven] = list(get_args(_ZeroToSeven.__value__))
 
 
 @dataclass(order=True, unsafe_hash=True, kw_only=True, slots=True)
@@ -16,9 +22,9 @@ class Permissions:
     group_read: bool = False
     group_write: bool = False
     group_execute: bool = False
-    other_read: bool = False
-    other_write: bool = False
-    other_execute: bool = False
+    others_read: bool = False
+    others_write: bool = False
+    others_execute: bool = False
 
     @override
     def __repr__(self) -> str:
@@ -37,9 +43,9 @@ class Permissions:
             ),
             self._repr_parts(
                 "o",
-                read=self.other_read,
-                write=self.other_write,
-                execute=self.other_execute,
+                read=self.others_read,
+                write=self.others_write,
+                execute=self.others_execute,
             ),
         ])
 
@@ -72,13 +78,15 @@ class Permissions:
                 read=self.group_read, write=self.group_write, execute=self.group_execute
             )
             + self._to_int_part(
-                read=self.other_read, write=self.other_write, execute=self.other_execute
+                read=self.others_read,
+                write=self.others_write,
+                execute=self.others_execute,
             )
         )
 
     def _to_int_part(
         self, *, read: bool = False, write: bool = False, execute: bool = False
-    ) -> Literal[0, 1, 2, 3, 4, 5, 6, 7]:
+    ) -> _ZeroToSeven:
         return (4 if read else 0) + (2 if write else 0) + (1 if execute else 0)
 
     @override
@@ -87,12 +95,19 @@ class Permissions:
 
     @classmethod
     def from_int(cls, n: int, /) -> Self:
-        as_str = str(n)
-        if not search(r"^[0-7]{3}$", as_str):
-            raise PermissionsFromIntError(n=n)
-        user_read, user_write, user_execute = cls._from_int_part(n // 100)
-        group_read, group_write, group_execute = cls._from_int_part((n // 10) % 10)
-        other_read, other_write, other_execute = cls._from_int_part(n % 10)
+        try:
+            user, group, others = extract_groups(r"^([0-7])([0-7])([0-7])$", str(n))
+        except ExtractGroupsError:
+            raise PermissionsFromIntError(n=n) from None
+        user_read, user_write, user_execute = cls._from_int(
+            ensure_member(int(user), _ZERO_TO_SEVEN)
+        )
+        group_read, group_write, group_execute = cls._from_int(
+            ensure_member(int(group), _ZERO_TO_SEVEN)
+        )
+        others_read, others_write, others_execute = cls._from_int(
+            ensure_member(int(others), _ZERO_TO_SEVEN)
+        )
         return cls(
             user_read=user_read,
             user_write=user_write,
@@ -100,14 +115,42 @@ class Permissions:
             group_read=group_read,
             group_write=group_write,
             group_execute=group_execute,
-            other_read=other_read,
-            other_write=other_write,
-            other_execute=other_execute,
+            others_read=others_read,
+            others_write=others_write,
+            others_execute=others_execute,
         )
 
     @classmethod
-    def _from_int_part(cls, n: int, /) -> tuple[bool, bool, bool]:
+    def _from_int(cls, n: _ZeroToSeven, /) -> tuple[bool, bool, bool]:
         return bool(4 & n), bool(2 & n), bool(1 & n)
+
+    @classmethod
+    def from_str(cls, text: str, /) -> Self:
+        try:
+            user, group, others = extract_groups(
+                r"^u=([r?w?x?]),g=([r?w?x?]),o=([r?w?x?])$", text
+            )
+        except ExtractGroupsError:
+            raise PermissionsFromStrError(text=text) from None
+        user_read, user_write, user_execute = cls._from_str(user)
+        group_read, group_write, group_execute = cls._from_str(group)
+        others_read, others_write, others_execute = cls._from_str(others)
+        return cls(
+            user_read=user_read,
+            user_write=user_write,
+            user_execute=user_execute,
+            group_read=group_read,
+            group_write=group_write,
+            group_execute=group_execute,
+            others_read=others_read,
+            others_write=others_write,
+            others_execute=others_execute,
+        )
+
+    @classmethod
+    def _from_str(cls, text: str, /) -> tuple[bool, bool, bool]:
+        read, write, execute = extract_groups("^(r?)(w?)(x?)$", text)
+        return read != "", write != "", execute != ""
 
     def replace(
         self,
@@ -118,9 +161,9 @@ class Permissions:
         group_read: bool | Sentinel = sentinel,
         group_write: bool | Sentinel = sentinel,
         group_execute: bool | Sentinel = sentinel,
-        other_read: bool | Sentinel = sentinel,
-        other_write: bool | Sentinel = sentinel,
-        other_execute: bool | Sentinel = sentinel,
+        others_read: bool | Sentinel = sentinel,
+        others_write: bool | Sentinel = sentinel,
+        others_execute: bool | Sentinel = sentinel,
     ) -> Self:
         return replace_non_sentinel(
             self,
@@ -130,9 +173,9 @@ class Permissions:
             group_read=group_read,
             group_write=group_write,
             group_execute=group_execute,
-            other_read=other_read,
-            other_write=other_write,
-            other_execute=other_execute,
+            others_read=others_read,
+            others_write=others_write,
+            others_execute=others_execute,
         )
 
 
@@ -149,4 +192,18 @@ class PermissionsFromIntError(PermissionsError):
         return f"Invalid integer for permissions; got {self.n}"
 
 
-__all__ = ["Permissions", "PermissionsError", "PermissionsFromIntError"]
+@dataclass(kw_only=True, slots=True)
+class PermissionsFromStrError(PermissionsError):
+    text: str
+
+    @override
+    def __str__(self) -> str:
+        return f"Invalid string for permissions; got {self.text}"
+
+
+__all__ = [
+    "Permissions",
+    "PermissionsError",
+    "PermissionsFromIntError",
+    "PermissionsFromStrError",
+]
