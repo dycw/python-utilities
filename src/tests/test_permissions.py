aec2d6e50d1947ea0a7c3ba17d8573544b249b3d
Derek Wan <d.wan@icloud.com>
2025-12-27 13:32:24 +0900

    2025-12-27 13:32:24 (Sat)  > DW-Mac  > derekwan

--- src/tests/test_permissions.py
+++ src/tests/test_permissions.py
@@ -11,22 +11,34 @@ from utilities.permissions import Permissions
 class _Case[T]:
     perms: Permissions
     human_int: int
+    octal: int
     text: str
 
 
 _CASES: list[_Case] = [
-    _Case(perms=Permissions(), human_int=0, text="u=,g=,o="),
-    _Case(perms=Permissions(user_read=True), human_int=400, text="u=r,g=,o="),
-    _Case(perms=Permissions(group_write=True), human_int=20, text="u=,g=w,o="),
-    _Case(perms=Permissions(others_execute=True), human_int=1, text="u=,g=,o=x"),
+    _Case(perms=Permissions(), human_int=0, octal=0o0, text="u=,g=,o="),
+    _Case(
+        perms=Permissions(user_read=True), human_int=400, octal=0o400, text="u=r,g=,o="
+    ),
+    _Case(
+        perms=Permissions(group_write=True), human_int=20, octal=0o020, text="u=,g=w,o="
+    ),
+    _Case(
+        perms=Permissions(others_execute=True),
+        human_int=1,
+        octal=0o001,
+        text="u=,g=,o=x",
+    ),
     _Case(
         perms=Permissions(user_read=True, user_write=True),
         human_int=600,
+        octal=0o600,
         text="u=rw,g=,o=",
     ),
     _Case(
         perms=Permissions(user_read=True, group_execute=True),
         human_int=410,
+        octal=0o410,
         text="u=r,g=x,o=",
     ),
 ]
@@ -39,12 +51,20 @@ class TestPermissions:
     def test_human_int(self, *, perms: Permissions, expected: str) -> None:
         result = perms.human_int
         assert result == expected
-        assert Permissions.from_int(result) == perms
+        assert Permissions.from_human_int(result) == perms
+
+    @mark.parametrize(
+        ("perms", "expected"), [param(case.perms, case.octal) for case in _CASES]
+    )
+    def test_octal(self, *, perms: Permissions, expected: str) -> None:
+        result = perms.octal
+        assert result == expected
+        assert Permissions.from_octal(result) == perms
 
     @mark.parametrize(
         ("perms", "expected"), [param(case.perms, case.text) for case in _CASES]
     )
-    def test_to_and_from_str(self, *, perms: Permissions, expected: str) -> None:
+    def test_text(self, *, perms: Permissions, expected: str) -> None:
         result = str(perms)
         assert result == expected
-        assert Permissions.from_str(result) == perms
+        assert Permissions.from_text(result) == perms
--- src/utilities/permissions.py
+++ src/utilities/permissions.py
@@ -2,6 +2,19 @@ from __future__ import annotations
 
 from contextlib import suppress
 from dataclasses import dataclass
+from functools import reduce
+from operator import or_
+from stat import (
+    S_IRGRP,
+    S_IROTH,
+    S_IRUSR,
+    S_IWGRP,
+    S_IWOTH,
+    S_IWUSR,
+    S_IXGRP,
+    S_IXOTH,
+    S_IXUSR,
+)
 from typing import Literal, Self, override
 
 from utilities.dataclasses import replace_non_sentinel
@@ -77,16 +90,16 @@ class Permissions:
         return repr(self)
 
     @classmethod
-    def from_int(cls, n: int, /) -> Self:
+    def from_human_int(cls, n: int, /) -> Self:
         with suppress(ExtractGroupsError):
             user, group, others = extract_groups(r"^([0-7])([0-7])([0-7])$", str(n))
-            user_read, user_write, user_execute = cls._from_int(
+            user_read, user_write, user_execute = cls._from_human_int(
                 ensure_member(int(user), _ZERO_TO_SEVEN)
             )
-            group_read, group_write, group_execute = cls._from_int(
+            group_read, group_write, group_execute = cls._from_human_int(
                 ensure_member(int(group), _ZERO_TO_SEVEN)
             )
-            others_read, others_write, others_execute = cls._from_int(
+            others_read, others_write, others_execute = cls._from_human_int(
                 ensure_member(int(others), _ZERO_TO_SEVEN)
             )
             return cls(
@@ -102,10 +115,10 @@ class Permissions:
             )
         with suppress(ExtractGroupsError):
             group, others = extract_groups(r"^([0-7])([0-7])$", str(n))
-            group_read, group_write, group_execute = cls._from_int(
+            group_read, group_write, group_execute = cls._from_human_int(
                 ensure_member(int(group), _ZERO_TO_SEVEN)
             )
-            others_read, others_write, others_execute = cls._from_int(
+            others_read, others_write, others_execute = cls._from_human_int(
                 ensure_member(int(others), _ZERO_TO_SEVEN)
             )
             return cls(
@@ -118,7 +131,7 @@ class Permissions:
             )
         with suppress(ExtractGroupError):
             others = extract_group(r"^([0-7])$", str(n))
-            others_read, others_write, others_execute = cls._from_int(
+            others_read, others_write, others_execute = cls._from_human_int(
                 ensure_member(int(others), _ZERO_TO_SEVEN)
             )
             return cls(
@@ -129,20 +142,34 @@ class Permissions:
         return cls()
 
     @classmethod
-    def _from_int(cls, n: _ZeroToSeven, /) -> tuple[bool, bool, bool]:
+    def _from_human_int(cls, n: _ZeroToSeven, /) -> tuple[bool, bool, bool]:
         return bool(4 & n), bool(2 & n), bool(1 & n)
 
     @classmethod
-    def from_str(cls, text: str, /) -> Self:
+    def from_octal(cls, n: int, /) -> Self:
+        return cls(
+            user_read=bool(n & S_IRUSR),
+            user_write=bool(n & S_IWUSR),
+            user_execute=bool(n & S_IXUSR),
+            group_read=bool(n & S_IRGRP),
+            group_write=bool(n & S_IWGRP),
+            group_execute=bool(n & S_IXGRP),
+            others_read=bool(n & S_IROTH),
+            others_write=bool(n & S_IWOTH),
+            others_execute=bool(n & S_IXOTH),
+        )
+
+    @classmethod
+    def from_text(cls, text: str, /) -> Self:
         try:
             user, group, others = extract_groups(
                 r"^u=(r?w?x?),g=(r?w?x?),o=(r?w?x?)$", text
             )
         except ExtractGroupsError:
             raise PermissionsFromStrError(text=text) from None
-        user_read, user_write, user_execute = cls._from_str(user)
-        group_read, group_write, group_execute = cls._from_str(group)
-        others_read, others_write, others_execute = cls._from_str(others)
+        user_read, user_write, user_execute = cls._from_text_part(user)
+        group_read, group_write, group_execute = cls._from_text_part(group)
+        others_read, others_write, others_execute = cls._from_text_part(others)
         return cls(
             user_read=user_read,
             user_write=user_write,
@@ -156,7 +183,7 @@ class Permissions:
         )
 
     @classmethod
-    def _from_str(cls, text: str, /) -> tuple[bool, bool, bool]:
+    def _from_text_part(cls, text: str, /) -> tuple[bool, bool, bool]:
         read, write, execute = extract_groups("^(r?)(w?)(x?)$", text)
         return read != "", write != "", execute != ""
 
@@ -182,6 +209,21 @@ class Permissions:
         self, *, read: bool = False, write: bool = False, execute: bool = False
     ) -> _ZeroToSeven:
         return (4 if read else 0) + (2 if write else 0) + (1 if execute else 0)
+
+    @property
+    def octal(self) -> int:
+        flags: list[int] = [
+            S_IRUSR if self.user_read else 0,
+            S_IWUSR if self.user_write else 0,
+            S_IXUSR if self.user_execute else 0,
+            S_IRGRP if self.group_read else 0,
+            S_IWGRP if self.group_read else 0,
+            S_IXGRP if self.group_read else 0,
+            S_IROTH if self.others_read else 0,
+            S_IWOTH if self.others_read else 0,
+            S_IXOTH if self.others_read else 0,
+        ]
+        return reduce(or_, flags)
 
     def replace(
         self,
