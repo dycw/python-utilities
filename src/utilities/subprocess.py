da0c49c3415a5b79a469e168beb4530709325191
Derek Wan <d.wan@icloud.com>
2026-01-01 07:26:38 +0900

    2026-01-01 07:26:38 (Thu)  > DW-Mac  > derekwan

--- src/utilities/subprocess.py
+++ src/utilities/subprocess.py
@@ -49,6 +49,9 @@ UPDATE_CA_CERTIFICATES: str = "update-ca-certificates"
 ##
 
 
+##
+
+
 def apt_install(package: str, /, *, update: bool = False, sudo: bool = False) -> None:
     """Install a package."""
     if update:  # pragma: no cover
