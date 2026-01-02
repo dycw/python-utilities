8eb933bb622f6dd7f514a328613aced62eac55c1
Derek Wan <d.wan@icloud.com>
2026-01-02 10:21:26 +0900

    2026-01-02 10:21:26 (Fri)  > DW-Mac  > derekwan

--- src/tests/test_docker.py
+++ src/tests/test_docker.py
@@ -9,6 +9,7 @@ from typing import TYPE_CHECKING
 from pytest import CaptureFixture, LogCaptureFixture, mark, param, raises
 
 from utilities.docker import (
+    _docker_compose_cmd,
     docker_compose_down_cmd,
     docker_compose_pull_cmd,
     docker_compose_up_cmd,
@@ -30,6 +31,31 @@ if TYPE_CHECKING:
     from utilities.types import PathLike
 
 
+class TestDockerComposeCmd:
+    def test_main(self) -> None:
+        result = _docker_compose_cmd("cmd")
+        expected = ["docker", "compose", "cmd"]
+        assert result == expected
+
+    def test_single_file(self) -> None:
+        result = _docker_compose_cmd("cmd", files="compose.yaml")
+        expected = ["docker", "compose", "--file", "compose.yaml", "cmd"]
+        assert result == expected
+
+    def test_multiple_files(self) -> None:
+        result = _docker_compose_cmd("cmd", files=["compose1.yaml", "compose2.yaml"])
+        expected = [
+            "docker",
+            "compose",
+            "--file",
+            "compose1.yaml",
+            "--file",
+            "compose2.yaml",
+            "cmd",
+        ]
+        assert result == expected
+
+
 class TestDockerComposeDownCmd:
     def test_main(self) -> None:
         result = docker_compose_down_cmd()
@@ -48,24 +74,6 @@ class TestDockerComposeUpCmd:
     def test_main(self) -> None:
         result = docker_compose_up_cmd()
         expected = ["docker", "compose", "up"]
-        assert result == expected
-
-    def test_single_file(self) -> None:
-        result = docker_compose_up_cmd(files="compose.yaml")
-        expected = ["docker", "compose", "--file", "compose.yaml", "up"]
-        assert result == expected
-
-    def test_multiple_files(self) -> None:
-        result = docker_compose_up_cmd(files=["compose1.yaml", "compose2.yaml"])
-        expected = [
-            "docker",
-            "compose",
-            "--file",
-            "compose1.yaml",
-            "--file",
-            "compose2.yaml",
-            "up",
-        ]
         assert result == expected
 
 
