ea6957a8c3752d336093b14167ebf5e3d335d203
Derek Wan <d.wan@icloud.com>
2026-01-08 10:41:32 +0900

    2026-01-08 10:41:32 (Thu)  > DW-Mac  > derekwan

--- src/tests/test_packaging.py
+++ src/tests/test_packaging.py
@@ -1,9 +1,32 @@
 from __future__ import annotations
 
 from packaging.requirements import InvalidRequirement
-from pytest import raises
+from pytest import mark, param, raises
 
-from utilities.packaging import SortedRequirement
+from utilities.packaging import SortedRequirement, format_requirement
+
+
+class TestFormatRequirement:
+    @mark.parametrize(
+        ("requirement", "expected"),
+        [
+            param("package", "package"),
+            param("package>=1.2.3", "package>=1.2.3"),
+            param("package<1.3", "package<1.3"),
+            param("package>=1.2.3,<1.3", "package>=1.2.3, <1.3"),
+            param("package<1.3,>=1.2.3", "package>=1.2.3, <1.3"),
+            param("package[extra]>=1.2.3", "package[extra]>=1.2.3"),
+            param("package[extra1,extra2]>=1.2.3", "package[extra1,extra2]>=1.2.3"),
+            param("package@https://www.github.com", "package@ https://www.github.com"),
+            param("package;python_version>='3'", 'package; python_version >= "3"'),
+            param(
+                "package[extra1,extra2]@https://www.github.com>=1.2.3,<1.3;python_version>='3'",
+                "package[extra1,extra2]@ https://www.github.com>=1.2.3,<1.3;python_version>='3'",
+            ),
+        ],
+    )
+    def test_main(self, *, requirement: str, expected: str) -> None:
+        assert format_requirement(requirement) == expected
 
 
 class TestSortedRequirement:
--- src/utilities/packaging.py
+++ src/utilities/packaging.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, override
+from typing import override
 
-from packaging._tokenizer import ParserSyntaxError
-from packaging.requirements import InvalidRequirement, Requirement, _parse_requirement
+from packaging.requirements import Requirement, _parse_requirement
 from packaging.specifiers import Specifier, SpecifierSet
-
-if TYPE_CHECKING:
-    from collections.abc import Iterator
 
 
 def format_requirement(requirement: str, /) -> str:
@@ -18,26 +14,8 @@ class SortedRequirement(Requirement):
     @override
     def __init__(self, requirement_string: str) -> None:
         super().__init__(requirement_string)
-        try:
-            parsed = _parse_requirement(requirement_string)
-        except ParserSyntaxError as e:
-            raise InvalidRequirement(str(e)) from e
+        parsed = _parse_requirement(requirement_string)
         self.specifier = _CustomSpecifierSet(parsed.specifier)
-
-    @override
-    def _iter_parts(self, name: str) -> Iterator[str]:
-        yield name
-        if self.extras:
-            formatted_extras = ",".join(sorted(self.extras))
-            yield f"[{formatted_extras}]"
-        if self.specifier:
-            yield f" {self.specifier}"
-        if self.url:
-            yield f"@ {self.url}"
-            if self.marker:
-                yield " "
-        if self.marker:
-            yield f"; {self.marker}"
 
 
 class _CustomSpecifierSet(SpecifierSet):
