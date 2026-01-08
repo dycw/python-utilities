acd7e6312bcc889646447d2c88ce3b7826b50276
Derek Wan <d.wan@icloud.com>
2026-01-08 20:41:50 +0900

    2026-01-08 20:41:50 (Thu)  > DW-Mac  > derekwan

--- src/tests/test_throttle.py
+++ src/tests/test_throttle.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 
+from asyncio import sleep
 from inspect import signature
-from time import sleep
-from typing import TYPE_CHECKING, ClassVar, NoReturn
+from typing import TYPE_CHECKING, NoReturn
 
-from pytest import fixture, mark, param, raises
+from pytest import mark, param, raises
 
-from utilities.asyncio import sleep_td, timeout_td
+from utilities.asyncio import sleep_td
 from utilities.iterables import one
 from utilities.os import temp_environ
 from utilities.throttle import throttle
-from utilities.types import Delta
 from utilities.whenever import SECOND
 
 if TYPE_CHECKING:
@@ -107,6 +106,94 @@ class TestThrottle:
         func()
         assert counter == 2
 
+    @mark.parametrize("on_try", [param(False), param(True)])
+    async def test_async_func_passing(self, *, on_try: bool, temp_file: Path) -> None:
+        counter = 0
+
+        @throttle(on_try=on_try, delta=_DELTA, path=temp_file)
+        async def func() -> None:
+            await sleep(0)
+            nonlocal counter
+            counter += 1
+
+        for _ in range(2):
+            await func()
+            assert counter == 1
+            assert temp_file.is_file()
+        await sleep_td(2 * _DELTA)
+        for _ in range(2):
+            await func()
+            assert counter == 2
+
+    @mark.parametrize("on_try", [param(False), param(True)])
+    async def test_async_func_with_raiser(
+        self, *, on_try: bool, temp_file: Path
+    ) -> None:
+        class CustomError(Exception): ...
+
+        counter = 0
+
+        def raiser() -> NoReturn:
+            raise CustomError
+
+        @throttle(on_try=on_try, delta=_DELTA, path=temp_file, raiser=raiser)
+        async def func() -> None:
+            await sleep(0)
+            nonlocal counter
+            counter += 1
+
+        await func()
+        assert counter == 1
+        assert temp_file.is_file()
+        with raises(CustomError):
+            await func()
+        assert counter == 1
+
+    async def test_async_func_on_pass_failing(self, *, temp_file: Path) -> None:
+        class CustomError(Exception): ...
+
+        counter = 0
+
+        @throttle(delta=_DELTA, path=temp_file)
+        async def func() -> None:
+            await sleep(0)
+            nonlocal counter
+            counter += 1
+            raise CustomError
+
+        for i in range(2):
+            with raises(CustomError):
+                await func()
+            assert counter == (i + 1)
+            assert not temp_file.exists()
+
+    async def test_async_on_func_on_try_failing(self, *, temp_file: Path) -> None:
+        class CustomError(Exception): ...
+
+        counter = 0
+
+        @throttle(on_try=True, delta=_DELTA, path=temp_file)
+        async def func() -> None:
+            await sleep(0)
+            nonlocal counter
+            counter += 1
+            raise CustomError
+
+        with raises(CustomError):
+            await func()
+        assert counter == 1
+        assert temp_file.is_file()
+        await func()
+        assert counter == 1
+        await sleep_td(2 * _DELTA)
+        with raises(CustomError):
+            await func()
+        assert counter == 2
+        assert temp_file.is_file()
+        await func()
+        assert counter == 2
+
+    ##
     @mark.skip
     @mark.parametrize("on_try", [param(True), param(False)])
     def test_disabled_via_env_var(
