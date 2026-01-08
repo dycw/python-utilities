b3415abc769b8a81c7158a4f0b626a629fea2087
Derek Wan <d.wan@icloud.com>
2026-01-08 11:30:22 +0900

    Add `packaging` (#1822)

---
+++ src/tests/test_packaging.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from pytest import mark, param, raises
+
+from utilities.iterables import one
+from utilities.packaging import Requirement
+
+
+class TestRequirement:
+    def test_extra(self) -> None:
+        extra = "extra"
+        requirement = Requirement.new(f"package[{extra}]")
+        assert requirement.extras == [extra]
+
+    def test_get_item(self) -> None:
+        requirement = Requirement.new("package>=1.2.3, <1.3")
+        assert requirement[">="] == "1.2.3"
+        assert requirement["<"] == "1.3"
+        with raises(KeyError):
+            _ = requirement[">"]
+
+    def test_marker(self) -> None:
+        requirement = Requirement.new('package; python_version >= "3"')
+        assert requirement.marker is not None
+        variable, op, value = one(requirement.marker)
+        assert str(variable) == "python_version"
+        assert str(op) == ">="
+        assert str(value) == "3"
+
+    def test_name(self) -> None:
+        requirement = Requirement.new("package")
+        assert requirement.name == "package"
+
+    def test_specifier(self) -> None:
+        requirement = Requirement.new("package>=1.2.3, <1.3")
+        assert requirement.specifier == ">=1.2.3,<1.3"
+
+    def test_specifier_set(self) -> None:
+        requirement = Requirement.new("package>=1.2.3, <1.3")
+        assert requirement.specifier_set == ">=1.2.3,<1.3"
+
+    @mark.parametrize(
+        ("requirement", "expected"),
+        [
+            param("package", "package"),
+            param("package>=1.2.3", "package>=1.2.3"),
+            param("package<1.3", "package<1.3"),
+            param("package>=1.2.3,<1.3", "package>=1.2.3, <1.3"),
+            param("package<1.3,>=1.2.3", "package>=1.2.3, <1.3"),
+            param("package[extra]>=1.2.3", "package[extra]>=1.2.3"),
+            param("package[extra1,extra2]>=1.2.3", "package[extra1,extra2]>=1.2.3"),
+            param("package@https://www.github.com", "package@ https://www.github.com"),
+            param("package;python_version>='3'", 'package; python_version >= "3"'),
+            param(
+                "package[extra1,extra2]@https://www.github.com>=1.2.3,<1.3;python_version>='3'",
+                "package[extra1,extra2]@ https://www.github.com>=1.2.3,<1.3;python_version>='3'",
+            ),
+        ],
+    )
+    def test_str(self, *, requirement: str, expected: str) -> None:
+        assert str(Requirement.new(requirement)) == expected
+
+    def test_url(self) -> None:
+        url = "https://www.github.com"
+        requirement = Requirement.new(f"package@{url}")
+        assert requirement.url == url
---
+++ src/utilities/packaging.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Self, override
+
+import packaging._parser
+import packaging.requirements
+from packaging.requirements import _parse_requirement
+from packaging.specifiers import Specifier, SpecifierSet
+
+from utilities.iterables import OneEmptyError, one
+
+if TYPE_CHECKING:
+    from packaging._parser import MarkerList
+
+
+@dataclass(order=True, unsafe_hash=True, kw_only=True, slots=True)
+class Requirement:
+    requirement: str
+    _parsed_req: packaging._parser.ParsedRequirement
+    _custom_req: _CustomRequirement
+
+    def __getitem__(self, key: str, /) -> str:
+        return self.specifier_set[key]
+
+    @override
+    def __str__(self) -> str:
+        return str(self._custom_req)
+
+    @classmethod
+    def new(cls, requirement: str, /) -> Self:
+        return cls(
+            requirement=requirement,
+            _parsed_req=_parse_requirement(requirement),
+            _custom_req=_CustomRequirement(requirement),
+        )
+
+    @property
+    def extras(self) -> list[str]:
+        return self._parsed_req.extras
+
+    @property
+    def marker(self) -> MarkerList | None:
+        return self._parsed_req.marker
+
+    @property
+    def name(self) -> str:
+        return self._parsed_req.name
+
+    @property
+    def specifier(self) -> str:
+        return self._parsed_req.specifier
+
+    @property
+    def specifier_set(self) -> _CustomSpecifierSet:
+        return _CustomSpecifierSet(_parse_requirement(self.requirement).specifier)
+
+    @property
+    def url(self) -> str:
+        return self._parsed_req.url
+
+
+class _CustomRequirement(packaging.requirements.Requirement):
+    @override
+    def __init__(self, requirement_string: str) -> None:
+        super().__init__(requirement_string)
+        parsed = _parse_requirement(requirement_string)
+        self.specifier = _CustomSpecifierSet(parsed.specifier)
+
+
+class _CustomSpecifierSet(SpecifierSet):
+    def __getitem__(self, key: str, /) -> str:
+        try:
+            return one(s.version for s in self if s.operator == key)
+        except OneEmptyError:
+            raise KeyError(key) from None
+
+    @override
+    def __str__(self) -> str:
+        specs = sorted(self._specs, key=self._key)
+        return ", ".join(map(str, specs))
+
+    def _key(self, spec: Specifier, /) -> int:
+        return [">=", "<"].index(spec.operator)
+
+
+__all__ = ["Requirement"]
