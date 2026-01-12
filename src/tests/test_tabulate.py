9a64d445a7d407f52704c3c9f538fa673f5993ea
Derek Wan <d.wan@icloud.com>
2026-01-12 12:26:20 +0900

    Add `func_param_desc` (#1831)

---
+++ src/tests/test_tabulate.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from utilities.tabulate import func_param_desc, params_table
+from utilities.text import strip_and_dedent
+
+
+class TestFuncParamDesc:
+    def test_empty(self) -> None:
+        x = 1
+        y = 2
+
+        def func() -> None: ...
+
+        result = func_param_desc(func, "0.0.1", f"{x=}", f"{y=}")
+        expected = strip_and_dedent("""
+            Running 'func' (version 0.0.1) with:
+              ╭───┬───╮
+              │ x │ 1 │
+              │ y │ 2 │
+              ╰───┴───╯
+        """)
+        assert result == expected
+
+    def test_main(self) -> None:
+        x = 1
+        y = 2
+        result = params_table(f"{x=}", f"{y=}")
+        expected = strip_and_dedent("""
+            ╭───┬───╮
+            │ x │ 1 │
+            │ y │ 2 │
+            ╰───┴───╯
+        """)
+        assert result == expected
+
+
+class TestParamsTable:
+    def test_empty(self) -> None:
+        result = params_table()
+        expected = ""
+        assert result == expected
+
+    def test_main(self) -> None:
+        x = 1
+        y = 2
+        result = params_table(f"{x=}", f"{y=}")
+        expected = strip_and_dedent("""
+            ╭───┬───╮
+            │ x │ 1 │
+            │ y │ 2 │
+            ╰───┴───╯
+        """)
+        assert result == expected
---
+++ src/utilities/tabulate.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from textwrap import indent
+from typing import TYPE_CHECKING, Any
+
+from tabulate import tabulate
+
+from utilities.functions import get_func_name
+from utilities.text import split_f_str_equals
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+
+def func_param_desc(func: Callable[..., Any], version: str, /, *variables: str) -> str:
+    """Generate a string describing a function & its parameters."""
+    name = get_func_name(func)
+    table = indent(params_table(*variables), "  ")
+    return f"""\
+Running {name!r} (version {version}) with:
+{table}"""
+
+
+def params_table(*variables: str) -> str:
+    """Generate a table of parameter names and values."""
+    return tabulate(
+        list(map(split_f_str_equals, variables)), tablefmt="rounded_outline"
+    )
+
+
+__all__ = ["func_param_desc", "params_table"]
