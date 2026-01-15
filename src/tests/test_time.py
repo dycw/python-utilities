87e301f5a36f76acd60cfe05afcf89ad5398abdb
Derek Wan <d.wan@icloud.com>
2026-01-15 13:15:03 +0900

    2026-01-15 13:15:03 (Thu)  > DW-Mac  > derekwan

--- src/tests/test_time.py
+++ src/tests/test_time.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from utilities.constants import SECOND
 from utilities.time import sleep
 from utilities.timer import Timer
+
+if TYPE_CHECKING:
+    from whenever import TimeDelta
+
+
+_DURATION: TimeDelta = 0.05 * SECOND
+_MULTIPLE: int = 1
 
 
 class TestSleep:
     def test_main(self) -> None:
-        delta = 0.1
         with Timer() as timer:
-            sleep(delta)
-        assert timer <= 10 * delta
+            sleep(_DURATION)
+        assert timer <= _MULTIPLE * _DURATION
 
     def test_none(self) -> None:
         sleep()
